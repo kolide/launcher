b84abb2b1bd47dab6df461a3c7db51871036dce7
Zachary Wasserman <zachwass2000@gmail.com>
2017-05-05 10:06:50 -0700

    Remove linux differentiation

--- platform.go
+++ platform.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"errors"
-	"os"
 	"runtime"
 )
 
@@ -13,8 +12,7 @@ const (
 	Unknown OsqueryPlatform = "unknown"
 	Windows OsqueryPlatform = "windows"
 	Darwin  OsqueryPlatform = "darwin"
-	Ubuntu  OsqueryPlatform = "ubuntu"
-	CentOS  OsqueryPlatform = "centos"
+	Linux   OsqueryPlatform = "linux"
 )
 
 // DetectPlatform returns the runtime platform, or an error if it cannot
@@ -26,26 +24,8 @@ func DetectPlatform() (OsqueryPlatform, error) {
 	case "darwin":
 		return Darwin, nil
 	case "linux":
-		return detectLinux()
+		return Linux, nil
 	default:
 		return Unknown, errors.New("unrecognized runtime.GOOS: " + runtime.GOOS)
-	}
-}
-
-// fileExists checks whether a file exists at the given path
-func fileExists(path string) bool {
-	_, err := os.Stat(path)
-	return err == nil
-}
-
-// detectLinux differentiates between the supported flavors of Linux
-func detectLinux() (OsqueryPlatform, error) {
-	switch {
-	case fileExists("/etc/debian_version"):
-		return Ubuntu, nil
-	case fileExists("/etc/redhat-release") || fileExists("/etc/centos-relese"):
-		return CentOS, nil
-	default:
-		return Unknown, errors.New("cannot differentiate Linux flavor")
 	}
 }
