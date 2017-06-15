aa31026e0876a08a07991908110c3a2f6a0521d7
Mike Arpaia <mike@arpaia.co>
2017-06-15 13:53:57 -0600

    @groob comments

--- osquery/runtime.go
+++ osquery/runtime.go
@@ -17,8 +17,12 @@ type OsqueryInstance struct {
 // process initialization, an error will be returned.
 func LaunchOsqueryInstance(path string) (*OsqueryInstance, error) {
 	// Ensure that the supplied path exists
-	if _, err := os.Stat(path); err != nil && os.IsNotExist(err) {
-		return nil, errors.Wrapf(err, "path not found: %s", path)
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return nil, errors.Wrapf(err, "supplied osquery instance path: %s", path)
+		} else {
+			return nil, errors.Wrapf(err, "could not stat supplied osquery instance patch")
+		}
 	}
 
 	// TODO Launch the osqueryd process
@@ -28,7 +32,7 @@ func LaunchOsqueryInstance(path string) (*OsqueryInstance, error) {
 	oi := &OsqueryInstance{}
 	// TODO populate OsqueryInstance with relevant data
 
-	if err := oi.IsHealthy(); err != nil {
+	if ok, err := oi.Healthy(); !ok {
 		return nil, errors.Wrap(err, "osquery instance instantly became unhealthy after launch")
 	}
 
@@ -37,7 +41,7 @@ func LaunchOsqueryInstance(path string) (*OsqueryInstance, error) {
 
 // Kill will terminate all managed osquery processes and release all resources.
 func (o *OsqueryInstance) Kill() error {
-	if err := o.IsHealthy(); err != nil {
+	if ok, err := o.Healthy(); !ok {
 		return errors.Wrap(err, "could not kill osquery because it was not healthy")
 	}
 
@@ -49,21 +53,21 @@ func (o *OsqueryInstance) Kill() error {
 	return watcher.Kill()
 }
 
-// IsHealthy will check to determine whether or not the osquery process that is
+// Healthy will check to determine whether or not the osquery process that is
 // being managed by the current instantiation of this OsqueryInstance is
 // healthy.
-func (o *OsqueryInstance) IsHealthy() error {
+func (o *OsqueryInstance) Healthy() (bool, error) {
 	// TODO Query the osquery_info table and update the OsqueryInstance data
 	// structure if any information has changed
 
-	return errors.New("not implemented")
+	return false, errors.New("not implemented")
 }
 
 // Pid returns the process ID of the osqueryd watcher process (or whatever the
 // most senior process ID is). If the osquery instance is not healthy, an error
 // will be returned.
 func (o *OsqueryInstance) Pid() (int, error) {
-	if err := o.IsHealthy(); err != nil {
+	if ok, err := o.Healthy(); !ok {
 		return 0, errors.Wrap(err, "could not get the pid because osquery was not healthy")
 	}
 
