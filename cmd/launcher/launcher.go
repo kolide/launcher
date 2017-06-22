dfeb3205fe78500a39d7189f2a227020d6bd80b3
Victor Vrantchan <vrancean+github@gmail.com>
2017-06-22 17:34:26 -0400

    add updater imports

--- cmd/launcher/launcher.go
+++ cmd/launcher/launcher.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 
 	"github.com/kolide/launcher/osquery"
+	"github.com/kolide/updater"
 )
 
 func main() {
@@ -30,6 +31,21 @@ func main() {
 		workingDirectory = os.TempDir()
 	}
 
+	notary := os.Getenv("KOLIDE_LAUNCHER_NOTARY_URL")
+	if notary != "" {
+		osqueryUpdater, err := updater.Start(updater.Settings{}, updateOsquery)
+		if err != nil {
+			log.Fatalf("Error launching osqueryd updater service %s\n", err)
+		}
+		defer osqueryUpdater.Stop()
+
+		launcherUpdater, err := updater.Start(updater.Settings{}, updateLauncher)
+		if err != nil {
+			log.Fatalf("Error launching osqueryd updater service %s\n", err)
+		}
+		defer launcherUpdater.Stop()
+	}
+
 	if _, err := osquery.LaunchOsqueryInstance(*flBinPath, workingDirectory); err != nil {
 		log.Fatalf("Error launching osquery instance: %s", err)
 	}
@@ -37,4 +53,12 @@ func main() {
 	sig := make(chan os.Signal)
 	signal.Notify(sig, os.Interrupt)
 	<-sig
+}
+
+func updateOsquery(stagingDir string, err error) {
+	return
+}
+
+func updateLauncher(stagingDir string, err error) {
+	return
 }
