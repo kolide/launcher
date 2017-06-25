71d3ee2f9f75318b9f67570391e500eaea6ddbad
Victor Vrantchan <vrancean+github@gmail.com>
2017-06-24 23:11:38 -0400

    rename files

--- osquery/table/mdfind/mdfind.go
+++ osquery/table/mdfind/mdfind.go
@@ -1,37 +1,52 @@
-// +build !cgo darwin
+// +build darwin
 
+/*
+package mdfind implements an osquery table for the macOS spotlight search.
+
+Example Query:
+	SELECT uid, f.path FROM file
+	AS f JOIN mdfind ON mdfind.path = f.path
+	AND mdfind.query = "kMDItemKint = 'Agile Keychain'";
+*/
 package mdfind
 
 import (
-	"bufio"
-	"bytes"
 	"context"
-	"io"
-	"os/exec"
-	"time"
+	"strings"
+
+	"github.com/kolide/osquery-go/plugin/table"
+	"github.com/pkg/errors"
 )
 
-func mdfind(args ...string) ([]string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
+func NewTable(name string) *table.Plugin {
+	return table.NewPlugin(name, Columns(), Generate)
+}
 
-	path := "/usr/bin/mdfind"
+func Columns() []table.ColumnDefinition {
+	return []table.ColumnDefinition{
+		table.TextColumn("query"),
+		table.TextColumn("path"),
+	}
+}
 
-	out, err := exec.CommandContext(ctx, path, args...).Output()
+func Generate(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+	where := queryContext.Constraints["query"].Constraints[0].Expression
+	var query []string
+	if strings.Contains(where, "-") {
+		query = strings.Split(where, " ")
+	} else {
+		query = []string{where}
+	}
+	lines, err := mdfind(query...)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "call mdfind")
 	}
-	var lines []string
-	lr := bufio.NewReader(bytes.NewReader(out))
-	for {
-		line, _, err := lr.ReadLine()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-		lines = append(lines, string(line))
+	var resp []map[string]string
+	for _, line := range lines {
+		m := make(map[string]string, 2)
+		m["query"] = where
+		m["path"] = line
+		resp = append(resp, m)
 	}
-	return lines, nil
+	return resp, nil
 }
