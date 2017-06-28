8e1f6fca987d32e44cfdc6f29f365e553d7b0f71
Mike Arpaia <mike@arpaia.co>
2017-06-28 13:44:43 -0600

    example illustrating the bug

--- osquery/best_practices_test.go
+++ osquery/best_practices_test.go
@@ -1,6 +1,7 @@
 package osquery
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -24,10 +25,29 @@ func TestBestPractices(t *testing.T) {
 	require.True(t, healthy)
 
 	results, err := instance.Query("select * from kolide_best_practices")
-	require.NoError(t, err)
-	require.Len(t, results, 1)
+	// results is nil
+	fmt.Printf("results : < %+v >\n", results)
+	// err is "Error running query: no such table: kolide_best_practices"
+	fmt.Printf("err : < %+v >\n", err)
 
-	passwordRequiredFromScreensaver, ok := results[0]["password_required_from_screensaver"]
+	// You can enumerate the whole registry from osquery's perspective. spotlight
+	// and kolide_best_practices are not there, but both internal_noop plugins
+	// are there (config and logger)
+	registry, err := instance.Query("select * from osquery_registry")
+	require.NoError(t, err)
+	fmt.Println("Printing the osquery registry...")
+	for _, row := range registry {
+		fmt.Printf("%s\t\t- %s\n", row["registry"], row["name"])
+	}
+
+	// now it works. wtf?
+	results2, err := instance.Query("select * from kolide_best_practices")
+	fmt.Printf("results2 : < %+v >\n", results2)
+	fmt.Printf("err : < %+v >\n", err)
+	require.NoError(t, err)
+	require.Len(t, results2, 1)
+
+	passwordRequiredFromScreensaver, ok := results2[0]["password_required_from_screensaver"]
 	require.True(t, ok)
 	require.Equal(t, "true", passwordRequiredFromScreensaver)
 
