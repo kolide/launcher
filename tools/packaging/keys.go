kolide/launcher

commit e52576b715a9555b23d3a63d65c85f2df9673d05

Packaging Tooling (#53)

tools/packaging/keys.go
@@ -1,0 +1,36 @@
+package packaging
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// enrollSecret will generate an enrollment secret for a tenant given a valid
+// signing key
+func enrollSecret(tenantName string, pemKey []byte) (string, error) {
+	fingerPrint := fmt.Sprintf("% x", md5.Sum([]byte(pemKey)))
+	fingerPrint = strings.Replace(fingerPrint, " ", ":", 15)
+
+	var claims = struct {
+		tenant string `json:"tenant"`
+		KID    string `json:"kid"`
+		jwt.StandardClaims
+	}{
+		tenant: tenantName,
+		KID:    fingerPrint,
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(pemKey)
+	if err != nil {
+		return "", fmt.Errorf("parsing pem key: %s", err)
+	}
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodRS256, &claims)
+	signed, err := jwtToken.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+	return signed, nil
+}

.gitignore
@@ -1,5 +1,6 @@
 vendor
 build
+bin
 .DS_Store
 
 ## Below from https://github.com/github/gitignore/blob/master/Go.gitignore ##

Makefile
@@ -24,7 +24,6 @@ else
 	NOW	= $(shell powershell Get-Date -format s)
 endif
 
-
 build: launcher extension
 
 .pre-build:
@@ -43,8 +42,17 @@ launcher: .pre-build
 	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.buildUser=${USER} \
 	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.goVersion=${GOVERSION}" ./cmd/launcher/
 
-mac-pkg-builder: .pre-build
-	go build -i -o build/mac-pkg-builder ./cmd/mac-pkg-builder/
+package-builder: .pre-build launcher extension
+	go build -i -o build/package-builder -ldflags "\
+	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.appName=package-builder \
+	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.version=${VERSION} \
+	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.branch=${BRANCH} \
+	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.revision=${REVISION} \
+	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.buildDate=${NOW} \
+	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.buildUser=${USER} \
+	-X github.com/kolide/launcher/vendor/github.com/kolide/kit/version.goVersion=${GOVERSION}" ./cmd/package-builder/
+
+
 
 deps:
 	go get -u github.com/Masterminds/glide
@@ -52,11 +60,3 @@ deps:
 
 test:
 	go test -cover -race -v $(shell go list ./... | grep -v /vendor/)
-
-build-mac-pkg: launcher extension mac-pkg-builder
-	mkdir -p bin/
-	cp /usr/local/bin/osqueryd ./bin
-	cp ./build/launcher ./bin
-	cp ./build/osquery-extension.ext ./bin
-	./build/mac-pkg-builder -key ${ENROLL_JWT_KEY} -package
-	rm -rf bin

cmd/launcher/launcher.go
@@ -3,14 +3,13 @@ package main
 import (
 	"context"
 	"flag"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path/filepath"
 	"time"
-
-	"google.golang.org/grpc"
 
 	"github.com/boltdb/bolt"
 	"github.com/kolide/kit/env"
@@ -20,6 +19,8 @@ import (
 	"github.com/kolide/osquery-go/plugin/config"
 	"github.com/kolide/osquery-go/plugin/distributed"
 	"github.com/kolide/osquery-go/plugin/logger"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -32,12 +33,13 @@ var (
 // options is the set of configurable options that may be set when launching this
 // program
 type options struct {
-	osquerydPath    string
-	rootDirectory   string
-	notaryServerURL string
-	kolideServerURL string
-	enrollSecret    string
-	printVersion    bool
+	osquerydPath     string
+	rootDirectory    string
+	notaryServerURL  string
+	kolideServerURL  string
+	enrollSecret     string
+	enrollSecretPath string
+	printVersion     bool
 }
 
 // parseOptions parses the options that may be configured via command-line flags
@@ -75,16 +77,22 @@ func parseOptions() (*options, error) {
 			env.String("KOLIDE_LAUNCHER_ENROLL_SECRET", ""),
 			"Enroll secret to authenticate with the Kolide server",
 		)
+		flEnrollSecretPath = flag.String(
+			"enroll_secret_path",
+			env.String("KOLIDE_LAUNCHER_ENROLL_SECRET_PATH", ""),
+			"Path to a file containing the enroll secret to authenticate with the Kolide server",
+		)
 	)
 	flag.Parse()
 
 	opts := &options{
-		osquerydPath:    *flOsquerydPath,
-		rootDirectory:   *flRootDirectory,
-		notaryServerURL: *flNotaryServerURL,
-		printVersion:    *flVersion,
-		kolideServerURL: *flKolideServerURL,
-		enrollSecret:    *flEnrollSecret,
+		osquerydPath:     *flOsquerydPath,
+		rootDirectory:    *flRootDirectory,
+		notaryServerURL:  *flNotaryServerURL,
+		printVersion:     *flVersion,
+		kolideServerURL:  *flKolideServerURL,
+		enrollSecret:     *flEnrollSecret,
+		enrollSecretPath: *flEnrollSecretPath,
 	}
 
 	// if an osqueryd path was not set, it's likely that we want to use the bundled
@@ -96,8 +104,12 @@ func parseOptions() (*options, error) {
 		} else if path, err := exec.LookPath("osqueryd"); err == nil {
 			opts.osquerydPath = path
 		} else {
-			log.Fatal("Could not find osqueryd binary")
+			return nil, errors.New("Could not find osqueryd binary")
 		}
+	}
+
+	if opts.enrollSecret != "" && opts.enrollSecretPath != "" {
+		return nil, errors.New("Both enroll_secret and enroll_secret_path were defined")
 	}
 
 	return opts, nil
@@ -150,7 +162,17 @@ func main() {
 
 	client := service.New(conn)
 
-	ext, err := osquery.NewExtension(client, db, osquery.ExtensionOpts{EnrollSecret: opts.enrollSecret})
+	var enrollSecret string
+	if opts.enrollSecret != "" {
+		enrollSecret = opts.enrollSecret
+	} else if opts.enrollSecretPath != "" {
+		content, err := ioutil.ReadFile(opts.enrollSecretPath)
+		if err != nil {
+			log.Fatalf("Could not read enroll_secret_path (%s): %s", opts.enrollSecretPath, err)
+		}
+		enrollSecret = string(content)
+	}
+	ext, err := osquery.NewExtension(client, db, osquery.ExtensionOpts{EnrollSecret: enrollSecret})
 	if err != nil {
 		log.Fatalf("Error starting grpc extension: %s\n", err)
 	}

cmd/mac-pkg-builder/mac-pkg-builder.go
@@ -1,277 +1,0 @@
-package main
-
-import (
-	"bytes"
-	"crypto/md5"
-	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-
-	jwt "github.com/dgrijalva/jwt-go"
-)
-
-const (
-	packageRoot = "mac-pkg/root"
-	scriptsRoot = "mac-pkg/scripts"
-)
-
-func createMacPackage(pemKey []byte, id int, p packageParams) {
-	// make temp directory for package
-	pkgroot := filepath.Join(os.TempDir(), "pkgroot")
-	os.RemoveAll(pkgroot)
-
-	if err := copyDir(packageRoot, pkgroot); err != nil {
-		log.Fatal(err)
-	}
-	etcPath := filepath.Join(pkgroot, "etc", "kolide")
-	if err := os.MkdirAll(etcPath, dirMode); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := os.MkdirAll(filepath.Join(pkgroot, "var", "kolide"), dirMode); err != nil {
-		log.Fatal(err)
-	}
-
-	binPath := filepath.Join(pkgroot, "usr", "local", "kolide", "bin")
-	if err := os.MkdirAll(binPath, dirMode); err != nil {
-		log.Fatal(err)
-	}
-
-	t := tennant{id: id}
-	secretPath := filepath.Join(etcPath, "token")
-	token, err := t.Secret(pemKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := ioutil.WriteFile(secretPath, []byte(token), 0400); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := copyDir(p.bindir, binPath); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := pkgbuild(pkgroot, "1.0.0", fmt.Sprintf("%s-launcher.pkg", t.Name())); err != nil {
-		log.Fatal(err)
-	}
-
-	os.RemoveAll(pkgroot)
-
-}
-
-/*
-runs the following pkgbuild command:
-	pkgbuild \
-	--root root \
-	--scripts scripts \
-	--identifier ${PKGID} \
-	--version ${PKGVERSION} \
-	out/${PKGNAME}-${PKGVERSION}.pkg
-*/
-func pkgbuild(pkgroot, version, pkgname string) error {
-	identifier := "com.kolide.osquery"
-	cmd := exec.Command("pkgbuild",
-		"--root", pkgroot,
-		"--scripts", scriptsRoot,
-		"--identifier", identifier,
-		"--version", version,
-		fmt.Sprintf("build/%s", pkgname),
-	)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
-}
-
-const (
-	dirMode  = 0755
-	fileMode = 0644
-)
-
-func copyDir(src, dest string) error {
-	dir, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	if err := os.MkdirAll(dest, dirMode); err != nil {
-		return err
-	}
-
-	files, err := dir.Readdir(-1)
-	if err != nil {
-		return err
-	}
-	for _, file := range files {
-		srcptr := filepath.Join(src, file.Name())
-		dstptr := filepath.Join(dest, file.Name())
-		if file.IsDir() {
-			if err := copyDir(srcptr, dstptr); err != nil {
-				return err
-			}
-		} else {
-			if err := copyFile(srcptr, dstptr); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func copyFile(src, dest string) error {
-	source, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer source.Close()
-
-	destfile, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer destfile.Close()
-
-	_, err = io.Copy(destfile, source)
-	if err != nil {
-		return err
-	}
-	sourceinfo, err := os.Stat(src)
-	if err != nil {
-		return err
-	}
-
-	return os.Chmod(dest, sourceinfo.Mode())
-}
-
-func printTennant(pemKey []byte, id int, p packageParams) {
-	t := tennant{id: id}
-	token, err := t.Secret(pemKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Name: %s\n", t.Name())
-	fmt.Printf("JWT: %s\n", token)
-}
-
-type tennant struct {
-	id     int
-	name   string
-	secret string
-}
-
-type packageParams struct {
-	bindir string //path to agent, osquery and other binaries that need to be in the pkg
-}
-
-func (t *tennant) Name() string {
-	if t.name != "" {
-		return t.name
-	}
-	m := newMunemo()
-	m.tos(t.id)
-	t.name = m.String()
-	return t.name
-}
-
-func (t *tennant) Secret(pemKey []byte) (token string, err error) {
-	fingerPrint := fmt.Sprintf("% x", md5.Sum([]byte(pemKey)))
-	fingerPrint = strings.Replace(fingerPrint, " ", ":", 15)
-
-	var claims = struct {
-		Tennant string `json:"tennant"`
-		KID     string `json:"kid"`
-		jwt.StandardClaims
-	}{
-		Tennant: t.Name(),
-		KID:     fingerPrint,
-	}
-
-	key, err := jwt.ParseRSAPrivateKeyFromPEM(pemKey)
-	if err != nil {
-		return "", fmt.Errorf("parsing pem key: %s", err)
-	}
-	jwtToken := jwt.NewWithClaims(jwt.SigningMethodRS256, &claims)
-	signed, err := jwtToken.SignedString(key)
-	if err != nil {
-		return "", err
-	}
-	return signed, nil
-}
-
-// munemo is based off of the ruby library https://github.com/jmettraux/munemo
-// it provides a deterministic way to create a tennant name from UID
-type munemo struct {
-	count int
-	neg   string
-	syls  []string
-	w     *bytes.Buffer // string buffer
-}
-
-func newMunemo() *munemo {
-	m := &munemo{
-		syls: []string{"ba", "bi", "bu", "be", "bo", "cha", "chi", "chu", "che", "cho", "da", "di", "du", "de", "do", "fa", "fi", "fu", "fe", "fo", "ga", "gi", "gu", "ge", "go", "ha", "hi", "hu", "he", "ho", "ja", "ji", "ju", "je", "jo", "ka", "ki", "ku", "ke", "ko", "la", "li", "lu", "le", "lo", "ma", "mi", "mu", "me", "mo", "na", "ni", "nu", "ne", "no", "pa", "pi", "pu", "pe", "po", "ra", "ri", "ru", "re", "ro", "sa", "si", "su", "se", "so", "sha", "shi", "shu", "she", "sho", "ta", "ti", "tu", "te", "to", "tsa", "tsi", "tsu", "tse", "tso", "wa", "wi", "wu", "we", "wo", "ya", "yi", "yu", "ye", "yo", "za", "zi", "zu", "ze", "zo"},
-		neg:  "xa",
-		w:    new(bytes.Buffer),
-	}
-	m.count = len(m.syls)
-	return m
-}
-
-func (m *munemo) String() string {
-	return m.w.String()
-}
-
-func (m *munemo) tos(num int) {
-	if num < 0 {
-		m.w.Write([]byte(m.neg))
-		return
-	}
-
-	mod := num % m.count
-	rst := num / m.count
-
-	if rst > 0 {
-		m.tos(rst)
-	}
-
-	m.w.Write([]byte(m.syls[mod]))
-}
-
-func main() {
-	var (
-		flKey       = flag.String("key", "", "path to rsa private key")
-		flTennantID = flag.Int("id", 100001, "tennant id. must be greater than 1")
-		flPrint     = flag.Bool("print", false, "print info for stdout -- requires passing a tennant id")
-		flPackage   = flag.Bool("package", false, "generate macOS package")
-		bindirPath  = flag.String("bindir", "./bin", "path to binaries")
-	)
-	flag.Parse()
-
-	printMode := (*flTennantID > 0) && *flPrint
-	packageMode := !printMode && *flPackage
-	badInput := (*flTennantID == 0 && *flPrint) || *flKey == "" || (!printMode && !packageMode)
-	if badInput {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	keyData, err := ioutil.ReadFile(*flKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	p := packageParams{bindir: *bindirPath}
-	if printMode {
-		printTennant(keyData, *flTennantID, p)
-		return
-	}
-
-	if packageMode {
-		createMacPackage(keyData, *flTennantID, p)
-	}
-
-}

cmd/package-builder/dev.go
@@ -1,0 +1,177 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/kolide/kit/env"
+	"github.com/kolide/launcher/tools/packaging"
+	"github.com/pkg/errors"
+)
+
+func safePathHostname(hostname string) string {
+	return strings.Replace(hostname, ":", "-", -1)
+}
+
+func createMacPackage(uploadRoot, osqueryVersion, hostname, tenant string, pemKey []byte) (string, error) {
+	macPackagePath, err := packaging.MakeMacOSPkg(osqueryVersion, tenant, hostname, pemKey)
+	defer os.RemoveAll(filepath.Dir(macPackagePath))
+	if err != nil {
+		return "", errors.Wrap(err, "could not make macOS package")
+	}
+
+	darwinRoot := filepath.Join(uploadRoot, safePathHostname(hostname), tenant, "darwin")
+	if err := os.MkdirAll(darwinRoot, packaging.DirMode); err != nil {
+		return "", errors.Wrap(err, "could not create darwin root")
+	}
+
+	destinationPath := filepath.Join(darwinRoot, "launcher.pkg")
+	if err = packaging.CopyFile(macPackagePath, destinationPath); err != nil {
+		return "", errors.Wrap(err, "could not copy file to upload root")
+	}
+	return destinationPath, nil
+}
+
+func runDev(args []string) error {
+	flagset := flag.NewFlagSet("query", flag.ExitOnError)
+	var (
+		flDebug = flagset.Bool(
+			"debug",
+			false,
+			"enable debug logging",
+		)
+		flOsqueryVersion = flagset.String(
+			"osquery_version",
+			env.String("KOLIDE_LAUNCHER_PACKAGE_BUILDER_OSQUERY_VERSION", ""),
+			"the osquery version to include in the resultant packages",
+		)
+		flEnrollmentSecretSigningKeyPath = flagset.String(
+			"enrollment_secret_signing_key",
+			env.String("KOLIDE_LAUNCHER_PACKAGE_BUILDER_ENROLLMENT_SECRET_SIGNING_KEY", ""),
+			"the path to the PEM key which is used to sign the enrollment secret JWT token",
+		)
+	)
+
+	flagset.Usage = usageFor(flagset, "package-builder dev [flags]")
+	if err := flagset.Parse(args); err != nil {
+		return err
+	}
+
+	logger := log.NewJSONLogger(os.Stderr)
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+	logger = log.With(logger, "caller", log.DefaultCaller)
+
+	if *flDebug {
+		logger = level.NewFilter(logger, level.AllowDebug())
+	} else {
+		logger = level.NewFilter(logger, level.AllowInfo())
+	}
+
+	osqueryVersion := *flOsqueryVersion
+	if osqueryVersion == "" {
+		osqueryVersion = "stable"
+	}
+
+	enrollmentSecretSigningKeyPath := *flEnrollmentSecretSigningKeyPath
+	if enrollmentSecretSigningKeyPath == "" {
+		enrollmentSecretSigningKeyPath = fmt.Sprintf("%s/tools/packaging/example_rsa.pem", packaging.LauncherSource())
+	}
+
+	if _, err := os.Stat(enrollmentSecretSigningKeyPath); err != nil {
+		if os.IsNotExist(err) {
+			return errors.Wrap(err, "key file doesn't exist")
+		} else {
+			return errors.Wrap(err, "could not stat key file")
+		}
+	}
+	pemKey, err := ioutil.ReadFile(enrollmentSecretSigningKeyPath)
+	if err != nil {
+		return errors.Wrap(err, "could not read the supplied key file")
+	}
+
+	// Generate packages for PRs
+	prToStartFrom, prToGenerateUntil := 350, 400
+	firstID, numberOfIDsToGenerate := 100001, 3
+
+	uploadRoot, err := ioutil.TempDir("", "upload_")
+	if err != nil {
+		return errors.Wrap(err, "could not create upload root temporary directory")
+	}
+	defer os.RemoveAll(uploadRoot)
+
+	makeHostnameDirInRoot := func(hostname string) error {
+		if err := os.MkdirAll(filepath.Join(uploadRoot, safePathHostname(hostname)), packaging.DirMode); err != nil {
+			return errors.Wrap(err, "could not create hostname root")
+		}
+		return nil
+	}
+
+	// Generate packages for localhost and master
+	hostnames := []string{
+		"master.cloud.kolide.net",
+		"localhost:5000",
+	}
+	for _, hostname := range hostnames {
+		if err := makeHostnameDirInRoot(hostname); err != nil {
+			return err
+		}
+		for id := firstID; id < firstID+numberOfIDsToGenerate; id++ {
+			tenant := packaging.Munemo(id)
+			destinationPath, err := createMacPackage(uploadRoot, osqueryVersion, hostname, tenant, pemKey)
+			if err != nil {
+				return errors.Wrap(err, "could not generate macOS package for tenant")
+			}
+			level.Debug(logger).Log(
+				"msg", "copied macOS package for tenant and hostname",
+				"destination", destinationPath,
+				"tenant", tenant,
+				"hostname", hostname,
+			)
+		}
+	}
+
+	// Generate packages for PRs
+	for i := prToStartFrom; i <= prToGenerateUntil; i++ {
+		hostname := fmt.Sprintf("%d.cloud.kolide.net", i)
+		if err := makeHostnameDirInRoot(hostname); err != nil {
+			return err
+		}
+
+		for id := firstID; id < firstID+numberOfIDsToGenerate; id++ {
+			tenant := packaging.Munemo(id)
+			destinationPath, err := createMacPackage(uploadRoot, osqueryVersion, hostname, tenant, pemKey)
+			if err != nil {
+				return errors.Wrap(err, "could not generate macOS package for tenant")
+			}
+			level.Debug(logger).Log(
+				"msg", "copied macOS package for tenant and hostname",
+				"path", destinationPath,
+				"tenant", tenant,
+				"hostname", hostname,
+			)
+		}
+	}
+
+	logger.Log(
+		"msg", "package generation complete",
+		"path", uploadRoot,
+	)
+
+	if err := packaging.GsutilRsync(uploadRoot, "gs://packaging/"); err != nil {
+		return errors.Wrap(err, "could not upload files to GCS")
+	}
+
+	if err := os.RemoveAll(uploadRoot); err != nil {
+		return errors.Wrap(err, "could not remove the upload root")
+	}
+
+	logger.Log("msg", "upload complete")
+
+	return nil
+}

cmd/package-builder/package-builder.go
@@ -1,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/kolide/kit/version"
+)
+
+func usageFor(fs *flag.FlagSet, short string) func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "USAGE\n")
+		fmt.Fprintf(os.Stderr, "  %s\n", short)
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "FLAGS\n")
+		w := tabwriter.NewWriter(os.Stderr, 0, 2, 2, ' ', 0)
+		fs.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(w, "\t-%s %s\t%s\n", f.Name, f.DefValue, f.Usage)
+		})
+		w.Flush()
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "USAGE\n")
+	fmt.Fprintf(os.Stderr, "  %s <mode> --help\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "MODES\n")
+	fmt.Fprintf(os.Stderr, "  dev          Generate development launcher packages and upload them to GCS\n")
+	fmt.Fprintf(os.Stderr, "  version      Print full version information\n")
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "VERSION\n")
+	fmt.Fprintf(os.Stderr, "  %s\n", version.Version().Version)
+	fmt.Fprintf(os.Stderr, "\n")
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	var run func([]string) error
+	switch strings.ToLower(os.Args[1]) {
+	case "dev":
+		run = runDev
+	case "version":
+		run = runVersion
+	default:
+		usage()
+		os.Exit(1)
+	}
+
+	if err := run(os.Args[2:]); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}

cmd/package-builder/version.go
@@ -1,0 +1,10 @@
+package main
+
+import (
+	"github.com/kolide/kit/version"
+)
+
+func runVersion(args []string) error {
+	version.PrintFull()
+	return nil
+}

commit a14f6936f7a05300f738a0ee14b7cf63017d55ff

use exported field in JWT claim
Fixes a bug introduced in #53. Since JWT uses reflection, all fields
in a claim must be exported.

tools/packaging/keys.go
@@ -15,11 +15,11 @@ func enrollSecret(tenantName string, pemKey []byte) (string, error) {
 	fingerPrint = strings.Replace(fingerPrint, " ", ":", 15)
 
 	var claims = struct {
-		tenant string `json:"tenant"`
+		Tenant string `json:"tenant"`
 		KID    string `json:"kid"`
 		jwt.StandardClaims
 	}{
-		tenant: tenantName,
+		Tenant: tenantName,
 		KID:    fingerPrint,
 	}
 