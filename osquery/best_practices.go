459cff98e7f0bee0b410c2f4e53c6b14cb902096
Zachary Wasserman <zachwass2000@gmail.com>
2017-08-21 17:41:21 -0700

    Update to use new client QueryRow functions

--- osquery/best_practices.go
+++ osquery/best_practices.go
@@ -24,48 +24,23 @@ func BestPractices(client *osquery.ExtensionManagerClient) *table.Plugin {
 	return table.NewPlugin("kolide_best_practices", columns, generateBestPractices(client))
 }
 
-func getQueryRows(client *osquery.ExtensionManagerClient, query string) ([]map[string]string, error) {
-	res, err := client.Query(query)
-	if err != nil {
-		return nil, errors.Wrap(err, "transport error in query")
-	}
-	if res.Status == nil {
-		return nil, errors.New("query returned nil status")
-	}
-	if res.Status.Code != 0 {
-		return nil, errors.Errorf("query returned error: %s", res.Status.Message)
-	}
-	return res.Response, nil
-}
-
-func getQueryRow(client *osquery.ExtensionManagerClient, query string) (map[string]string, error) {
-	res, err := getQueryRows(client, query)
-	if err != nil {
-		return nil, err
-	}
-	if len(res) != 1 {
-		return nil, errors.Errorf("expected 1 row, got %d", len(res))
-	}
-	return res[0], nil
-}
-
 func generateBestPractices(client *osquery.ExtensionManagerClient) table.GenerateFunc {
 	return func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
 		row := map[string]string{}
 
-		res, err := getQueryRow(client, "select enabled as enabled from sip_config where config_flag='sip'")
+		res, err := client.QueryRow("SELECT enabled AS enabled FROM sip_config WHERE config_flag='sip'")
 		if err != nil {
 			return nil, errors.Wrap(err, "query sip_config")
 		}
 		row[sipEnabled] = res["enabled"]
 
-		res, err = getQueryRow(client, "select assessments_enabled as enabled from gatekeeper")
+		res, err = client.QueryRow("SELECT assessments_enabled AS enabled FROM gatekeeper")
 		if err != nil {
 			return nil, errors.Wrap(err, "query gatekeeper")
 		}
 		row[gatekeeperEnabled] = res["enabled"]
 
-		res, err = getQueryRow(client, "select de.encrypted as enabled from mounts m join disk_encryption de on m.device_alias = de.name where m.path = '/'")
+		res, err = client.QueryRow("SELECT de.encrypted AS enabled FROM mounts m join disk_encryption de ON m.device_alias = de.name WHERE m.path = '/'")
 		if err != nil {
 			return nil, errors.Wrap(err, "query filevault")
 		}
