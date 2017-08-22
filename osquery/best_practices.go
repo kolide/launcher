4e52001f636597f1899f4b9d62f843c7b5dee7ad
Zachary Wasserman <zachwass2000@gmail.com>
2017-08-21 18:40:03 -0700

    Add screensaver and firewall

--- osquery/best_practices.go
+++ osquery/best_practices.go
@@ -10,9 +10,11 @@ import (
 )
 
 const (
-	sipEnabled        = "sip_enabled"
-	gatekeeperEnabled = "gatekeeper_enabled"
-	filevaultEnabled  = "filevault_enabled"
+	sipEnabled                 = "sip_enabled"
+	gatekeeperEnabled          = "gatekeeper_enabled"
+	filevaultEnabled           = "filevault_enabled"
+	firewallEnabled            = "firewall_enabled"
+	screensaverPasswordEnabled = "screensaver_password_enabled"
 )
 
 func BestPractices(client *osquery.ExtensionManagerClient) *table.Plugin {
@@ -20,33 +22,48 @@ func BestPractices(client *osquery.ExtensionManagerClient) *table.Plugin {
 		table.IntegerColumn(sipEnabled),
 		table.IntegerColumn(gatekeeperEnabled),
 		table.IntegerColumn(filevaultEnabled),
+		table.IntegerColumn(firewallEnabled),
+		table.IntegerColumn(screensaverPasswordEnabled),
 	}
 	return table.NewPlugin("kolide_best_practices", columns, generateBestPractices(client))
 }
 
 func generateBestPractices(client *osquery.ExtensionManagerClient) table.GenerateFunc {
 	return func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
-		row := map[string]string{}
+		res := map[string]string{}
 
-		res, err := client.QueryRow("SELECT enabled AS enabled FROM sip_config WHERE config_flag='sip'")
+		row, err := client.QueryRow("SELECT enabled AS enabled FROM sip_config WHERE config_flag='sip'")
 		if err != nil {
 			return nil, errors.Wrap(err, "query sip_config")
 		}
-		row[sipEnabled] = res["enabled"]
+		res[sipEnabled] = row["enabled"]
 
-		res, err = client.QueryRow("SELECT assessments_enabled AS enabled FROM gatekeeper")
+		row, err = client.QueryRow("SELECT assessments_enabled AS enabled FROM gatekeeper")
 		if err != nil {
 			return nil, errors.Wrap(err, "query gatekeeper")
 		}
-		row[gatekeeperEnabled] = res["enabled"]
+		res[gatekeeperEnabled] = row["enabled"]
 
-		res, err = client.QueryRow("SELECT de.encrypted AS enabled FROM mounts m join disk_encryption de ON m.device_alias = de.name WHERE m.path = '/'")
+		row, err = client.QueryRow("SELECT de.encrypted AS enabled FROM mounts m join disk_encryption de ON m.device_alias = de.name WHERE m.path = '/'")
 		if err != nil {
 			return nil, errors.Wrap(err, "query filevault")
 		}
-		row[filevaultEnabled] = res["enabled"]
+		res[filevaultEnabled] = row["enabled"]
 
-		fmt.Println(row) // TODO remove before merge
-		return []map[string]string{row}, nil
+		row, err = client.QueryRow("SELECT global_state AS enabled FROM alf")
+		if err != nil {
+			return nil, errors.Wrap(err, "query firewall")
+		}
+		res[firewallEnabled] = row["enabled"]
+
+		// TODO account for possibility of multiple logged in users
+		row, err = client.QueryRow("SELECT value AS enabled FROM preferences WHERE domain='com.apple.screensaver' AND key='askForPassword' AND username in (SELECT user FROM logged_in_users) LIMIT 1")
+		if err != nil {
+			return nil, errors.Wrap(err, "query screensaver password")
+		}
+		res[screensaverPasswordEnabled] = row["enabled"]
+
+		fmt.Println(res) // TODO remove before merge
+		return []map[string]string{res}, nil
 	}
 }
