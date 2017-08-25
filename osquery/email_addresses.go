32e15c76ea5b71ba46a92fd03dcf669052dd3f27
Mike Arpaia <mike@arpaia.co>
2017-08-25 12:25:49 -0400

    add extension manager client to table implementation

--- osquery/email_addresses.go
+++ osquery/email_addresses.go
@@ -3,26 +3,29 @@ package osquery
 import (
 	"context"
 
+	osquery "github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 )
 
-func EmailAddresses() *table.Plugin {
+func EmailAddresses(client *osquery.ExtensionManagerClient) *table.Plugin {
 	columns := []table.ColumnDefinition{
 		table.TextColumn("email"),
 		table.TextColumn("domain"),
 	}
-	return table.NewPlugin("email_addresses", columns, generateEmailAddresses)
+	return table.NewPlugin("email_addresses", columns, generateEmailAddresses(client))
 }
 
-func generateEmailAddresses(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
-	return []map[string]string{
-		map[string]string{
-			"email":  "mike@kolide.co",
-			"domain": "kolide.co",
-		},
-		map[string]string{
-			"email":  "mike@arpaia.co",
-			"domain": "arpaia.co",
-		},
-	}, nil
+func generateEmailAddresses(client *osquery.ExtensionManagerClient) table.GenerateFunc {
+	return func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+		return []map[string]string{
+			map[string]string{
+				"email":  "mike@kolide.co",
+				"domain": "kolide.co",
+			},
+			map[string]string{
+				"email":  "mike@arpaia.co",
+				"domain": "arpaia.co",
+			},
+		}, nil
+	}
 }
