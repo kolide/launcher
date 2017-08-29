8e66324d81ddc563659cfea793ffb27ec41f3636
Mike Arpaia <mike@arpaia.co>
2017-08-29 10:55:18 -0600

    google chrome state profiles

--- osquery/email_addresses.go
+++ osquery/email_addresses.go
@@ -2,9 +2,16 @@ package osquery
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	osquery "github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
+	"github.com/pkg/errors"
 )
 
 func EmailAddresses(client *osquery.ExtensionManagerClient) *table.Plugin {
@@ -17,19 +24,71 @@ func EmailAddresses(client *osquery.ExtensionManagerClient) *table.Plugin {
 
 func generateEmailAddresses(client *osquery.ExtensionManagerClient) table.GenerateFunc {
 	return func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
-		return []map[string]string{
-			map[string]string{
-				"email":  "jason@kolide.co",
-				"domain": "kolide.co",
-			},
-			map[string]string{
-				"email":  "jason@kolide.com",
-				"domain": "kolide.com",
-			},
-			map[string]string{
-				"email":  "jason.meller@gmail.com",
-				"domain": "gmail.com",
-			},
-		}, nil
+		results := []map[string]string{}
+
+		switch runtime.GOOS {
+		case "darwin":
+			// Google Chrome State Files
+			for _, stateFilePath := range findChromeStateFiles() {
+				fileContent, err := ioutil.ReadFile(stateFilePath)
+				if err != nil {
+					return nil, errors.Wrap(err, "could not read file")
+				}
+
+				var parsedStateFileContent chromeLocalStateFile
+				if err := json.Unmarshal(fileContent, &parsedStateFileContent); err != nil {
+					return nil, errors.Wrap(err, "could not unmarshal json file")
+				}
+
+				for _, profile := range parsedStateFileContent.Profile.InfoCache {
+					results = addEmailToResults(profile.Username, results)
+				}
+			}
+		}
+
+		return results, nil
 	}
+}
+
+type chromeLocalStateFile struct {
+	Profile struct {
+		InfoCache map[string]struct {
+			Username string `json:"user_name"`
+		} `json:"info_cache"`
+	} `json:"profile"`
+}
+
+func emailDomain(email string) string {
+	parts := strings.Split(email, "@")
+	switch len(parts) {
+	case 0:
+		return email
+	default:
+		return parts[len(parts)-1]
+	}
+}
+
+func addEmailToResults(email string, results []map[string]string) []map[string]string {
+	return append(results, map[string]string{
+		"email":  email,
+		"domain": emailDomain(email),
+	})
+}
+
+func findChromeStateFiles() []string {
+	chromeLocalStateFiles := []string{}
+	filesInUser, err := ioutil.ReadDir("/Users")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range filesInUser {
+		if f.IsDir() && (f.Name() != "Guest" || f.Name() != "Shared") {
+			stateFilePath := filepath.Join("/Users", f.Name(), "Library/Application Support/Google/Chrome/Local State")
+			if _, err := os.Stat(stateFilePath); err == nil {
+				chromeLocalStateFiles = append(chromeLocalStateFiles, stateFilePath)
+			}
+		}
+	}
+
+	return chromeLocalStateFiles
 }
