101c00cb6cb21435e5e66cd5bfc5ce42bc4253bc
John Murphy <murphybytes@gmail.com>
2017-09-13 11:40:21 -0500

    Use Go SDK instead of shelling out to rsutil (#118)

--- tools/packaging/gcp.go
+++ tools/packaging/gcp.go
@@ -1,17 +1,17 @@
 package packaging
 
 import (
+	"context"
+	"io"
 	"os"
 	"os/exec"
-)
+	"path/filepath"
+	"strings"
 
-// GsutilRsync copies a local directory of files to a specified bucket URI
-func GsutilRsync(source, bucketURI string) error {
-	cmd := exec.Command("gsutil", "-m", "rsync", "-d", "-r", source, bucketURI)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
+	"cloud.google.com/go/storage"
+	"github.com/pkg/errors"
+	"google.golang.org/api/iterator"
+)
 
 // SetGCPProject will set the local GCP project to the supplied project name
 func SetGCPProject(project string) error {
@@ -19,4 +19,55 @@ func SetGCPProject(project string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
+}
+
+// CopyContentsToCloudStorage recursively copies contents of the path uploadRoot to
+// the named cloud storage bucket. Items that exist in cloud storage but not in uploadRoot
+// are removed
+func CopyContentsToCloudStorage(uploadRoot, bucketName string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return errors.Wrapf(err, "preparing to copy %s to storage", uploadRoot)
+	}
+	defer client.Close()
+	// Clear out old objects. This is a fairly naive way to do this. We could use paging
+	// to grab batches of object attributes, record their names in a lookup table, and then remove
+	// the name if an object of the same name is uploaded, then delete what's left.
+	iter := client.Bucket(bucketName).Objects(ctx, nil)
+	for {
+		objAttr, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return errors.Wrap(err, "deleting existing objects")
+		}
+		if err := client.Bucket(bucketName).Object(objAttr.Name).Delete(ctx); err != nil {
+			return errors.Wrapf(err, "deleting %s", objAttr.Name)
+		}
+	}
+	// recursively upload new objects
+	err = filepath.Walk(uploadRoot, func(path string, info os.FileInfo, _ error) error {
+		if info.IsDir() {
+			return nil
+		}
+		objectName := strings.TrimLeft(strings.Replace(path, uploadRoot, "", 1), "/")
+		obj := client.Bucket(bucketName).Object(objectName)
+		rdr, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer rdr.Close()
+		wtr := obj.NewWriter(ctx)
+		defer wtr.Close()
+		if _, err = io.Copy(wtr, rdr); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return errors.Wrapf(err, "uploading from %s", uploadRoot)
+	}
+	return nil
 }
