a1adab004cb1f31cd98033bf17f969071936f54c
Mike Arpaia <mike@arpaia.co>
2017-09-21 13:43:16 -0600

    use normal http.Transport

--- tools/notary/generate_tuf.go
+++ tools/notary/generate_tuf.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"path"
 	"path/filepath"
-	"time"
 
 	"github.com/docker/notary/client"
 	"github.com/docker/notary/trustpinning"
@@ -54,18 +49,12 @@ func bootstrapFromNotary(baseDir, localRepo, gun string) {
 		log.Fatal(err)
 	}
 
-	// Create the transport
-	transport, err := getTransport(baseDir, conf.RemoteServer)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// Safely fetch and validate all TUF metadata from remote Notary server.
 	repo, err := client.NewFileCachedRepository(
 		baseDir,
 		data.GUN(gun),
 		conf.RemoteServer.URL,
-		transport,
+		&http.Transport{},
 		passwordRetriever,
 		trustpinning.TrustPinConfig{},
 	)
@@ -91,45 +80,6 @@ type RemoteServer struct {
 	RootCA     string `json:"root_ca"`
 	ClientCert string `json:"tls_client_cert"`
 	ClientKey  string `json:"tls_client_key"`
-}
-
-func getTransport(baseDir string, svr RemoteServer) (http.RoundTripper, error) {
-	rootCA := filepath.Join(baseDir, svr.RootCA)
-	var tlsConfig tls.Config
-	if svr.RootCA != "" {
-		pool := x509.NewCertPool()
-		pem, err := ioutil.ReadFile(rootCA)
-		if err != nil {
-			return nil, err
-		}
-		if !pool.AppendCertsFromPEM(pem) {
-			return nil, fmt.Errorf("could not append to ca cert pool %q", rootCA)
-		}
-		tlsConfig.RootCAs = pool
-	}
-	if svr.ClientKey != "" && svr.ClientKey != "" {
-		clientCert := filepath.Join(baseDir, svr.ClientCert)
-		clientKey := filepath.Join(baseDir, svr.ClientKey)
-
-		tlsCert, err := tls.LoadX509KeyPair(clientCert, clientKey)
-		if err != nil {
-			return nil, err
-		}
-		tlsConfig.Certificates = []tls.Certificate{tlsCert}
-	}
-	rt := &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   10 * time.Second,
-			KeepAlive: 10 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		TLSHandshakeTimeout: 5 * time.Second,
-		TLSClientConfig:     &tlsConfig,
-		DisableKeepAlives:   true,
-	}
-
-	return rt, nil
 }
 
 func passwordRetriever(key, alias string, createNew bool, attempts int) (pass string, giveUp bool, err error) {
