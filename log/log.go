620ce50790685a4cd372aff42fb35595d45ce1a9
Zachary Wasserman <zachwass2000@gmail.com>
2017-10-25 13:01:00 -0700

    Cleanup + comments

--- log/log.go
+++ log/log.go
@@ -8,12 +8,19 @@ import (
 	"github.com/go-kit/kit/log/level"
 )
 
+// Logger is the interface exposed to users of this launcher logger package.
 type Logger interface {
-	AllowDebug()
-	AllowInfo()
+	// Debug logs keyvals at the "debug" level
 	Debug(keyvals ...interface{}) error
+	// Info logs keyvals at the "info" level
 	Info(keyvals ...interface{}) error
+	// Fatal logs keyvals at the "info" level, and then exits with error code 1.
 	Fatal(keyvals ...interface{}) error
+
+	// AllowDebug sets the allowed log level to include debug logs and above (info).
+	AllowDebug()
+	// AllowInfo sets the allowed log level to include info logs.
+	AllowInfo()
 }
 
 type logger struct {
@@ -51,8 +58,9 @@ func (l *logger) Info(keyvals ...interface{}) error {
 	return level.Info(l.swapLogger).Log(keyvals...)
 }
 
-// Fatal logs at an Info level, and then exits with error code 1.
 func (l *logger) Fatal(keyvals ...interface{}) error {
+	// Call this directly instead of using l.Info so that we get the
+	// correct caller.
 	level.Info(l.swapLogger).Log(keyvals...)
 	os.Exit(1)
 	// never hit
@@ -63,8 +71,10 @@ func NewLogger(w io.Writer) Logger {
 	base := log.NewJSONLogger(log.NewSyncWriter(w))
 	base = log.With(base, "ts", log.DefaultTimestampUTC)
 
-	// Note: the constant in log.Caller is fragile and must be set
-	// appropriately based on the level of wrapping of the lo  gger
+	// The constant in log.Caller is fragile and must be set
+	// appropriately based on the level of wrapping of the logger. If the
+	// wrapping changes and this value becomes set incorrectly, TestCaller
+	// should fail.
 	base = log.With(base, "caller", log.Caller(7))
 
 	l := &logger{
