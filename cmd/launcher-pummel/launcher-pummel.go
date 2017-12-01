35c1c4c8aab46298ebfe8ae6af4318dd3b189036
Zachary Wasserman <zachwass2000@gmail.com>
2017-11-30 18:55:34 -0700

    MVP of working simulator

--- cmd/launcher-pummel/launcher-pummel.go
+++ cmd/launcher-pummel/launcher-pummel.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/go-kit/kit/log/level"
 	"github.com/kolide/kit/env"
 	"github.com/kolide/kit/version"
 	kolidelog "github.com/kolide/launcher/log"
+	"github.com/kolide/launcher/simulator"
 )
 
 func main() {
@@ -17,6 +21,11 @@ func main() {
 			env.Bool("VERSION", false),
 			"Print version and exit",
 		)
+		flHostPath = flag.String(
+			"host_path",
+			env.String("HOST_PATH", "simulator/hosts"),
+			"Directory path for loading host yaml files",
+		)
 	)
 	flag.Parse()
 
@@ -25,7 +34,66 @@ func main() {
 		version.PrintFull()
 		os.Exit(0)
 	}
+
+	hosts, err := simulator.LoadHosts(*flHostPath)
+	if err != nil {
+		level.Info(logger).Log(
+			"msg", "error loading host definitions",
+			"err", err,
+		)
+		os.Exit(1)
+	}
+
 	level.Info(logger).Log(
 		"msg", "starting load testing tool",
 	)
+
+	if len(flag.Args()) == 0 {
+		level.Info(logger).Log("msg", "no hosts specified")
+		os.Exit(1)
+	}
+
+	for _, arg := range flag.Args() {
+		s := strings.Split(arg, ":")
+		if len(s) != 2 {
+			level.Info(logger).Log(
+				"msg", "arguments should be of the form host_type:count",
+				"arg", arg,
+			)
+			os.Exit(1)
+		}
+		hostType, countStr := s[0], s[1]
+
+		count, err := strconv.Atoi(countStr)
+		if err != nil {
+			level.Info(logger).Log(
+				"msg", "unable to parse count",
+				"arg", arg,
+			)
+			os.Exit(1)
+		}
+
+		host, ok := hosts[hostType]
+		if !ok {
+			level.Info(logger).Log(
+				"msg", "unrecognized host type",
+				"type", hostType,
+			)
+			os.Exit(1)
+		}
+
+		// Start hosts
+		for i := 0; i < count; i++ {
+			simulator.LaunchSimulation(
+				host,
+				"localhost:8080",
+				fmt.Sprintf("%s_%d", hostType, i),
+				"kRc8wX1klPTCqcqCKYAcc4lHfkzf51yb",
+				simulator.WithInsecure(),
+			)
+		}
+	}
+
+	sleep := make(chan struct{})
+	<-sleep
 }
--- simulator/simulator.go
+++ simulator/simulator.go
@@ -1,10 +1,21 @@
 package simulator
 
 import (
-	"errors"
+	"context"
+	"crypto/tls"
 	"fmt"
+	"net"
 	"sync"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/go-kit/kit/log"
+	"github.com/kolide/launcher/service"
+	"github.com/kolide/osquery-go/plugin/distributed"
+	"github.com/kolide/osquery-go/plugin/logger"
+	"github.com/pkg/errors"
 )
 
 // QueryRunner is the interface which defines the pluggable behavior of a simulated
@@ -17,11 +28,14 @@ type QueryRunner interface {
 type HostSimulation struct {
 	// the following define the configurable aspects of the simulation
 	host                   QueryRunner
+	uuid                   string
+	enrollSecret           string
+	grpcURL                string
 	insecure               bool
 	insecureGrpc           bool
 	requestQueriesInterval time.Duration
 	requestConfigInterval  time.Duration
-	publishResultsInterval time.Duration
+	publishLogsInterval    time.Duration
 
 	// The state of the simulation is gated with a read/write lock.
 	// To read something in state:
@@ -39,11 +53,100 @@ type HostSimulation struct {
 	done     chan bool
 }
 
+func (h *HostSimulation) Enroll() error {
+	h.state.lock.Lock()
+	defer h.state.lock.Unlock()
+
+	nodeKey, invalid, err := h.state.serviceClient.RequestEnrollment(context.Background(), h.enrollSecret, h.uuid)
+	if err != nil {
+		return errors.Wrap(err, "transport error in enrollment")
+	}
+	if invalid {
+		return errors.New("enrollment invalid")
+	}
+
+	h.state.nodeKey = nodeKey
+	return nil
+}
+
+func (h *HostSimulation) RequestConfig() error {
+	h.state.lock.Lock()
+	defer h.state.lock.Unlock()
+
+	config, invalid, err := h.state.serviceClient.RequestConfig(context.Background(), h.state.nodeKey)
+	if err != nil {
+		return errors.Wrap(err, "transport error retrieving config")
+	}
+	if invalid {
+		return errors.New("enrollment invalid in request config")
+	}
+
+	// Ignore actual config. Someday we may do something with the config.
+	_ = config
+	return nil
+}
+
+func (h *HostSimulation) PublishLogs() error {
+	h.state.lock.Lock()
+	defer h.state.lock.Unlock()
+
+	logs := []string{"foo", "bar", "baz"}
+	_, _, invalid, err := h.state.serviceClient.PublishLogs(context.Background(), h.state.nodeKey, logger.LogTypeStatus, logs)
+	if err != nil {
+		return errors.Wrap(err, "transport error publishing logs")
+	}
+	if invalid {
+		return errors.New("enrollment invalid in publish logs")
+	}
+	return nil
+}
+
+func (h *HostSimulation) RequestQueries() error {
+	h.state.lock.Lock()
+	defer h.state.lock.Unlock()
+
+	queries, invalid, err := h.state.serviceClient.RequestQueries(context.Background(), h.state.nodeKey)
+	if err != nil {
+		return errors.Wrap(err, "transport error requesting queries")
+	}
+	if invalid {
+		return errors.New("enrollment invalid in request queries")
+	}
+
+	if len(queries.Queries) == 0 {
+		// No queries to run
+		return nil
+	}
+
+	results := []distributed.Result{}
+	for name, sql := range queries.Queries {
+		rows, err := h.host.RunQuery(sql)
+		if err != nil {
+			rows = []map[string]string{}
+		}
+		results = append(results,
+			distributed.Result{QueryName: name, Status: 0, Rows: rows},
+		)
+	}
+
+	_, _, invalid, err = h.state.serviceClient.PublishResults(context.Background(), h.state.nodeKey, results)
+	if err != nil {
+		return errors.Wrap(err, "transport error publishing results")
+	}
+	if invalid {
+		return errors.New("enrollment invalid in publish results")
+	}
+
+	return nil
+}
+
 // hostSimulationState is a light container around simulation state management
 type hostSimulationState struct {
-	lock    sync.RWMutex
-	failed  bool
-	started bool
+	lock          sync.RWMutex
+	serviceClient service.KolideService
+	nodeKey       string
+	failed        bool
+	started       bool
 }
 
 // SimulationOption is a functional option pattern for defining how a host
@@ -51,14 +154,6 @@ type hostSimulationState struct {
 // patter, see the following blog post:
 // https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
 type SimulationOption func(*HostSimulation)
-
-// WithQueryRunner is a functional option which allows the user to declare the
-// behavior of the simulated host
-func WithQueryRunner(host QueryRunner) SimulationOption {
-	return func(i *HostSimulation) {
-		i.host = host
-	}
-}
 
 // WithRequestQueriesInterval is a functional option which allows the user to
 // specify how often the simulation host should check-in to the server and ask
@@ -78,11 +173,11 @@ func WithRequestConfigInterval(interval time.Duration) SimulationOption {
 	}
 }
 
-// WithPublishResultsInterval is a functional option which allows the user to
+// WithPublishLogsInterval is a functional option which allows the user to
 // specify how often the simulation host should log status and results logs
-func WithPublishResultsInterval(interval time.Duration) SimulationOption {
+func WithPublishLogsInterval(interval time.Duration) SimulationOption {
 	return func(i *HostSimulation) {
-		i.publishResultsInterval = interval
+		i.publishLogsInterval = interval
 	}
 }
 
@@ -102,13 +197,24 @@ func WithInsecureGrpc() SimulationOption {
 	}
 }
 
+// WithUUID is a functional option that sets the UUID the fake host provides to
+// the server.
+func WithUUID() SimulationOption {
+	return func(i *HostSimulation) {
+		i.insecureGrpc = true
+	}
+}
+
 // createSimulationRuntime is an internal helper which creates an instance of
 // *HostSimulation given a set of supplied functional options
-func createSimulationRuntime(opts ...SimulationOption) *HostSimulation {
+func createSimulationRuntime(host QueryRunner, uuid, enrollSecret string, opts ...SimulationOption) *HostSimulation {
 	h := &HostSimulation{
+		host:                   host,
+		uuid:                   uuid,
+		enrollSecret:           enrollSecret,
 		requestQueriesInterval: 2 * time.Second,
 		requestConfigInterval:  5 * time.Second,
-		publishResultsInterval: 10 * time.Second,
+		publishLogsInterval:    10 * time.Second,
 		shutdown:               make(chan bool),
 		done:                   make(chan bool),
 		state:                  &hostSimulationState{},
@@ -122,27 +228,77 @@ func createSimulationRuntime(opts ...SimulationOption) *HostSimulation {
 
 // LaunchSimulation is a utility which allows the user to configure and run an
 // asynchronous instance of a host simulation given a set of functional options
-func LaunchSimulation(opts ...SimulationOption) *HostSimulation {
-	h := createSimulationRuntime(opts...)
+func LaunchSimulation(host QueryRunner, grpcURL, uuid, enrollSecret string, opts ...SimulationOption) *HostSimulation {
+	h := createSimulationRuntime(host, uuid, enrollSecret, opts...)
+	fmt.Println(h)
 	go func() {
 		h.state.lock.Lock()
 		h.state.started = true
+
+		fmt.Println("in goroutine")
+		grpcOpts := []grpc.DialOption{
+			grpc.WithTimeout(time.Second),
+		}
+		if h.insecureGrpc {
+			grpcOpts = append(grpcOpts, grpc.WithInsecure())
+		} else {
+			host, _, err := net.SplitHostPort(grpcURL)
+			if err != nil {
+				err = errors.Wrapf(err, "split grpc server host and port: %s", grpcURL)
+				h.state.failed = true
+				h.state.lock.Unlock()
+				return
+			}
+			creds := credentials.NewTLS(&tls.Config{
+				ServerName:         host,
+				InsecureSkipVerify: h.insecure,
+			})
+			grpcOpts = append(grpcOpts, grpc.WithTransportCredentials(creds))
+		}
+		conn, err := grpc.Dial(grpcURL, grpcOpts...)
+		if err != nil {
+			fmt.Println(err)
+			h.state.failed = true
+			h.state.lock.Unlock()
+			return
+		}
+		defer conn.Close()
+		fmt.Println("created conn")
+
+		h.state.serviceClient = service.New(conn, log.NewNopLogger())
+
 		h.state.lock.Unlock()
+
+		err = h.Enroll()
+		fmt.Println("enroll", err)
+		if err != nil {
+			h.state.lock.Lock()
+			h.state.failed = true
+			h.state.lock.Unlock()
+			return
+		}
 
 		requestQueriesTicker := time.NewTicker(h.requestQueriesInterval)
 		requestConfigTicker := time.NewTicker(h.requestConfigInterval)
-		publishResultsTicker := time.NewTicker(h.publishResultsInterval)
+		publishLogsTicker := time.NewTicker(h.publishLogsInterval)
 
 		for {
+			var err error
 			select {
 			case <-requestQueriesTicker.C:
-				fmt.Println("requestQueries")
+				err = h.RequestQueries()
 			case <-requestConfigTicker.C:
-				fmt.Println("requestConfig")
-			case <-publishResultsTicker.C:
-				fmt.Println("publishResults")
+				err = h.RequestConfig()
+			case <-publishLogsTicker.C:
+				err = h.PublishLogs()
 			case <-h.shutdown:
 				h.done <- true
+				return
+			}
+			if err != nil {
+				h.state.lock.Lock()
+				h.state.failed = true
+				h.state.lock.Unlock()
 				return
 			}
 		}
