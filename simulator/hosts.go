7fcefca6bff613ef370db2e62fd05ea2d9bcbc21
Zachary Wasserman <zachwass2000@gmail.com>
2017-12-01 14:10:23 -0700

    comments

--- simulator/hosts.go
+++ simulator/hosts.go
@@ -13,14 +13,14 @@ import (
 	"github.com/pkg/errors"
 )
 
-type Host struct {
+type queryRunner struct {
 	// Name of this host type.
 	Name string `json:"name"`
 	// parentName is the name of the parent type.
 	ParentName string `json:"parent"`
 	// parent is a pointer to the parent (nil if no parent) used for query
 	// result inheritance.
-	parent *Host
+	parent *queryRunner
 	//QueryResults maps from regexp pattern to query results that should be
 	//returned.
 	Queries []matcher `json:"queries"`
@@ -31,6 +31,8 @@ type Host struct {
 	unmatchedQueries map[string]bool
 }
 
+// matcher contains a regex for matching input queries, and the results to
+// return if the query matches.
 type matcher struct {
 	// Pattern is a regexp for the query patterns this should match.
 	Pattern regexp.Regexp `json:"pattern"`
@@ -38,7 +40,10 @@ type matcher struct {
 	Results []map[string]string `json:"results"`
 }
 
-type hostYAML struct {
+// querySpec exists for loading from the YAML files. After it is parsed into
+// this structure, a queryRunner is created by compiling the regexes and
+// linking the parents.
+type querySpec struct {
 	Name       string `json:"name"`
 	ParentName string `json:"parent"`
 	Queries    []struct {
@@ -47,13 +52,15 @@ type hostYAML struct {
 	} `json:"queries"`
 }
 
-func LoadHosts(dir string, logger log.Logger) (map[string]*Host, error) {
+// LoadHosts will load the host specifications and return a map of the
+// queryRunners representing these host types.
+func LoadHosts(dir string, logger log.Logger) (map[string]queryRunner, error) {
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		return nil, errors.Wrap(err, "listing hosts directory")
 	}
 
-	hostMap := map[string]*Host{}
+	hostMap := map[string]queryRunner{}
 
 	// Load all files
 	for _, file := range files {
@@ -64,13 +71,13 @@ func LoadHosts(dir string, logger log.Logger) (map[string]*Host, error) {
 				return nil, errors.Wrapf(err, "reading file %s", path)
 			}
 
-			var h hostYAML
+			var h querySpec
 			err = yaml.Unmarshal(contents, &h)
 			if err != nil {
 				return nil, errors.Wrapf(err, "unmarshal yaml for %s", path)
 			}
 
-			host := Host{
+			runner := queryRunner{
 				Name:             h.Name,
 				ParentName:       h.ParentName,
 				Queries:          []matcher{},
@@ -83,29 +90,31 @@ func LoadHosts(dir string, logger log.Logger) (map[string]*Host, error) {
 				if err != nil {
 					return nil, errors.Wrapf(err, "compile regexp for %s", path)
 				}
-				host.Queries = append(host.Queries, matcher{*re, q.Results})
+				runner.Queries = append(runner.Queries, matcher{*re, q.Results})
 			}
 
-			// Check for duplicate host type name
-			if _, exists := hostMap[host.Name]; exists {
-				return nil, errors.Errorf("duplicate host type: %s", host.Name)
+			// Check for duplicate host type name. It is user error
+			// to provide multiple definitions for the same host
+			// type.
+			if _, exists := hostMap[runner.Name]; exists {
+				return nil, errors.Errorf("duplicate host type: %s", runner.Name)
 			}
 
-			hostMap[host.Name] = &host
+			hostMap[runner.Name] = runner
 		}
 	}
 
 	// Link parents
-	for _, host := range hostMap {
-		if host.ParentName == "" {
+	for _, runner := range hostMap {
+		if runner.ParentName == "" {
 			continue
 		}
 
-		parent, ok := hostMap[host.ParentName]
+		parent, ok := hostMap[runner.ParentName]
 		if !ok {
-			return nil, errors.Errorf("missing parent named: %s", host.ParentName)
+			return nil, errors.Errorf("missing parent named: %s", runner.ParentName)
 		}
-		host.parent = parent
+		runner.parent = &parent
 	}
 
 	// TODO check for cycles
@@ -113,7 +122,7 @@ func LoadHosts(dir string, logger log.Logger) (map[string]*Host, error) {
 	return hostMap, nil
 }
 
-func (h *Host) RunQuery(sql string) (rows []map[string]string, err error) {
+func (h *queryRunner) RunQuery(sql string) (rows []map[string]string, err error) {
 	sql = strings.ToLower(sql)
 	defer func() {
 		if err == nil {
@@ -140,7 +149,7 @@ func (h *Host) RunQuery(sql string) (rows []map[string]string, err error) {
 	return h.runQueryRecurse(sql)
 }
 
-func (h *Host) runQueryRecurse(sql string) ([]map[string]string, error) {
+func (h *queryRunner) runQueryRecurse(sql string) ([]map[string]string, error) {
 	// Try matching patterns
 	for _, q := range h.Queries {
 		if q.Pattern.MatchString(sql) {
