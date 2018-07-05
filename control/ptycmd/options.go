952db6bce5bfd8aabceee8a67c6698e76a9bc613
Mike Arpaia <mike@arpaia.co>
2018-07-05 13:59:59 -0600

    Add Logan's broker code

---
+++ control/ptycmd/options.go
@@ -0,0 +1,25 @@
+package ptycmd
+
+import (
+	"syscall"
+	"time"
+)
+
+// Option is a configuration option for a Cmd
+type Option func(*Cmd)
+
+// WithCloseSignal lets you specify the signal to send to the
+// underlying process on close
+func WithCloseSignal(signal syscall.Signal) Option {
+	return func(c *Cmd) {
+		c.closeSignal = signal
+	}
+}
+
+// WithCloseTimeout lets you specify how long to wait for the
+// underlying process to terminate before sending a SIGKILL
+func WithCloseTimeout(timeout time.Duration) Option {
+	return func(c *Cmd) {
+		c.closeTimeout = timeout
+	}
+}
