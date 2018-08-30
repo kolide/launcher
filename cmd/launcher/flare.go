30a2a03c2147513d4c51ebd92fdc31090c4354d3
Victor Vrantchan <vrancean+github@gmail.com>
2018-08-30 19:33:17 -0400

    work on tar file

--- cmd/launcher/flare.go
+++ cmd/launcher/flare.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"archive/tar"
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 	"os/user"
+	"time"
 
 	"github.com/kolide/kit/ulid"
+	"github.com/kolide/kit/version"
 )
 
 func runFlare(args []string) error {
@@ -19,16 +23,66 @@ func runFlare(args []string) error {
 		return err
 	}
 
-	b := new(bytes.Buffer)
 	id := ulid.New()
+	b := new(bytes.Buffer)
+	reportName := fmt.Sprintf("kolide_launcher_flare_report_%s", id)
+	tarOut, err := os.Create(fmt.Sprintf("%s.tar.gz", reportName))
+	if err != nil {
+		fatal(b, err)
+	}
+	defer func() {
+		if err := tarOut.Close(); err != nil {
+			fatal(b, err)
+		}
+	}()
+
+	tw := tar.NewWriter(tarOut)
+
+	// create directory at root of tar file
+	hdr := &tar.Header{
+		Name:     fmt.Sprintf("%s_", reportName),
+		Mode:     int64(os.ModeDir),
+		ModTime:  time.Now().UTC(),
+		Typeflag: tar.TypeDir,
+	}
+
+	if err := tw.WriteHeader(hdr); err != nil {
+		fatal(b, err)
+	}
+
+	defer func() {
+		hdr := &tar.Header{
+			Name: fmt.Sprintf("%s_%s.log", reportName, id),
+			Mode: int64(os.ModePerm),
+			Size: int64(b.Len()),
+		}
+
+		if err := tw.WriteHeader(hdr); err != nil {
+			fatal(b, err)
+		}
+
+		if _, err := tw.Write(b.Bytes()); err != nil {
+			fatal(b, err)
+		}
+
+		if err := tw.Close(); err != nil {
+			fatal(b, err)
+		}
+	}()
 
 	output(b, stdout, "Starting Launcher Diagnostics\n")
-	output(b, stdout, "\tID: %s\n", id)
+	output(b, stdout, "ID: %s\n", id)
 	user, err := user.Current()
 	if err != nil {
 		fatal(b, err)
 	}
-	output(b, stdout, "\tCurrentUser: %s uid: %s\n", user.Username, user.Uid)
+	output(b, stdout, "CurrentUser: %s uid: %s\n", user.Username, user.Uid)
+	v := version.Version()
+	jsonVersion, err := json.Marshal(&v)
+	if err != nil {
+		fatal(b, err)
+	}
+	output(b, stdout, string(jsonVersion))
 
 	return nil
 }
