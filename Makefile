5d96a381cfe2633de56e7646f35107a694325554
Victor Vrantchan <groob@users.noreply.github.com>
2018-12-05 13:02:45 -0500

    move go build targets to make.go (#385)
    Moving the build targets to make.go further simplifies the bandaids that would otherwise be necessary to create builds on both unix and windows. This particular PR was driven by the desire to have clean outputs for binary names (launcher vs launcher.exe and extension.ext vs extension.exe) but the move to Go also allowed for consistency in various build targets.

--- Makefile
+++ Makefile
@@ -7,45 +7,18 @@ ifndef ($(GOPATH))
 endif
 
 PATH := $(GOPATH)/bin:$(PATH)
-VERSION = $(shell git describe --tags --always --dirty)
-BRANCH = $(shell git rev-parse --abbrev-ref HEAD)
-REVISION = $(shell git rev-parse HEAD)
-REVSHORT = $(shell git rev-parse --short HEAD)
-USER = $(shell whoami)
 
 export GO111MODULE=on
 
-KIT_VERSION = "\
-	-X github.com/kolide/kit/version.appName=${APP_NAME} \
-	-X github.com/kolide/kit/version.version=${VERSION} \
-	-X github.com/kolide/kit/version.branch=${BRANCH} \
-	-X github.com/kolide/kit/version.revision=${REVISION} \
-	-X github.com/kolide/kit/version.buildDate=${NOW} \
-	-X github.com/kolide/kit/version.buildUser=${USER} \
-	-X github.com/kolide/kit/version.goVersion=${GOVERSION}"
-
 ifneq ($(OS), Windows_NT)
-	CURRENT_PLATFORM = linux
-
 	# If on macOS, set the shell to bash explicitly
 	ifeq ($(shell uname), Darwin)
 		SHELL := /bin/bash
-		CURRENT_PLATFORM = darwin
 	endif
-
-	# To populate version metadata, we use unix tools to get certain data
-	GOVERSION = $(shell go version | awk '{print $$3}')
-	NOW	= $(shell date -u +"%Y-%m-%dT%H:%M:%SZ")
-else
-	CURRENT_PLATFORM = windows
-	# To populate version metadata, we use windows tools to get the certain data
-	GOVERSION_CMD = "(go version).Split()[2]"
-	GOVERSION = $(shell powershell $(GOVERSION_CMD))
-	NOW	= $(shell powershell Get-Date -format s)
 endif
 
+all: build
 build: launcher extension
-
 .pre-build: ${BUILD_DIR}
 
 ${BUILD_DIR}:
@@ -55,48 +28,35 @@ else
 	mkdir -p ${BUILD_DIR}
 endif
 
-extension: .pre-build
-	go build -o build/osquery-extension.ext ./cmd/osquery-extension/
+launcher: .pre-build
+	go run cmd/make/make.go -targets=launcher -linkstamp
 
-osqueryi: .pre-build
-	go build -o build/launcher.ext ./cmd/launcher.ext/
-	osqueryi --extension=./build/launcher.ext
+extension: .pre-build
+	go run cmd/make/make.go -targets=extension
+
+xp: xp-launcher xp-extension
+
+xp-launcher: .pre-build
+	go run cmd/make/make.go -targets=launcher -linkstamp -os=darwin
+	go run cmd/make/make.go -targets=launcher -linkstamp -os=linux
+	go run cmd/make/make.go -targets=launcher -linkstamp -os=windows
 
 xp-extension: .pre-build
-	GOOS=darwin go build -o build/darwin/osquery-extension.ext ./cmd/osquery-extension/
-	GOOS=linux CGO_ENABLED=0 go build -o build/linux/osquery-extension.ext ./cmd/osquery-extension/
-	ln -f build/$(CURRENT_PLATFORM)/osquery-extension.ext build/osquery-extension.ext
-
-.pre-launcher:
-	$(eval APP_NAME = launcher)
-
-launcher: .pre-build .pre-launcher
-	go build -o build/launcher -ldflags ${KIT_VERSION} ./cmd/launcher/
+	go run cmd/make/make.go -targets=extension -os=darwin
+	go run cmd/make/make.go -targets=extension -os=linux
+	go run cmd/make/make.go -targets=extension -os=windows
 
 codesign-darwin:
 	codesign --force -s "${CODESIGN_IDENTITY}" -v ./build/darwin/launcher
 	codesign --force -s "${CODESIGN_IDENTITY}" -v ./build/darwin/osquery-extension.ext
 
-xp: xp-launcher xp-extension
-
 xp-codesign: xp codesign-darwin
 
-xp-launcher: .pre-build .pre-launcher
-	GOOS=darwin go build -o build/darwin/launcher -ldflags ${KIT_VERSION} ./cmd/launcher/
-	GOOS=linux CGO_ENABLED=0 go build -o build/linux/launcher -ldflags ${KIT_VERSION} ./cmd/launcher/
-	ln -f build/$(CURRENT_PLATFORM)/launcher build/launcher
-
-.pre-package-builder:
-	$(eval APP_NAME = package-builder)
-
-package-builder: .pre-build xp-codesign .pre-package-builder generate
-	go build -o build/package-builder -ldflags ${KIT_VERSION} ./cmd/package-builder/
-
-.pre-launcher-pummel:
-	$(eval APP_NAME = launcher-pummel)
+package-builder: .pre-build xp-codesign generate
+	go run cmd/make/make.go -targets=package-builder -linkstamp
 
 launcher-pummel:
-	go build -o build/launcher-pummel -ldflags ${KIT_VERSION} ./cmd/launcher-pummel/
+	go run cmd/make/make.go -targets=launcher-pummel
 
 deps-go: 
 	go run cmd/make/make.go -targets=deps-go,install-tools
@@ -110,31 +70,8 @@ proto:
 	@(cd pkg/pb/launcher; go generate)
 	@echo "Generated code from proto definitions."
 
-# Publishes osqueryd for autoupdate. NOTARY_DELEGATE_PASSPHRASE must be set
-# and the delegate key must be imported by Notary client.
-publish-osquery: package-builder
-	./build/package-builder mirror -osquery-all -platform darwin
-	./build/package-builder mirror -osquery-all -platform linux
-
-# Publishes launcher for autoupdate. NOTARY_DELEGATE_PASSPHRASE must be set
-# and the delegate key must be imported by Notary client.
-publish-launcher: package-builder
-	./build/package-builder mirror -launcher-all -platform darwin
-	./build/package-builder mirror -launcher-all -platform linux
-
-# Publishes launcher and osqueryd for autoupdate. NOTARY_DELEGATE_PASSPHRASE must be set
-# and the delegate key must be imported by Notary client.
-publish: package-builder
-	./build/package-builder mirror -all -platform darwin
-	./build/package-builder mirror -all -platform linux
-
 test: generate
 	go test -cover -race -v $(shell go list ./... | grep -v /vendor/)
-
-install: build
-	mkdir -p $(GOPATH)/bin
-	cp ./build/launcher $(GOPATH)/bin/launcher
-	cp ./build/osquery-extension.ext $(GOPATH)/bin/osquery-extension.ext
 
 CONTAINERS = ubuntu14 ubuntu16 centos6 centos7
 
--- cmd/make/make.go
+++ cmd/make/make.go
@@ -9,10 +9,12 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/user"
 	"path"
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/Masterminds/semver"
 	"github.com/go-kit/kit/log"
@@ -33,17 +35,34 @@ func main() {
 	}, ",")
 
 	var (
-		flTargets = flag.String("targets", buildAll, "comma separated list of targets")
-		flDebug   = flag.Bool("debug", false, "use a debug logger")
+		flTargets      = flag.String("targets", buildAll, "comma separated list of targets")
+		flDebug        = flag.Bool("debug", false, "use a debug logger")
+		flBuildARCH    = flag.String("arch", runtime.GOARCH, "Architecture to build for.")
+		flBuildOS      = flag.String("os", runtime.GOOS, "Operating system to build for.")
+		flRace         = flag.Bool("race", false, "Build race-detector version of binaries.")
+		flStatic       = flag.Bool("static", false, "Build a static binary.")
+		flStampVersion = flag.Bool("linkstamp", false, "Add version info with ldflags.")
 	)
 	flag.Parse()
 
 	logger := logutil.NewCLILogger(*flDebug)
-	b := builder{logger: logger}
+	b := builder{
+		logger:       logger,
+		os:           *flBuildOS,
+		arch:         *flBuildARCH,
+		race:         *flRace,
+		static:       *flStatic,
+		stampVersion: *flStampVersion,
+	}
 	targetSet := map[string]func() error{
-		"deps-go":       b.depsGo,
-		"install-tools": b.installTools,
-		"generate-tuf":  b.generateTUF,
+		"deps-go":         b.depsGo,
+		"install-tools":   b.installTools,
+		"generate-tuf":    b.generateTUF,
+		"launcher":        b.buildCmd("./cmd/launcher", b.platformBinary("launcher")),
+		"extension":       b.buildCmd("./cmd/osquery-extension", b.extBinary("osquery-extension")),
+		"table-extension": b.buildCmd("./cmd/launcher.ext", b.extBinary("tables")),
+		"grpc-extension":  b.buildCmd("./cmd/grpc.ext", b.extBinary("grpc")),
+		"package-builder": b.buildCmd("./cmd/package-builder", b.platformBinary("package-builder")),
 	}
 
 	if t := strings.Split(*flTargets, ","); len(t) != 0 && t[0] != "" {
@@ -59,7 +78,29 @@ func main() {
 }
 
 type builder struct {
-	logger log.Logger
+	logger       log.Logger
+	os           string
+	arch         string
+	static       bool
+	race         bool
+	stampVersion bool
+}
+
+func (b *builder) extBinary(input string) string {
+	input = filepath.Join("build", b.os, input)
+	if b.os == "windows" {
+		return input + ".exe"
+	} else {
+		return input + ".ext"
+	}
+}
+
+func (b *builder) platformBinary(input string) string {
+	input = filepath.Join("build", b.os, input)
+	if b.os == "windows" {
+		return input + ".exe"
+	}
+	return input
 }
 
 func (b *builder) goVersionCompatible() error {
@@ -242,4 +283,91 @@ func bootstrapFromNotary(notaryConfigDir, remoteServerURL, localRepo, gun string
 	}
 
 	return nil
+}
+
+func (b *builder) buildCmd(src, output string) func() error {
+	return func() error {
+		_, appName := filepath.Split(output)
+		baseArgs := []string{"build", "-o", output}
+		if b.race {
+			baseArgs = append(baseArgs, "-race")
+		}
+		var ldFlags []string
+		if b.static {
+			ldFlags = append(ldFlags, "-w -d -linkmode internal")
+		}
+		if b.stampVersion {
+			v, err := execOut("git", "describe", "--tags", "--always", "--dirty")
+			if err != nil {
+				return err
+			}
+
+			branch, err := execOut("git", "rev-parse", "--abbrev-ref", "HEAD")
+			if err != nil {
+				return err
+			}
+
+			revision, err := execOut("git", "rev-parse", "HEAD")
+			if err != nil {
+				return err
+			}
+
+			usr, err := user.Current()
+			if err != nil {
+				return err
+			}
+
+			ldFlags = append(ldFlags, fmt.Sprintf(`-X "github.com/kolide/kit/version.appName=%s"`, appName))
+			ldFlags = append(ldFlags, fmt.Sprintf(`-X "github.com/kolide/kit/version.version=%s"`, v))
+			ldFlags = append(ldFlags, fmt.Sprintf(`-X "github.com/kolide/kit/version.branch=%s"`, branch))
+			ldFlags = append(ldFlags, fmt.Sprintf(`-X "github.com/kolide/kit/version.revision=%s"`, revision))
+			ldFlags = append(ldFlags, fmt.Sprintf(`-X "github.com/kolide/kit/version.buildDate=%s"`, time.Now().UTC().Format("2006-01-02")))
+			ldFlags = append(ldFlags, fmt.Sprintf(`-X "github.com/kolide/kit/version.buildUser=%s (%s)"`, usr.Name, usr.Username))
+			ldFlags = append(ldFlags, fmt.Sprintf(`-X "github.com/kolide/kit/version.goVersion=%s"`, runtime.Version()))
+		}
+		if len(ldFlags) != 0 {
+			baseArgs = append(baseArgs, fmt.Sprintf("--ldflags=%s", strings.Join(ldFlags, " ")))
+		}
+		args := append(baseArgs, src)
+
+		level.Debug(b.logger).Log("mgs", "building binary", "app_name", appName, "output", output, "go_args", strings.Join(args, "  "))
+
+		cmd := exec.Command("go", args...)
+		cmd.Env = os.Environ()
+		cmd.Env = append(cmd.Env, "GO111MODULE=on")
+		cmd.Env = append(cmd.Env, fmt.Sprintf("GOOS=%s", b.os))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("GOARCH=%s", b.arch))
+		if b.static {
+			cmd.Env = append(cmd.Env, "CGO_ENABLED=0")
+		}
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+
+		// all the builds go to `build/<os>/binary`, but if the build OS is the same as the target OS,
+		// we also want to hardlink the resulting binary at the root of `build/` for convenience.
+		// ex: running ./build/launcher on macos instead of ./build/darwin/launcher
+		if b.os == runtime.GOOS {
+			platformPath := filepath.Join("build", appName)
+			if err := os.Remove(platformPath); err != nil {
+				// log but don't fail. This could happen if for example ./build/launcher.exe is referenced by a running service.
+				// if this becomes clearer, we can either return an error here, or go back to silently ignoring.
+				level.Debug(b.logger).Log("msg", "remove before hardlink failed", "err", err, "app_name", appName)
+			}
+			return os.Link(output, platformPath)
+		}
+		return nil
+	}
+}
+
+func execOut(argv0 string, args ...string) (string, error) {
+	cmd := exec.Command(argv0, args...)
+	stdout, stderr := new(bytes.Buffer), new(bytes.Buffer)
+	cmd.Stdout, cmd.Stderr = stdout, stderr
+	if err := cmd.Run(); err != nil {
+		return "", errors.Wrapf(err, "run command %s %v, stderr=%s", argv0, args, stderr)
+	}
+	return strings.TrimSpace(stdout.String()), nil
 }
