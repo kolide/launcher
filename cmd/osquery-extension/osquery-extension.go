c2e2e92ab637ea29af17308a33fe924ae7372073
Victor Vrantchan <groob@users.noreply.github.com>
2019-01-16 11:13:30 -0500

    add self clean-up to extension process. (#342)

--- cmd/osquery-extension/osquery-extension.go
+++ cmd/osquery-extension/osquery-extension.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"time"
 )
 
 func main() {
@@ -16,7 +17,33 @@ func main() {
 
 	fmt.Fprintf(os.Stderr, "%+v", os.Args)
 
+	go monitorForParent()
+
 	sig := make(chan os.Signal)
 	signal.Notify(sig, os.Interrupt)
 	<-sig
+}
+
+// continuously monitor for ppid and exit if osqueryd is no longer the parent process.
+// because osqueryd is always the process starting the extension, when osqueryd is killed this process should also be cleaned up.
+// sometimes the termination is not clean, causing this process to remain running, which sometimes prevents osqueryd from properly restarting.
+// https://github.com/kolide/launcher/issues/341
+func monitorForParent() {
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	f := func() {
+		ppid := os.Getppid()
+		if ppid <= 1 {
+			fmt.Println("extension process no longer owned by osqueryd, quitting")
+			os.Exit(1)
+		}
+	}
+
+	f()
+
+	select {
+	case <-ticker.C:
+		f()
+	}
 }
