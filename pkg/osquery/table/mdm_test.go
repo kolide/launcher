67ac92ab7d3a509e6f6bdb1866d4f57f8f5b06af
seph <seph@directionless.org>
2019-02-12 22:46:54 -0500

    Skip MDM tests on travis

--- pkg/osquery/table/mdm_test.go
+++ pkg/osquery/table/mdm_test.go
@@ -5,10 +5,15 @@ package table
 import (
 	"testing"
 
+	"github.com/kolide/kit/env"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMDMProfileStatus(t *testing.T) {
+	if env.Bool("SKIP_TEST_MDM", true) {
+		t.Skip("No docker")
+	}
+
 	_, err := getMDMProfileStatus()
 	require.Nil(t, err)
 }
