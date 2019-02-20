0d9992d2e150e85487c556c8b466f61c3a595d31
blaedj <blaedj@users.noreply.github.com>
2019-02-20 16:08:58 -0500

    Update cmd/launcher/options.go
    Co-Authored-By: directionless <github@directionless.org>

--- cmd/launcher/options.go
+++ cmd/launcher/options.go
@@ -62,7 +62,7 @@ func parseOptions(args []string) (*options, error) {
 		flControlServerURL  = flagset.String("control_hostname", "", "The hostname of the control server")
 		flEnrollSecret      = flagset.String("enroll_secret", "", "The enroll secret that is used in your environment")
 		flEnrollSecretPath  = flagset.String("enroll_secret_path", "", "Optionally, the path to your enrollment secret")
-		flGetShellsInterval = flagset.Duration("control_get_shells_interval", 3*time.Second, "The interval at which the get shells request will be made")
+		flGetShellsInterval = flagset.Duration("control_get_shells_interval", 3*time.Second, "The interval at which the 'get shells' request will be made")
 		flInitialRunner     = flagset.Bool("with_initial_runner", false, "Run differential queries from config ahead of scheduled interval.")
 		flKolideServerURL   = flagset.String("hostname", "", "The hostname of the gRPC server")
 		flLoggingInterval   = flagset.Duration("logging_interval", 60*time.Second, "The interval at which logs should be flushed to the server")
