a5216ef6b72639a8303bbf84bcf08f4a4e6d04b2
Jonathan Nogueira <jonathan@kolide.co>
2019-03-13 15:41:14 -0400

    tide things up
    This commit:
     - Moved Info.plist parsing to objective for better support
     - Removed debugging errors in icon column when an icon could not be encoded/found
     - Passed queryContext to apps query

--- pkg/osquery/table/app_icons_darwin.go
+++ pkg/osquery/table/app_icons_darwin.go
@@ -6,27 +6,23 @@ package table
 #include <CoreFoundation/CoreFoundation.h>
 #import <Collaboration/Collaboration.h>
 void Icon(CFDataRef *iconDataRef, char* path) {
-NSString *iconPath = [[NSString alloc] initWithUTF8String:path];
+	NSString *appPath = [[NSString alloc] initWithUTF8String:path];
+	// nil if path does not identify an accessible bundle directory
+	NSBundle *appBundle = [NSBundle bundleWithPath:appPath];
+	if (appBundle == nil){
+		return;
+	}
+	NSString *iconFilename = [appBundle objectForInfoDictionaryKey:@"CFBundleIconFile"];
+	NSString *iconBasename = [iconFilename stringByDeletingPathExtension];
+	NSString *iconExtension = [iconFilename pathExtension];
+	NSString *iconPath = [appBundle pathForResource:iconBasename ofType:@"icns"];
 
-if (![[NSFileManager defaultManager] fileExistsAtPath:iconPath]) {
- return;
-}
-
-NSImage *img = [[NSImage alloc] initWithContentsOfFile:iconPath];
-NSArray *reps = [img representations];
-NSBitmapImageRep *largestRep;
-
-for (NSImageRep *rep in reps) {
-	if (![rep isKindOfClass:[NSBitmapImageRep class]]) {
-  	continue;
-  }
- NSBitmapImageRep *brep = (NSBitmapImageRep *)rep;
- if ([brep pixelsWide] >= 128) {
- 	NSData *imageData = [brep TIFFRepresentation];
+	NSImage *img = [[NSImage alloc] initWithContentsOfFile:iconPath];
+	//request 512x512 since we are going to resize the icon
+	NSRect targetFrame = NSMakeRect(0, 0, 512, 512);
+	NSBitmapImageRep *brep = (NSBitmapImageRep *)[img bestRepresentationForRect:targetFrame context:nil hints:nil];
+	NSData *imageData = [brep TIFFRepresentation];
 	*iconDataRef = (CFDataRef)imageData;
-	break;
- }
-}
 }
 */
 import "C"
@@ -37,12 +33,9 @@ import (
 	"fmt"
 	"image"
 	"image/png"
-	"os"
-	"path"
 	"unsafe"
 
 	"github.com/go-kit/kit/log"
-	"github.com/groob/plist"
 	osquery "github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 	"github.com/nfnt/resize"
@@ -64,75 +57,58 @@ type appIconsTable struct {
 	apps   []map[string]string
 }
 
-type infoPlist struct {
-	CFBundleIconFile string
-}
-
 func (t *appIconsTable) generateAppIcons(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
 	var results []map[string]string
-	apps, err := collectApps(t.client)
+	apps, err := collectApps(t.client, queryContext)
 	if err != nil {
-		return nil, errors.Wrap(err, "collecting apps")
+		return nil, errors.Wrap(err, "collecting installed apps")
 	}
+
 	for _, app := range apps {
-		img, err := appIcnsToPng(app["path"])
+		img, err := getAppIcon(app["path"])
 		if err != nil {
 			results = append(results, map[string]string{
 				"path": app["path"],
-				"icon": errors.Wrap(err, "missing").Error(),
 			})
-			continue
+		} else {
+			buf := new(bytes.Buffer)
+			img = resize.Resize(128, 128, img, resize.Bilinear)
+			png.Encode(buf, img)
+			results = append(results, map[string]string{
+				"path": app["path"],
+				"icon": base64.StdEncoding.EncodeToString(buf.Bytes()),
+			})
 		}
-		buf := new(bytes.Buffer)
-		img = resize.Resize(128, 128, img, resize.Bilinear)
-		png.Encode(buf, img)
-		results = append(results, map[string]string{
-			"path": app["path"],
-			"icon": base64.StdEncoding.EncodeToString(buf.Bytes()),
-		})
 	}
 
 	return results, nil
 }
 
-func appIcnsToPng(appPath string) (image.Image, error) {
-	file, err := os.Open(path.Join(appPath, "/Contents/Info.plist"))
-	defer file.Close()
-	if err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("opening %s Info.plist", appPath))
-	}
-	var appInfoPlist infoPlist
-	if err := plist.NewDecoder(file).Decode(&appInfoPlist); err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("decoding %s Info.plist", appPath))
-	}
-	pathStr := path.Join(appPath, "Contents/Resources", appInfoPlist.CFBundleIconFile)
-	if path.Ext(pathStr) == "" {
-		pathStr = pathStr + ".icns"
-	}
-	if _, err := os.Stat(pathStr); os.IsNotExist(err) {
-		return nil, errors.Wrap(err, fmt.Sprintf("icns file doesn't exist"))
-	}
-
+func getAppIcon(appPath string) (image.Image, error) {
 	var data C.CFDataRef = 0
-	C.Icon(&data, C.CString(pathStr))
+	C.Icon(&data, C.CString(appPath))
 	if data == 0 {
-		return nil, errors.New("no icon found")
+		return nil, errors.Errorf("no icon image for app %s", appPath)
 	}
 	defer C.CFRelease(C.CFTypeRef(data))
+
 	tiffBytes := C.GoBytes(unsafe.Pointer(C.CFDataGetBytePtr(data)), C.int(C.CFDataGetLength(data)))
 	if len(tiffBytes) == 0 {
 		return nil, errors.Errorf("no icon image for app %s", appPath)
 	}
 	img, err := tiff.Decode(bytes.NewBuffer(tiffBytes))
 	if err != nil {
-		return nil, errors.Wrap(err, "decoding tiff image from C")
+		return nil, errors.Wrap(err, "decoding tiff bytes")
 	}
 
 	return img, nil
 }
 
-func collectApps(client *osquery.ExtensionManagerClient) ([]map[string]string, error) {
-	query := `select name, path from apps`
+func collectApps(client *osquery.ExtensionManagerClient, queryContext table.QueryContext) ([]map[string]string, error) {
+	query := "select name, path from apps"
+	if q, ok := queryContext.Constraints["path"]; ok && len(q.Constraints) != 0 {
+		query = fmt.Sprintf("%s where path='%s'", query, q.Constraints[0].Expression)
+	}
 	rows, err := client.QueryRows(query)
 	if err != nil {
 		return nil, errors.Wrap(err, "querying for apps")
