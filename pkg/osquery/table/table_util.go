7b03adc48dd13996f4417d08407dbc20450866ef
seph <seph@directionless.org>
2019-03-15 16:32:10 -0400

    Make username a functional argument

--- pkg/osquery/table/table_util.go
+++ pkg/osquery/table/table_util.go
@@ -9,6 +9,18 @@ import (
 	"github.com/pkg/errors"
 )
 
+type findFile struct {
+	username string
+}
+
+type FindFileOpt func(*findFile)
+
+func WithUsername(username string) FindFileOpt {
+	return func(ff *findFile) {
+		ff.username = username
+	}
+}
+
 var homedirLocations = map[string][]string{
 	"windows": {"/Users"}, // windows10 uses /Users
 	"darwin":  {"/Users"},
@@ -18,7 +30,13 @@ var homedirLocations = map[string][]string{
 // findFileInUserDirs looks for the existence of a specified path as a
 // subdirectory of users' home directories. It does this by searching
 // likely paths
-func findFileInUserDirs(path string, user string) ([]string, error) {
+func findFileInUserDirs(path string, opts ...FindFileOpt) ([]string, error) {
+	ff := &findFile{}
+
+	for _, opt := range opts {
+		opt(ff)
+	}
+
 	var foundDirs []string
 
 	homedirRoots, ok := homedirLocations[runtime.GOOS]
@@ -29,20 +47,37 @@ func findFileInUserDirs(path string, user string) ([]string, error) {
 		}
 	}
 
-	for _, userDir := range homedirRoots {
+	foundPaths := []string{}
 
-		userDirs, err := ioutil.ReadDir(userDir)
-		if err != nil {
-			continue
-		}
+	// Redo/remove when we make username a required parameter
+	if ff.username == "" {
+		for _, possibleHome := range homedirRoots {
 
-		for _, dir := range userDirs {
-			fullPath := filepath.Join(userDir, dir.Name(), path)
-			if stat, err := os.Stat(fullPath); err == nil && stat.Mode().IsRegular() {
-				foundDirs = append(foundDirs, fullPath)
+			userDirs, err := ioutil.ReadDir(possibleHome)
+			if err != nil {
+				// This possibleHome doesn't exist. Move on
+				continue
+			}
+
+			// For each user's dir, in this possibleHome, check!
+			for _, ud := range userDirs {
+				fullPath := filepath.Join(possibleHome, ud.Name(), path)
+				if stat, err := os.Stat(fullPath); err == nil && stat.Mode().IsRegular() {
+					foundPaths = append(foundPaths, fullPath)
+				}
 			}
 		}
+
+		return foundPaths, nil
 	}
 
-	return foundDirs, nil
+	// We have a username. Future normal path here
+	for _, possibleHome := range homedirRoots {
+		fullPath := filepath.Join(possibleHome, ff.username, path)
+		if stat, err := os.Stat(fullPath); err == nil && stat.Mode().IsRegular() {
+			foundPaths = append(foundPaths, fullPath)
+		}
+	}
+	return foundPaths, nil
+
 }
