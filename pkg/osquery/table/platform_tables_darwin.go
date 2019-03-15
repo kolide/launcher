62040430c281401a7443f72fce3ed90eedf386ca
Jonathan Nogueira <jonathan.nogueira001@gmail.com>
2019-03-15 13:03:27 -0400

    user avatar table (#448)
    Implements #360
    Returns a base64 encoded PNG thumbnail for the user.
    Not sure if it works outside the user context yet. Was easier to build first and then test.

--- pkg/osquery/table/platform_tables_darwin.go
+++ pkg/osquery/table/platform_tables_darwin.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/knightsc/system_policy/osquery/table/kextpolicy"
 	"github.com/knightsc/system_policy/osquery/table/legacyexec"
-	"github.com/kolide/osquery-go"
+	osquery "github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -30,6 +30,7 @@ func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger) [
 		munki.ManagedInstalls(client, logger),
 		munki.MunkiReport(client, logger),
 		Spotlight(),
+		UserAvatar(logger),
 		legacyexec.TablePlugin(),
 		kextpolicy.TablePlugin(),
 	}
