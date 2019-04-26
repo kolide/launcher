8adc5c52f65e20169278e29f7b31978686cf91da
seph <seph@directionless.org>
2019-04-25 23:49:55 -0400

    Tables

--- pkg/osquery/table/platform_tables.go
+++ pkg/osquery/table/platform_tables.go
@@ -2,7 +2,6 @@ package table
 
 import (
 	"github.com/go-kit/kit/log"
-	"github.com/kolide/launcher/pkg/osquery/tables/sshtable"
 	"github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 )
@@ -10,12 +9,13 @@ import (
 func platformTablesCommon(client *osquery.ExtensionManagerClient, logger log.Logger) []*table.Plugin {
 	return []*table.Plugin{
 		BestPractices(client),
-		sshtable.New(client, logger),
 		ChromeLoginDataEmails(client, logger),
 		ChromeUserProfiles(client, logger),
 		EmailAddresses(client, logger),
+		KeyInfo(client, logger),
 		LauncherInfoTable(),
 		OnePasswordAccounts(client, logger),
 		SlackConfig(client, logger),
+		SshKeys(client, logger),
 	}
 }
