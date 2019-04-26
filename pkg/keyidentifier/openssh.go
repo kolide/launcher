f34eb3ca934585879142568fec557c0c7f7336e4
seph <seph@directionless.org>
2019-04-26 12:25:54 -0400

    Keyidentifier
    This adds a key identifier

---
+++ pkg/keyidentifier/openssh.go
@@ -0,0 +1,58 @@
+package keyidentifier
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/ssh"
+)
+
+const opensshMagic = "openssh-key-v1\x00"
+
+// ParseOpenSSHPrivateKey returtns key informatin from an openssh
+// private key. It is adapted from
+// https://github.com/golang/crypto/blob/master/ssh/keys.go
+func ParseOpenSSHPrivateKey(keyBytes []byte) (*KeyInfo, error) {
+
+	if !bytes.HasPrefix(keyBytes, []byte(opensshMagic)) {
+		return nil, errors.New("missing openssh magic")
+	}
+	remaining := keyBytes[len([]byte(opensshMagic)):]
+
+	var w struct {
+		CipherName   string
+		KdfName      string
+		KdfOpts      string
+		NumKeys      uint32
+		PubKey       []byte
+		PrivKeyBlock []byte
+	}
+
+	if err := ssh.Unmarshal(remaining, &w); err != nil {
+		return nil, errors.Wrap(err, "ssh.Unmarshal")
+	}
+
+	ki := &KeyInfo{
+		Format: "openssh-new",
+		Parser: "ParseOpenSSHPrivateKey",
+	}
+
+	if w.KdfName != "none" || w.CipherName != "none" {
+		ki.Encrypted = truePtr()
+		ki.Encryption = fmt.Sprintf("%s-%s", w.CipherName, w.KdfName)
+	} else {
+		ki.Encrypted = falsePtr()
+	}
+
+	// If we can parse the public key. extract info
+	if pubKey, err := ssh.ParsePublicKey(w.PubKey); err == nil {
+		ki.Type = pubKey.Type()
+		// We ought be able to get the size of the key, but I don't see
+		// how it's exposed. The ssh.PublicKey type is very bare.
+		// ki.Bits = len(pubKey.Parameters().Y.Bytes()) * 8
+	}
+
+	return ki, nil
+
+}
