8f1bfec61fbef9f78c0281753e3b0800df0f0eb6
seph <seph@directionless.org>
2019-09-09 12:09:28 -0400

    Add productbuild to the osx packaging (#519)
    This adds `productbuild` to the osx packaging pipeline. This produces a _distribution_ package, suitable for use with MDM environments.
    
    Fixes: https://github.com/kolide/launcher/issues/407
    Relates To: https://github.com/kolide/launcher/pull/338

--- pkg/packagekit/package_pkg.go
+++ pkg/packagekit/package_pkg.go
@@ -10,6 +10,7 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/kolide/launcher/pkg/contexts/ctxlog"
 	"github.com/pkg/errors"
@@ -20,13 +21,9 @@ func PackagePkg(ctx context.Context, w io.Writer, po *PackageOptions) error {
 	ctx, span := trace.StartSpan(ctx, "packagekit.PackagePkg")
 	defer span.End()
 
-	logger := ctxlog.FromContext(ctx)
-
 	if err := isDirectory(po.Root); err != nil {
 		return err
 	}
-
-	outputFilename := fmt.Sprintf("%s-%s.pkg", po.Name, po.Version)
 
 	outputPathDir, err := ioutil.TempDir("", "packaging-pkg-output")
 	if err != nil {
@@ -34,7 +31,37 @@ func PackagePkg(ctx context.Context, w io.Writer, po *PackageOptions) error {
 	}
 	defer os.RemoveAll(outputPathDir)
 
-	outputPath := filepath.Join(outputPathDir, outputFilename)
+	flatPkgPath := filepath.Join(outputPathDir, fmt.Sprintf("%s-%s.flat.pkg", po.Name, po.Version))
+	distributionPkgPath := filepath.Join(outputPathDir, fmt.Sprintf("%s-%s.pkg", po.Name, po.Version))
+
+	if err := runPkbuild(ctx, flatPkgPath, po); err != nil {
+		return errors.Wrap(err, "running pkgbuild")
+	}
+
+	if err := runProductbuild(ctx, flatPkgPath, distributionPkgPath, po); err != nil {
+		return errors.Wrap(err, "running productbuild")
+	}
+
+	outputFH, err := os.Open(distributionPkgPath)
+	if err != nil {
+		return errors.Wrap(err, "opening resultant output file")
+	}
+	defer outputFH.Close()
+
+	if _, err := io.Copy(w, outputFH); err != nil {
+		return errors.Wrap(err, "copying output")
+	}
+
+	return nil
+}
+
+// runPkbuild produces a flat pkg file. It uses the directories
+// specified in PackageOptions, and then execs pkgbuild
+func runPkbuild(ctx context.Context, outputPath string, po *PackageOptions) error {
+	ctx, span := trace.StartSpan(ctx, "packagekit.runPkbuild")
+	defer span.End()
+
+	logger := log.With(ctxlog.FromContext(ctx), "method", "packagekit.runPkbuild")
 
 	args := []string{
 		"--root", po.Root,
@@ -62,19 +89,45 @@ func PackagePkg(ctx context.Context, w io.Writer, po *PackageOptions) error {
 	stderr := new(bytes.Buffer)
 	cmd.Stderr = stderr
 	if err := cmd.Run(); err != nil {
-		return errors.Wrapf(err, "creating pkg package: %s", stderr)
-	}
-
-	outputFH, err := os.Open(filepath.Join(outputPathDir, outputFilename))
-	if err != nil {
-		return errors.Wrap(err, "opening resultant output file")
-	}
-	defer outputFH.Close()
-
-	if _, err := io.Copy(w, outputFH); err != nil {
-		return errors.Wrap(err, "copying output")
+		return errors.Wrapf(err, "creating flat pkg package: %s", stderr)
 	}
 
 	return nil
+}
 
+// runProductbuild converts a flat package to a distribution
+// package. It does this by execing productbuild.
+//
+// See https://github.com/kolide/launcher/issues/407 and associated links
+func runProductbuild(ctx context.Context, flatPkgPath, distributionPkgPath string, po *PackageOptions) error {
+	ctx, span := trace.StartSpan(ctx, "packagekit.runProductbuild")
+	defer span.End()
+
+	logger := log.With(ctxlog.FromContext(ctx), "method", "packagekit.runProductbuild")
+
+	args := []string{}
+
+	if po.AppleSigningKey != "" {
+		args = append(args, "--sign", po.AppleSigningKey)
+	}
+
+	args = append(args,
+		"--package", flatPkgPath,
+		distributionPkgPath,
+	)
+
+	level.Debug(logger).Log(
+		"msg", "Running productbuild",
+		"args", fmt.Sprintf("%v", args),
+	)
+
+	cmd := exec.CommandContext(ctx, "productbuild", args...)
+
+	stderr := new(bytes.Buffer)
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		return errors.Wrapf(err, "creating distribution pkg package: %s", stderr)
+	}
+
+	return nil
 }
