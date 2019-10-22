6e4f87e6641bd59c7d09fc4c8284dfc90e4c53d6
blaedj <blaedj@gmail.com>
2019-10-22 14:50:00 -0500

    use standard expected/actual for variable names

--- pkg/keyidentifier/keyidentifier_test.go
+++ pkg/keyidentifier/keyidentifier_test.go
@@ -59,67 +59,67 @@ func testIdentifyFile(t *testing.T, kIdentifer *KeyIdentifier, specFilePath stri
 	// load the json file
 	data, err := ioutil.ReadFile(specFilePath)
 	require.NoError(t, err, "reading spec file")
-	var example spec
-	err = json.Unmarshal(data, &example)
+	var expected spec
+	err = json.Unmarshal(data, &expected)
 	require.NoError(t, err, "parsing json spec file: %s", specFilePath)
-	keyPath := "testdata/specs/" + example.KeyPath
+	keyPath := "testdata/specs/" + expected.KeyPath
 
-	keyInfo, err := kIdentifer.IdentifyFile(keyPath)
+	actual, err := kIdentifer.IdentifyFile(keyPath)
 	require.NoError(t, err, "path to unparseable key: %s", keyPath)
 
 	// Key type. It's not wholly clear how we want to identify
 	// these. Right now, we do it this way. But it might change.
-	switch example.Type {
+	switch expected.Type {
 	case "rsa":
-		example.Type = "ssh-rsa"
+		expected.Type = "ssh-rsa"
 	case "dsa":
-		example.Type = "ssh-dss"
+		expected.Type = "ssh-dss"
 	case "ed25519":
-		example.Type = "ssh-ed25519"
+		expected.Type = "ssh-ed25519"
 		// ed25519 is always the new format
-		if keyInfo.Format == "openssh" || keyInfo.Format == "openssh-new" {
-			example.Format = "openssh-new"
-			keyInfo.Format = "openssh-new"
+		if actual.Format == "openssh" || actual.Format == "openssh-new" {
+			expected.Format = "openssh-new"
+			actual.Format = "openssh-new"
 		}
 	default:
 	}
 
 	// The elliptic keys don't always have a clear file format, so don't
 	// compare that in this test.
-	if example.Type == "ecdsa" && keyInfo.Format == "" {
-		example.Format = ""
+	if expected.Type == "ecdsa" && actual.Format == "" {
+		expected.Format = ""
 	}
 
 	// The elliptic types carry more detail than we need. So whomp down
 	// how we test. eg `ecdsa-sha2-nistp256` becomes `ecdsa` for testing
-	if strings.HasPrefix(keyInfo.Type, "ecdsa-") {
-		example.Type = "ecdsa"
-		keyInfo.Type = "ecdsa"
+	if strings.HasPrefix(actual.Type, "ecdsa-") {
+		expected.Type = "ecdsa"
+		actual.Type = "ecdsa"
 	}
 
 	// Test correct 'bits' reporting
 	// there are several key types/formats that we don't retrieve 'bits' from yet
 	switch {
-	case (keyInfo.Format == "openssh" && *keyInfo.Encrypted):
-	case (example.Type == "ecdsa" && *keyInfo.Encrypted):
-	case (keyInfo.Format == "openssh-new"):
-	case (keyInfo.Format == "putty"):
-	case (keyInfo.Format == "sshcom"):
+	case (actual.Format == "openssh" && *actual.Encrypted):
+	case (expected.Type == "ecdsa" && *actual.Encrypted):
+	case (actual.Format == "openssh-new"):
+	case (actual.Format == "putty"):
+	case (actual.Format == "sshcom"):
 	default:
-		require.Equal(t, example.Bits, keyInfo.Bits, "unexpected 'Bits' value, path: %s", example.KeyPath)
+		require.Equal(t, expected.Bits, actual.Bits, "unexpected 'Bits' value, path: %s", expected.KeyPath)
 	}
 
 	// test correct fingerprint reporting. limited support for now
-	if keyInfo.Format == "openssh-new" {
-		if example.Source != "putty" {
-			require.Equal(t, example.ExpectedFingerprintSHA256, keyInfo.FingerprintSHA256,
-				"unexpected sha256 fingerprint, path: %s", example.KeyPath)
+	if actual.Format == "openssh-new" {
+		if expected.Source != "putty" {
+			require.Equal(t, expected.ExpectedFingerprintSHA256, actual.FingerprintSHA256,
+				"unexpected sha256 fingerprint, path: %s", expected.KeyPath)
 		}
-		require.Equal(t, example.ExpectedFingerprintMD5, keyInfo.FingerprintMD5,
-			"unexpected md5 fingerprint, path: %s", example.KeyPath)
+		require.Equal(t, expected.ExpectedFingerprintMD5, actual.FingerprintMD5,
+			"unexpected md5 fingerprint, path: %s", expected.KeyPath)
 	}
 
-	require.Equal(t, example.Format, keyInfo.Format, "unexpected key format, path: %s", example.KeyPath)
-	require.Equal(t, example.Type, keyInfo.Type, "unexpected key type, path: %s", example.KeyPath)
-	require.Equal(t, example.Encrypted, *keyInfo.Encrypted, "unexpected encrypted boolean, path: %s", example.KeyPath)
+	require.Equal(t, expected.Format, actual.Format, "unexpected key format, path: %s", expected.KeyPath)
+	require.Equal(t, expected.Type, actual.Type, "unexpected key type, path: %s", expected.KeyPath)
+	require.Equal(t, expected.Encrypted, *actual.Encrypted, "unexpected encrypted boolean, path: %s", expected.KeyPath)
 }
