kolide/launcher

commit 7095eb7b911ddf4f88931abe45f0a001027760fa

Add Program Icon table (#514)
Adds the ability to query a base64 encoded 128 x 128 PNG icon for programs.

go.mod
@@ -52,6 +52,7 @@ require (
 	github.com/kolide/updater v0.0.0-20190315001611-15bbc19b5b80
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/kr/pty v1.1.2
+	github.com/mat/besticon v3.9.0+incompatible
 	github.com/mattn/go-sqlite3 v1.10.0
 	github.com/miekg/pkcs11 v0.0.0-20180208123018-5f6e0d0dad6f // indirect
 	github.com/mitchellh/go-ps v0.0.0-20170309133038-4fdf99ab2936

go.sum
@@ -164,6 +164,8 @@ github.com/lib/pq v1.0.0 h1:X5PMW56eZitiTeO7tKzZxFCSpbFZJtkMMooicw2us9A=
 github.com/lib/pq v1.0.0/go.mod h1:5WUZQaWbwv1U+lTReE5YruASi9Al49XbQIvNi/34Woo=
 github.com/magiconair/properties v1.8.0 h1:LLgXmsheXeRoUOBOjtwPQCWIYqM/LU1ayDtDePerRcY=
 github.com/magiconair/properties v1.8.0/go.mod h1:PppfXfuXeibc/6YijjN8zIbojt8czPbwD3XqdrwzmxQ=
+github.com/mat/besticon v3.9.0+incompatible h1:SLaWKCE7ptsjWbQee8Sbx8F/WK4bw8b55tUV4mY0m/c=
+github.com/mat/besticon v3.9.0+incompatible/go.mod h1:mA1auQYHt6CW5e7L9HJLmqVQC8SzNk2gVwouO0AbiEU=
 github.com/mattn/go-sqlite3 v1.10.0 h1:jbhqpg7tQe4SupckyijYiy0mJJ/pRyHvXf7JdWK860o=
 github.com/mattn/go-sqlite3 v1.10.0/go.mod h1:FPy6KqzDD04eiIsT53CuJW3U88zkxoIYsOqkbpncsNc=
 github.com/matttproud/golang_protobuf_extensions v1.0.1 h1:4hp9jkHxhMHkqkrB3Ix0jegS5sx/RkqARlsWZ6pIwiU=

pkg/osquery/table/platform_tables.go
@@ -1,0 +1,15 @@
+// +build !darwin, !windows
+
+package table
+
+import (
+	"github.com/go-kit/kit/log"
+	osquery "github.com/kolide/osquery-go"
+	"github.com/kolide/osquery-go/plugin/table"
+)
+
+// platformTables returns an empty set. It's here as a catchall for
+// unimplemented platforms.
+func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger) []*table.Plugin {
+	return []*table.Plugin{}
+}

pkg/osquery/table/platform_tables_windows.go
@@ -1,4 +1,4 @@
-// +build !darwin
+// +build windows
 
 package table
 
@@ -8,8 +8,8 @@ import (
 	"github.com/kolide/osquery-go/plugin/table"
 )
 
-// platformTables returns an empty set. It's here as a catchall for
-// unimplemented platforms.
 func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger) []*table.Plugin {
-	return []*table.Plugin{}
+	return []*table.Plugin{
+		ProgramIcons(),
+	}
 }

pkg/osquery/table/program_icons_windows.go
@@ -1,0 +1,190 @@
+package table
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"hash/crc64"
+	"image/png"
+	"os"
+
+	"strings"
+
+	"github.com/kolide/osquery-go/plugin/table"
+	"github.com/mat/besticon/ico"
+	"github.com/nfnt/resize"
+	"golang.org/x/sys/windows/registry"
+)
+
+var crcTable = crc64.MakeTable(crc64.ECMA)
+
+type icon struct {
+	base64 string
+	hash   uint64
+}
+
+func ProgramIcons() *table.Plugin {
+	columns := []table.ColumnDefinition{
+		table.TextColumn("name"),
+		table.TextColumn("version"),
+		table.TextColumn("icon"),
+		table.TextColumn("hash"),
+	}
+	return table.NewPlugin("kolide_program_icons", columns, generateProgramIcons)
+}
+
+func generateProgramIcons(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+	var results []map[string]string
+
+	results = append(results, generateUninstallerProgramIcons()...)
+	results = append(results, generateInstallersProgramIcons()...)
+
+	return results, nil
+}
+
+func generateUninstallerProgramIcons() []map[string]string {
+	var uninstallerIcons []map[string]string
+
+	uninstallRegPaths := map[registry.Key][]string{
+		registry.LOCAL_MACHINE: append(expandRegistryKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*`),
+			expandRegistryKey(registry.LOCAL_MACHINE, `\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*`)...),
+		registry.USERS: expandRegistryKey(registry.USERS, `*\Software\Microsoft\Windows\CurrentVersion\Uninstall\*`),
+	}
+
+	for key, paths := range uninstallRegPaths {
+		for _, path := range paths {
+			key, err := registry.OpenKey(key, path, registry.READ)
+			defer key.Close()
+			if err != nil {
+				continue
+			}
+
+			iconPath, _, err := key.GetStringValue("DisplayIcon")
+			icon, err := parseIcoFile(iconPath)
+			if err != nil {
+				continue
+			}
+			name, _, err := key.GetStringValue("DisplayName")
+			if err != nil {
+				continue
+			}
+			version, _, _ := key.GetStringValue("DisplayVersion")
+
+			uninstallerIcons = append(uninstallerIcons, map[string]string{
+				"icon":    icon.base64,
+				"hash":    fmt.Sprintf("%x", icon.hash),
+				"name":    name,
+				"version": version,
+			})
+		}
+	}
+	return uninstallerIcons
+}
+
+func generateInstallersProgramIcons() []map[string]string {
+	var installerIcons []map[string]string
+
+	productRegPaths := map[registry.Key][]string{
+		registry.CLASSES_ROOT: expandRegistryKey(registry.CLASSES_ROOT, `Installer\Products\*`),
+		registry.USERS:        expandRegistryKey(registry.USERS, `*\Software\Microsoft\Installer\Products\*`),
+	}
+
+	for key, paths := range productRegPaths {
+		for _, path := range paths {
+			key, err := registry.OpenKey(key, path, registry.READ)
+			defer key.Close()
+			if err != nil {
+				continue
+			}
+
+			iconPath, _, err := key.GetStringValue("ProductIcon")
+			icon, err := parseIcoFile(iconPath)
+			if err != nil {
+				continue
+			}
+			name, _, _ := key.GetStringValue("ProductName")
+			if err != nil {
+				continue
+			}
+
+			installerIcons = append(installerIcons, map[string]string{
+				"icon": icon.base64,
+				"hash": fmt.Sprintf("%x", icon.hash),
+				"name": name,
+			})
+		}
+	}
+
+	return installerIcons
+}
+
+// parseIcoFile returns a base64 encoded version and a hash of the ico.
+//
+// This doesn't support extracting an icon from a exe. Windows stores some icon in
+// the exe like 'OneDriveSetup.exe,-101'
+func parseIcoFile(fullPath string) (icon, error) {
+	var programIcon icon
+	expandedPath, err := registry.ExpandString(fullPath)
+	icoReader, err := os.Open(expandedPath)
+	if err != nil {
+		return programIcon, err
+	}
+	img, err := ico.Decode(icoReader)
+	if err != nil {
+		return programIcon, err
+	}
+	buf := new(bytes.Buffer)
+	img = resize.Resize(128, 128, img, resize.Bilinear)
+	if err := png.Encode(buf, img); err != nil {
+		return programIcon, err
+	}
+
+	checksum := crc64.Checksum(buf.Bytes(), crcTable)
+	return icon{base64: base64.StdEncoding.EncodeToString(buf.Bytes()), hash: checksum}, nil
+}
+
+// expandRegistryKey takes a hive and path, and does a non-recursive glob expansion
+//
+// For example expandRegistryKey(registry.USERS, `*\Software\Microsoft\Installer\Products\*`)
+// expands to
+// USER1\Software\Microsoft\Installer\Products\2CCC92FB8B3D5F6499511F546A784ACD
+// USER1\Software\Microsoft\Installer\Products\1AAA2FB8B3D5F6499511F546A784ACD
+// USER2\Software\Microsoft\Installer\Products\3FFF92FB8B3D5F6499511F546A784ACD
+// USER2\Software\Microsoft\Installer\Products\5DDD92FB8B3D5F6499511F546A784ACD
+func expandRegistryKey(hive registry.Key, pattern string) []string {
+	var paths []string
+	magicChar := `*`
+
+	patternsQueue := []string{pattern}
+	for len(patternsQueue) > 0 {
+		expandablePattern := patternsQueue[0]
+		patternsQueue = patternsQueue[1:]
+
+		// add path to results if it doesn't contain the magic char
+		if !strings.Contains(expandablePattern, magicChar) {
+			paths = append(paths, expandablePattern)
+			continue
+		}
+
+		patternParts := strings.SplitN(expandablePattern, magicChar, 2)
+		key, err := registry.OpenKey(hive, patternParts[0], registry.READ)
+		if err != nil {
+			continue
+		}
+		stats, err := key.Stat()
+		if err != nil {
+			continue
+		}
+		subKeyNames, err := key.ReadSubKeyNames(int(stats.SubKeyCount))
+		if err != nil {
+			continue
+		}
+
+		for _, subKeyName := range subKeyNames {
+			patternsQueue = append(patternsQueue, patternParts[0]+subKeyName+patternParts[1])
+		}
+	}
+
+	return paths
+}

commit 5667157617004a4d33fcb4c6dfc62951d81a9449

Fixes for a release (#536)
* Fix build tag syntax error accidentally introduced in #514
* Fix docker builds in Makefile
* speed up docker build by excluding `build` directory
* update go modules

.dockerignore
@@ -1,0 +1,1 @@
+build

Dockerfile
@@ -17,11 +17,10 @@ ARG FAKE
 WORKDIR /go/src/github.com/kolide/launcher
 
 # copy source into the docker builder. Perhaps this would be cleaner
-# via a volume mount. But, COPY allows the docker container to
-# have zero impact on the host's build directory. We want _all_ files
-# but `build`. Unfortunately there's no exclude. So, copy and rm.
+# via a volume mount. But, COPY allows the docker container to have
+# zero impact on the host's build directory. This uses .dockerignore
+# to exclude the build directory
 COPY . ./
-RUN rm -rf build
 
 # Build!
 RUN make deps

Makefile
@@ -160,7 +160,7 @@ build/binary-bundle/%:
 CONTAINER_OSES = ubuntu16 ubuntu18 centos6 centos7 distroless
 
 .PHONY: containers
-containers: $(foreach c,$(CONTAINER_OSES),docker-$(c) docker-fake-$(c))
+containers: $(foreach c,$(CONTAINER_OSES),docker-$(c) dockerfake-$(c))
 containers-push: $(foreach c,$(CONTAINER_OSES),dockerpush-$(c) dockerpush-fakedata-$(c))
 
 build-docker:
@@ -169,7 +169,7 @@ build-docker:
 build-dockerfake:
 	docker build -t launcher-fakedata-build --build-arg FAKE=-fakedata .
 
-docker-fake-%:  build-dockerfake
+dockerfake-%:  build-dockerfake
 	docker build -t gcr.io/kolide-public-containers/launcher-fakedata-$* --build-arg FAKE=-fakedata docker/$*
 
 docker-%: build-docker

go.mod
@@ -1,11 +1,5 @@
 module github.com/kolide/launcher
 
-// Force references to thrift to use the new place.
-// https://github.com/knightsc/system_policy/pull/6 is the remaining one
-replace git.apache.org/thrift.git => github.com/apache/thrift v0.12.0
-
-replace github.com/kolide/osquery-go => github.com/kolide/osquery-go v0.0.0-20190113061206-be0a8de4cf1d
-
 require (
 	cloud.google.com/go v0.43.0
 	github.com/Masterminds/semver v1.4.2
@@ -46,7 +40,7 @@ require (
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/jinzhu/now v0.0.0-20181116074157-8ec929ed50c3 // indirect
 	github.com/kardianos/osext v0.0.0-20170510131534-ae77be60afb1
-	github.com/knightsc/system_policy v1.1.1-0.20190125011806-04a47ae55cf7
+	github.com/knightsc/system_policy v1.1.1-0.20191030190822-139971392acb
 	github.com/kolide/kit v0.0.0-20191023141830-6312ecc11c23
 	github.com/kolide/osquery-go v0.0.0-20190904034940-a74aa860032d
 	github.com/kolide/updater v0.0.0-20190315001611-15bbc19b5b80

go.sum
@@ -139,12 +139,12 @@ github.com/jmoiron/sqlx v0.0.0-20180406164412-2aeb6a910c2b/go.mod h1:IiEW3SEiiEr
 github.com/jstemmer/go-junit-report v0.0.0-20190106144839-af01ea7f8024/go.mod h1:6v2b51hI/fHJwM22ozAgKL4VKDeJcHhJFhtBdhmNjmU=
 github.com/kardianos/osext v0.0.0-20170510131534-ae77be60afb1 h1:PJPDf8OUfOK1bb/NeTKd4f1QXZItOX389VN3B6qC8ro=
 github.com/kardianos/osext v0.0.0-20170510131534-ae77be60afb1/go.mod h1:1NbS8ALrpOvjt0rHPNLyCIeMtbizbir8U//inJ+zuB8=
-github.com/knightsc/system_policy v1.1.1-0.20190125011806-04a47ae55cf7 h1:LkPVVNfGk7yrRiKwE5dchY9eqrER+99SddkLntU2YX0=
-github.com/knightsc/system_policy v1.1.1-0.20190125011806-04a47ae55cf7/go.mod h1:Ah+zOuAz6TBHVeG1UiExiExlo8jeuU9yTchmhDVrAdY=
+github.com/knightsc/system_policy v1.1.1-0.20191030190822-139971392acb h1:Xu+ciFy0JPiPCm2uk3TNUjE7rgBLA3eMNzrsR8J6ZSQ=
+github.com/knightsc/system_policy v1.1.1-0.20191030190822-139971392acb/go.mod h1:oYXhDWn3yazA1es4R9mvw//dKRDoZwLEmWOMkkG8IQE=
 github.com/kolide/kit v0.0.0-20191023141830-6312ecc11c23 h1:7rykD5+Wf11u+03TOsunGbg7f4gZEBgS0gwIRR+Han4=
 github.com/kolide/kit v0.0.0-20191023141830-6312ecc11c23/go.mod h1:OYYulo9tUqRadRLwB0+LE914sa1ui2yL7OrcU3Q/1XY=
-github.com/kolide/osquery-go v0.0.0-20190113061206-be0a8de4cf1d h1:2/+lLTfZan4IGrD8TGC1ZVkehWLtrRuah47FASAnifE=
-github.com/kolide/osquery-go v0.0.0-20190113061206-be0a8de4cf1d/go.mod h1:umPEbeG8R8RDJpQ0EMyRdj+6pfnJK4FTzjafwgovDUk=
+github.com/kolide/osquery-go v0.0.0-20190904034940-a74aa860032d h1:alVW+rIOMejarlhjLl4AYaQaGAyfvIUDg9NyyKRhSek=
+github.com/kolide/osquery-go v0.0.0-20190904034940-a74aa860032d/go.mod h1:QVQKz6+eKB0syu5u8BS1E4S/nTMuy44d9WbRFQ4nLnQ=
 github.com/kolide/updater v0.0.0-20190315001611-15bbc19b5b80 h1:XFzdAHvTlbQoHZdEgOEiFt93eyfXP6VZCwH5p+lPpBg=
 github.com/kolide/updater v0.0.0-20190315001611-15bbc19b5b80/go.mod h1:x3dEGYbZovhD1t8OwEgdyu/4ZCvrn9QvkbPtOZnul8k=
 github.com/konsorten/go-windows-terminal-sequences v1.0.1 h1:mweAR1A6xJ3oS2pRaGiHgQ4OO8tzTaLawm8vnODuwDk=

pkg/osquery/table/platform_tables.go
@@ -1,4 +1,4 @@
-// +build !darwin, !windows
+// +build !darwin,!windows
 
 package table
 