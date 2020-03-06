7354a13ecfe6951f196485ef1a064a00d14f62a5
seph <seph@directionless.org>
2020-03-05 21:12:28 -0500

    nested plist tests

--- pkg/dataflatten/plist_test.go
+++ pkg/dataflatten/plist_test.go
@@ -1,6 +1,7 @@
 package dataflatten
 
 import (
+	"path/filepath"
 	"testing"
 )
 
@@ -25,7 +26,53 @@ func TestPlist(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		actual, err := Plist([]byte(tt.in))
-		testFlattenCase(t, tt, actual, err)
+		t.Run(tt.comment, func(t *testing.T) {
+			actual, err := Plist([]byte(tt.in))
+			testFlattenCase(t, tt, actual, err)
+		})
 	}
+}
+
+func TestNestedPlists(t *testing.T) {
+	t.Parallel()
+
+	var tests = []flattenTestCase{
+		{
+			options: []FlattenOpts{WithNestedPlist()},
+			comment: "expand nested",
+			out: []Row{
+				Row{Path: []string{"inbinary", "astring"}, Value: "hello"},
+				Row{Path: []string{"inbinary", "arr", "0"}, Value: "one"},
+				Row{Path: []string{"inbinary", "arr", "1"}, Value: "two"},
+				Row{Path: []string{"inxml", "arr", "0"}, Value: "one"},
+				Row{Path: []string{"inxml", "arr", "1"}, Value: "two"},
+				Row{Path: []string{"inxml", "astring"}, Value: "hello"},
+			},
+		},
+		{
+			comment: "nested and queried",
+			options: []FlattenOpts{WithNestedPlist(), WithQuery([]string{"*", "arr", "0"})},
+			out: []Row{
+				Row{Path: []string{"inbinary", "arr", "0"}, Value: "one"},
+				Row{Path: []string{"inxml", "arr", "0"}, Value: "one"},
+			},
+		},
+		{
+			comment: "not expanded",
+			out: []Row{
+				Row{Path: []string{"inbinary"}, Value: "YnBsaXN0MDDSAQIDBlNhcnJXYXN0cmluZ6IEBVNvbmVTdHdvVWhlbGxvCA0RGRwgJAAAAAAAAAEBAAAAAAAAAAcAAAAAAAAAAAAAAAAAAAAq"},
+				Row{Path: []string{"inxml"}, Value: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n<dict>\n\t<key>arr</key>\n\t<array>\n\t\t<string>one</string>\n\t\t<string>two</string>\n\t</array>\n\t<key>astring</key>\n\t<string>hello</string>\n</dict>\n</plist>"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		for _, f := range []string{"nested.xml", "nested.plist"} {
+			t.Run(tt.comment, func(t *testing.T) {
+				actual, err := PlistFile(filepath.Join("testdata", "nested", f), tt.options...)
+				testFlattenCase(t, tt, actual, err)
+			})
+		}
+	}
+
 }
