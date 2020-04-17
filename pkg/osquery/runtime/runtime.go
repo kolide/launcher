2c8f3b4adecf005b89eaf3ce56ea525b4f758a37
seph <seph@directionless.org>
2020-04-17 17:19:09 -0400

    Force osquery to verbose mode

--- pkg/osquery/runtime/runtime.go
+++ pkg/osquery/runtime/runtime.go
@@ -149,6 +149,18 @@ func (opts *osqueryOptions) createOsquerydCommand(osquerydBinary string, paths *
 		"--utc",
 	)
 
+	// TODO: This is a short term hack.
+	// In https://github.com/osquery/osquery/pull/6271 osquery
+	// shifted some debugging info from INFO to VERBOSE. This has
+	// the unfortunate effect of making it hard to correlate
+	// distributed query logs with the distributed query that
+	// caused them. While we're thinking through the longer term
+	// fix, we have a quick mitagation in dropping osquery into
+	// verbose mode. (This is duplicative wirth the opts.verbose
+	// parsing, because this whole block should be struck once we
+	// have a better approach)
+	cmd.Args = append(cmd.Args, "--verbose")
+
 	if opts.verbose {
 		cmd.Args = append(cmd.Args, "--verbose")
 	}
