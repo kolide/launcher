accc51559c6088ed903dd1c4abe3e2950c0c0e47
seph <seph@directionless.org>
2020-07-04 00:33:32 -0400

    windows

--- pkg/ptycmd/command_windows.go
+++ pkg/ptycmd/command_windows.go
@@ -2,11 +2,30 @@
 
 package ptycmd
 
-import (
-	"github.com/pkg/errors"
-)
+import "errors"
 
 // NewCmd creates a new command attached to a pty
 func NewCmd(command string, argv []string, options ...Option) (*Cmd, error) {
 	return nil, errors.New("Not supported on windows")
+}
+
+func (c *Cmd) Close() error {
+	return errors.New("Not supported on windows")
+}
+
+func (c *Cmd) Read(p []byte) (n int, err error) {
+	return c.pty.Read(p)
+}
+
+// Write writes from the specified buffer into the pty
+func (c *Cmd) Write(p []byte) (n int, err error) {
+	return c.pty.Write(p)
+}
+
+func (c *Cmd) Resize(width int, height int) error {
+	return nil
+}
+
+func (c *Cmd) Title() string {
+	return ""
 }
