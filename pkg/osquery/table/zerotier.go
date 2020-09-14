cc7c7e46d20e72b52152c64c5ddafedb93309340
seph <seph@directionless.org>
2020-09-14 16:40:42 -0400

    Zero Tier Information Tables (#656)
    Add informational reporting tables for zero tier one

---
+++ pkg/osquery/table/zerotier.go
@@ -0,0 +1,10 @@
+// +build !windows
+
+package table
+
+// zerotierCli returns the path to the CLI executable.
+func zerotierCli(args ...string) []string {
+	cmd := []string{"/usr/local/bin/zerotier-cli"}
+
+	return append(cmd, args...)
+}
