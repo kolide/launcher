17ffb54cc5a63f314cdde4a22fb0f76a001df78b
seph <seph@directionless.org>
2020-09-14 16:25:36 -0400

    fix getenv

--- pkg/osquery/table/zerotier_windows.go
+++ pkg/osquery/table/zerotier_windows.go
@@ -2,12 +2,15 @@
 
 package table
 
-import "path"
+import (
+	"os"
+	"path"
+)
 
 // zerotierCli returns the path to the CLI executable.
 func zerotierCli(args ...string) []string {
 	cmd := []string{
-		path.Join(os.GetEnv("SYSTEMROOT"), "ProgramData", "ZeroTier", "One", "zerotier-one_x64.exe"),
+		path.Join(os.Getenv("SYSTEMROOT"), "ProgramData", "ZeroTier", "One", "zerotier-one_x64.exe"),
 		"-q",
 	}
 
