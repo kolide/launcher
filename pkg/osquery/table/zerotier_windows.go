4a4878d1ce7106a588bf3a0570296e1bac65edc6
seph <seph@directionless.org>
2020-09-14 16:19:15 -0400

    use systemroot

--- pkg/osquery/table/zerotier_windows.go
+++ pkg/osquery/table/zerotier_windows.go
@@ -7,7 +7,7 @@ import "path"
 // zerotierCli returns the path to the CLI executable.
 func zerotierCli(args ...string) []string {
 	cmd := []string{
-		path.Join("C:", "ProgramData", "ZeroTier", "One", "zerotier-one_x64.exe"),
+		path.Join(os.GetEnv("SYSTEMROOT"), "ProgramData", "ZeroTier", "One", "zerotier-one_x64.exe"),
 		"-q",
 	}
 
