27521f9e752d4cd1f9056944127290d7d471eba4
blaedj <blaedj@gmail.com>
2020-10-06 08:51:22 -0500

    simple tests

--- pkg/osquery/tables/gsettings/gsettings_test.go
+++ pkg/osquery/tables/gsettings/gsettings_test.go
@@ -2,55 +2,68 @@ package gsettings
 
 import (
 	"bytes"
-	"io/ioutil"
+	"context"
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/go-kit/kit/log"
+	"github.com/kolide/launcher/pkg/osquery/tables/tablehelpers"
 	"github.com/stretchr/testify/require"
 )
 
-func TestParser(t *testing.T) {
+func TestOutputParsing(t *testing.T) {
 	t.Parallel()
 
 	var tests = []struct {
-		input    string
+		filename string
 		expected []map[string]string
 	}{
 		{
-			input:    "blank.txt",
+			filename: "blank.txt",
 			expected: []map[string]string{},
 		},
 		{
-			input: "simple.txt",
-			expected: []map[string]string{{
-				"domain": "org.gnome.rhythmbox.plugins.webremote",
-				"key":    "access-key",
-				"value":  "''",
-			},
+			filename: "simple.txt",
+			expected: []map[string]string{
 				{
-					"domain": "org.gnome.rhythmbox.plugins.webremote",
-					"key":    "foo-bar",
-					"value":  "2",
+					"fullkey": "org.gnome.rhythmbox.plugins.webremote/access-key",
+					"parent":  "org.gnome.rhythmbox.plugins.webremote",
+					"key":     "access-key",
+					"value":   "''",
+					"schema":  "org.gnome.rhythmbox.plugins.webremote",
+				},
+				{
+
+					"fullkey": "org.gnome.rhythmbox.plugins.webremote/foo-bar",
+					"parent":  "org.gnome.rhythmbox.plugins.webremote",
+					"key":     "foo-bar",
+					"value":   "2",
+					"schema":  "org.gnome.rhythmbox.plugins.webremote",
 				},
 			},
 		},
 	}
 
 	for _, tt := range tests {
-		table := GsettingsValues{logger: log.NewNopLogger()}
-		t.Run(tt.input, func(t *testing.T) {
-			inputBytes, err := ioutil.ReadFile(filepath.Join("testdata", tt.input))
-			require.NoError(t, err, "read file %s", tt.input)
+		table := GsettingsValues{
+			logger: log.NewNopLogger(),
+			getBytes: func(ctx context.Context, buf *bytes.Buffer) error {
+				f, err := os.Open(filepath.Join("testdata", tt.filename))
+				require.NoError(t, err, "opening file %s", tt.filename)
+				_, err = buf.ReadFrom(f)
+				require.NoError(t, err, "read file %s", tt.filename)
 
-			inputBuffer := bytes.NewBuffer(inputBytes)
+				return nil
+			},
+		}
+		t.Run(tt.filename, func(t *testing.T) {
+			ctx := context.TODO()
+			qCon := tablehelpers.MockQueryContext(map[string][]string{})
 
-			results := []map[string]string{}
-			for _, row := range table.parse(inputBuffer) {
-				results = append(results, row)
-			}
-
-			require.EqualValues(t, tt.expected, results)
+			results, err := table.generate(ctx, qCon)
+			require.NoError(t, err, "generating results from %s", tt.filename)
+			require.ElementsMatch(t, tt.expected, results)
 		})
 	}
 }
