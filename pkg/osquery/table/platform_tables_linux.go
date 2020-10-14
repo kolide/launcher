577dd0c03e443fd60172fef693fb4c41f19a3042
blaedj <blaedj@gmail.com>
2020-10-14 13:49:52 -0500

    move gsettings table to linux-specific platform tables file
    Note that gsettings itself isn't necessarily linux-only, we just
    aren't supporting darwin/windows at this time.

--- pkg/osquery/table/platform_tables_linux.go
+++ pkg/osquery/table/platform_tables_linux.go
@@ -1,9 +1,10 @@
-// +build !darwin,!windows,!linux
+// +build linux
 
 package table
 
 import (
 	"github.com/go-kit/kit/log"
+	"github.com/kolide/launcher/pkg/osquery/tables/gsettings"
 	osquery "github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 )
@@ -11,5 +12,8 @@ import (
 // platformTables returns an empty set. It's here as a catchall for
 // unimplemented platforms.
 func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, currentOsquerydBinaryPath string) []*table.Plugin {
-	return []*table.Plugin{}
+	return []*table.Plugin{
+		gsettings.Settings(client, logger),
+		gsettings.Metadata(client, logger),
+	}
 }
