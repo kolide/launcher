6c02d75899332992814dbcd9b1cad37c41113f86
blaedj <blaedj@users.noreply.github.com>
2020-10-15 10:22:09 -0500

    Add table for gsettings (#667)
    Add kolide_gsettings table
    
    Fixes: #508

--- pkg/osquery/table/platform_tables_linux.go
+++ pkg/osquery/table/platform_tables_linux.go
@@ -1,15 +1,17 @@
-// +build !darwin,!windows,!linux
+// +build linux
 
 package table
 
 import (
 	"github.com/go-kit/kit/log"
+	"github.com/kolide/launcher/pkg/osquery/tables/gsettings"
 	osquery "github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 )
 
-// platformTables returns an empty set. It's here as a catchall for
-// unimplemented platforms.
 func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, currentOsquerydBinaryPath string) []*table.Plugin {
-	return []*table.Plugin{}
+	return []*table.Plugin{
+		gsettings.Settings(client, logger),
+		gsettings.Metadata(client, logger),
+	}
 }
