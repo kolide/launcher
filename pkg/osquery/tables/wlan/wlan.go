e317d17cf3c28bb777dcd9017b98ee43fef56c88
blaedj <blaedj@gmail.com>
2021-01-05 10:10:32 -0600

    cleanup

--- pkg/osquery/tables/wlan/wlan.go
+++ pkg/osquery/tables/wlan/wlan.go
@@ -1,12 +1,9 @@
-// -+-build windows
-
 package wlan
 
 import (
 	"bufio"
 	"bytes"
 	"context"
-	"fmt"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -19,17 +16,10 @@ import (
 	"github.com/pkg/errors"
 )
 
-const allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const netshCmd = "netsh"
 
 var preludeInterfaceLineRegex = regexp.MustCompile(`Interface name :`)
 var preludeCountLineRegex = regexp.MustCompile(`There are [0-9]+ networks currently available`)
-
-var resultHeaderLine = regexp.MustCompile(`SSID [0-9]+`)
-var resultNetworkType = regexp.MustCompile(`Network type`)
-var resultAuthentication = regexp.MustCompile(`Authentication`)
-var resultEncryption = regexp.MustCompile(`Encryption`)
-var resultBSSID = regexp.MustCompile(`BSSID [0-9]+ +: `)
 
 type execer func(ctx context.Context, buf *bytes.Buffer) error
 
@@ -60,27 +50,6 @@ func TablePlugin(client *osquery.ExtensionManagerClient, logger log.Logger) *tab
 	}
 
 	return table.NewPlugin(t.tableName, columns, t.generate)
-}
-
-type wifiNetwork struct {
-	Name           string
-	Authentication string
-	SignalStrength int
-	BSSID          string
-	RadioType      string
-}
-
-func (w wifiNetwork) toMap() map[string]string {
-
-	r := map[string]string{
-		"name":                       w.Name,
-		"authentication":             w.Authentication,
-		"signal_strength_percentage": fmt.Sprintf("%d", w.SignalStrength),
-		"bssid":                      w.BSSID,
-		"radio_type":                 w.RadioType,
-	}
-
-	return r
 }
 
 func (t *WlanTable) generate(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
@@ -145,7 +114,10 @@ func buildParser(logger log.Logger) *OutputParser {
 				Match: func(in string) bool {
 					m, err := regexp.MatchString("^SSID [0-9]+", in)
 					if err != nil {
-						// TODO: add logging here
+						level.Debug(logger).Log(
+							"msg", "unable to match regexp",
+							"err", err,
+						)
 						return false
 					}
 					return m
@@ -183,6 +155,8 @@ func buildParser(logger log.Logger) *OutputParser {
 }
 
 func wlanVal(input string) (string, error) {
+	// lines usually look something like:
+	//   Authentication       : WPA2-Personal
 	parts := strings.SplitN(strings.TrimSpace(input), ":", 2)
 	if len(parts) < 2 {
 		return "", errors.Errorf("unable to determine value from %s", input)
@@ -193,11 +167,3 @@ func wlanVal(input string) (string, error) {
 func hasTrimmedPrefix(s, prefix string) bool {
 	return strings.HasPrefix(strings.TrimSpace(s), prefix)
 }
-
-// // dropCR drops a terminal \r from the data.
-// func dropCR(data []byte) []byte {
-// 	if len(data) > 0 && data[len(data)-1] == '\r' {
-// 		return data[0 : len(data)-1]
-// 	}
-// 	return data
-// }
--- pkg/osquery/tables/wlan/wlan_test.go
+++ pkg/osquery/tables/wlan/wlan_test.go
@@ -26,15 +26,44 @@ func TestTableGenerate(t *testing.T) {
 			expected: []map[string]string{},
 		},
 		// {
-		// 	filename: "results2.txt",
-		// 	expected: []map[string]string{},
-		// },
-		// {
 		// 	filename: "results.txt",
 		// 	expected: []map[string]string{},
 		// },
 		{
-			filename: "results3.txt",
+			filename: "results.txt",
+			expected: []map[string]string{
+				{
+					"name":                       "ddu23n104",
+					"authentication":             "WPA2-Personal",
+					"signal_strength_percentage": "92",
+					"bssid":                      "80:2a:9c:eb:bb:65",
+					"radio_type":                 "802.11n",
+				},
+				{
+					"name":                       "",
+					"authentication":             "WPA2-Personal",
+					"signal_strength_percentage": "92",
+					"bssid":                      "82:2a:a8:eb:bb:65",
+					"radio_type":                 "802.11n",
+				},
+				{
+					"name":                       "GMG_DB_315",
+					"authentication":             "WPA2-Personal",
+					"signal_strength_percentage": "34",
+					"bssid":                      "08:ea:88:84:cf:6c",
+					"radio_type":                 "802.11n",
+				},
+				{
+					"name":                       "MySpectrumWiFi90-2G",
+					"authentication":             "WPA2-Personal",
+					"signal_strength_percentage": "0",
+					"bssid":                      "7c:db:98:b3:e0:8e",
+					"radio_type":                 "802.11ac",
+				},
+			},
+		},
+		{
+			filename: "multiple_results.txt",
 			expected: []map[string]string{
 				{
 					"name":                       "ddu23n104",
@@ -91,20 +120,6 @@ func TestTableGenerate(t *testing.T) {
 
 			results, err := table.generate(ctx, qCon)
 			require.NoError(t, err, "generating results from %s", tt.filename)
-			if len(results) > 0 {
-				for _, r := range results {
-					jsonString, err := json.Marshal(r)
-					require.NoError(t, err, "marshalling json: %s", tt.filename)
-					fmt.Println(string(jsonString))
-				}
-			}
-
-			for _, r := range tt.expected {
-				jsonString, err := json.Marshal(r)
-				require.NoError(t, err, "marshalling json: %s", tt.filename)
-				fmt.Printf("\033[35m%s\033[0m\n", jsonString)
-			}
-
 			require.ElementsMatch(t, tt.expected, results)
 		})
 
