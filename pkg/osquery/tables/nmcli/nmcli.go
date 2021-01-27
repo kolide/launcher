37c7567d4dfb5bc654004450bd9e42850aa1241b
blaedj <blaedj@gmail.com>
2021-01-27 15:20:23 -0600

    remove some old code

--- pkg/osquery/tables/nmcli/nmcli.go
+++ pkg/osquery/tables/nmcli/nmcli.go
@@ -13,7 +13,6 @@ import (
 	"github.com/go-kit/kit/log/level"
 	"github.com/kolide/launcher/pkg/dataflatten"
 	"github.com/kolide/launcher/pkg/osquery/tables/dataflattentable"
-	"github.com/kolide/launcher/pkg/osquery/tables/tablehelpers"
 	"github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 	"github.com/pkg/errors"
@@ -23,13 +22,12 @@ type Table struct {
 	client   *osquery.ExtensionManagerClient
 	logger   log.Logger
 	getBytes execer
-	parser   tablehelpers.OutputParser
 }
 
 // TODO: are there other places this lives on various distros/flavors?
 const nmcliPath = "/usr/bin/nmcli"
 
-type execer func(ctx context.Context, fields []string) ([]byte, error)
+type execer func(ctx context.Context) ([]byte, error)
 
 func TablePlugin(client *osquery.ExtensionManagerClient, logger log.Logger) *table.Plugin {
 	columns := dataflattentable.Columns(table.TextColumn("bssid"))
@@ -37,46 +35,20 @@ func TablePlugin(client *osquery.ExtensionManagerClient, logger log.Logger) *tab
 		client:   client,
 		logger:   logger,
 		getBytes: nmcliExecer(logger),
-		parser:   newParser(logger),
 	}
-	return table.NewPlugin("kolide_nmcli", columns, t.generateFlat)
+	return table.NewPlugin("kolide_nmcli", columns, t.generate)
 }
 
 func (t *Table) generate(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
-	var rows []map[string]string
-
-	fields := []string{"bssid", "ssid", "chan", "rate", "signal", "security", "frequency", "mode", "device", "active", "dbus_path"}
-	output, err := t.getBytes(ctx, fields)
-	if err != nil {
-		return rows, errors.Wrap(err, "getting output")
-	}
-	scanner := bufio.NewScanner(bytes.NewBuffer(output))
-	scanner.Split(onlyDashesScanner)
-	for scanner.Scan() {
-		chunk := scanner.Text()
-		row := t.parser.Parse(bytes.NewBufferString(chunk))
-		// should check for blank/null row here
-		rows = append(rows, row)
-	}
-
-	return rows, nil
-}
-
-func (t *Table) generateFlat(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
 	var results []map[string]string
-	// TODO: remove the fields param
-	fields := []string{"bssid"}
-	output, err := t.getBytes(ctx, fields)
+	output, err := t.getBytes(ctx)
 	if err != nil {
 		return results, errors.Wrap(err, "getting output")
 	}
 	scanner := bufio.NewScanner(bytes.NewBuffer(output))
 	scanner.Split(onlyDashesScanner)
-	// rows := []dataflatten.Row{}
 	for scanner.Scan() {
 		chunk := scanner.Bytes()
-		// TODO: this isn't really ini-formatted, but it actually works. Should
-		// make a simple `:`-delimited parser for dataflatten
 		rows, err := dataflatten.StringDelimited(chunk, ":", dataflatten.WithLogger(t.logger))
 		if err != nil {
 			return results, errors.Wrap(err, "flattening nmcli output")
@@ -96,14 +68,13 @@ func (t *Table) generateFlat(ctx context.Context, queryContext table.QueryContex
 }
 
 func nmcliExecer(logger log.Logger) execer {
-	return func(ctx context.Context, fields []string) ([]byte, error) {
+	return func(ctx context.Context) ([]byte, error) {
 		ctx, cancel := context.WithTimeout(ctx, 20*time.Second)
 		defer cancel()
 
 		var stderr bytes.Buffer
 		var stdout bytes.Buffer
 
-		// TODO: nmcli automatically re-scans if the wifi list is > 30 seconds old, so we probably don't need to add "--rescan=yes".
 		args := []string{"--mode=multiline", "--pretty", "--fields=all", "device", "wifi", "list", "--rescan", "yes"}
 
 		cmd := exec.CommandContext(ctx, nmcliPath, args...)
@@ -121,7 +92,8 @@ func nmcliExecer(logger log.Logger) execer {
 	}
 }
 
-// split input on lines that only contain dashes
+// onlyDashesScanner returns tokens delimited by lines that only contain dashes,
+// but may contain trailing whitespace
 func onlyDashesScanner(data []byte, atEOF bool) (int, []byte, error) {
 	var onlyDashes = regexp.MustCompile(`\r?\n-+[\s]*\r?\n`)
 	if atEOF && len(data) == 0 {
@@ -139,103 +111,4 @@ func onlyDashesScanner(data []byte, atEOF bool) (int, []byte, error) {
 
 	// Request more data.
 	return 0, nil, nil
-}
-
-// only use this if you're sure the output doesn't omit lines with empty values.
-func numberOfLinesScanner(lines int) bufio.SplitFunc {
-	var eol = regexp.MustCompile(`\r?\n`)
-	// returns advance,token,err
-	// advance is where the scanner should start next time,
-	return func(data []byte, atEOF bool) (int, []byte, error) {
-		if atEOF && len(data) == 0 {
-			return 0, nil, nil
-		}
-
-		locs := eol.FindAllIndex(data, lines)
-		// need to make sure we don't get 'index out of range' errors
-		// if this condition is false (and we're not at EOF), we'll end up
-		// asking the scanner to try again with more data
-		// if len(locs) < lines, then we have incomplete data probably
-		if locs != nil && len(locs) > 0 {
-			lastMatch := locs[len(locs)-1]
-
-			return lastMatch[1], data[0:lastMatch[0]], nil
-		}
-
-		// If we're at EOF, we have a final, non-terminated line. Return it.
-		if atEOF {
-			return len(data), data, nil
-		}
-
-		// Request more data.
-		return 0, nil, nil
-	}
-}
-
-func newParser(logger log.Logger) tablehelpers.OutputParser {
-	return *tablehelpers.NewParser(
-		logger,
-		[]tablehelpers.Matcher{
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "SSID:") },
-				KeyFunc: func(_ string) (string, error) { return "ssid", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "BSSID:") },
-				KeyFunc: func(_ string) (string, error) { return "bssid", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "CHAN:") },
-				KeyFunc: func(_ string) (string, error) { return "channel", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "RATE:") },
-				KeyFunc: func(_ string) (string, error) { return "rate", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "SIGNAL:") },
-				KeyFunc: func(_ string) (string, error) { return "signal", nil },
-				ValFunc: func(in string) (string, error) { return value(in) }, // TODO: Convert this to rssi
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "SECURITY:") },
-				KeyFunc: func(_ string) (string, error) { return "security", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "FREQ:") },
-				KeyFunc: func(_ string) (string, error) { return "frequency", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "MODE:") },
-				KeyFunc: func(_ string) (string, error) { return "mode", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "ACTIVE:") },
-				KeyFunc: func(_ string) (string, error) { return "active", nil },
-				ValFunc: func(in string) (string, error) { return value(in) }, // TODO: convert yes/no to true/false
-			},
-
-			{
-				Match:   func(in string) bool { return strings.HasPrefix(in, "DBUS-PATH:") },
-				KeyFunc: func(_ string) (string, error) { return "dbus_path", nil },
-				ValFunc: func(in string) (string, error) { return value(in) },
-			},
-		},
-	)
-}
-
-func value(in string) (string, error) {
-	components := strings.SplitN(in, ":", 2)
-	if len(components) < 2 {
-		return "", nil
-	}
-
-	return strings.TrimSpace(components[1]), nil
 }
