7894ba402347fe7a1e5d3aa91ee1a0dc3fb9d046
blaedj <blaedj@gmail.com>
2021-02-03 14:05:46 -0600

    feedback

--- pkg/dataflatten/string_delimited.go
+++ pkg/dataflatten/string_delimited.go
@@ -11,7 +11,7 @@ type dataFunc func(data []byte, opts ...FlattenOpts) ([]Row, error)
 type recordSplittingStrategy int
 
 const (
-	None recordSplittingStrategy = iota + 1
+	None recordSplittingStrategy = iota
 	DuplicateKeys
 )
 
@@ -33,8 +33,8 @@ func StringDelimitedFunc(kVDelimiter string, splittingStrategy recordSplittingSt
 // is encountered, and the record being built already has a value for that key,
 // then that record is considered 'complete'. The record is stored in the
 // collection, and a new record is started. This strategy is only suitable if
-// the data output does not exclude k/v pairs with blank/missing values, and
-// assumes that the properties for a single record are grouped together.
+// properties for a single record are grouped together, and there is at least
+// one field that appears for every record before any sparse data.
 func duplicateKeyFunc(kVDelimiter string) dataFunc {
 	return func(rawdata []byte, opts ...FlattenOpts) ([]Row, error) {
 		results := []interface{}{}
