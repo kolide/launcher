4cbd0de3e8b3367562a3909a4c63ae24a04493c5
seph <seph@directionless.org>
2021-02-12 11:57:26 -0500

    Table for windows installed updates (#710)
    Add a table for `kolide_windows_update_history`
    
    This is using the existing powershell mechanism.

--- pkg/osquery/table/platform_tables_windows.go
+++ pkg/osquery/table/platform_tables_windows.go
@@ -13,6 +13,20 @@ import (
 	"github.com/kolide/osquery-go/plugin/table"
 )
 
+const (
+	wuUpdatesPowershell = `
+$WUSession = New-Object -ComObject Microsoft.Update.Session
+$WUSearcher = $WUSession.CreateUpdateSearcher()
+$UpdateCollection = $WUSearcher.Search("Type='Software'")
+$UpdateCollection.Updates | ConvertTo-Json
+`
+	wuHistoryPowershell = `
+$WUSession = New-Object -ComObject Microsoft.Update.Session
+$WUSearcher = $WUSession.CreateUpdateSearcher()
+$WUSearcher.QueryHistory(0, $WUSearcher.GetTotalHistoryCount()) | ConvertTo-Json
+`
+)
+
 func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, currentOsquerydBinaryPath string) []*table.Plugin {
 	return []*table.Plugin{
 		ProgramIcons(),
@@ -21,16 +35,11 @@ func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, c
 		wifi_networks.TablePlugin(client, logger),
 		dataflattentable.TablePluginExec(client, logger, "kolide_windows_updates",
 			dataflattentable.JsonType,
-			[]string{
-				"powershell.exe",
-				"-NoProfile",
-				"-NonInteractive",
-				`$WUSession = New-Object -ComObject Microsoft.Update.Session
-$WUSearcher = $WUSession.CreateUpdateSearcher()
-$UpdateCollection = $WUSearcher.Search("Type='Software'")
-$UpdateCollection.Updates | ConvertTo-Json
-`,
-			},
+			[]string{"powershell.exe", "-NoProfile", "-NonInteractive", wuUpdatesPowershell},
+		),
+		dataflattentable.TablePluginExec(client, logger, "kolide_windows_update_history",
+			dataflattentable.JsonType,
+			[]string{"powershell.exe", "-NoProfile", "-NonInteractive", wuHistoryPowershell},
 		),
 	}
 }
