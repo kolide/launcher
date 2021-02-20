65957a505f21eeca181471981e95719d3931d021
seph <seph@directionless.org>
2021-02-20 00:04:21 -0500

    Use dsim to get associations on windows

--- pkg/osquery/table/platform_tables_windows.go
+++ pkg/osquery/table/platform_tables_windows.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kolide/launcher/pkg/osquery/tables/dataflattentable"
 	"github.com/kolide/launcher/pkg/osquery/tables/secedit"
 	"github.com/kolide/launcher/pkg/osquery/tables/wifi_networks"
+	"github.com/kolide/launcher/pkg/osquery/tables/windows_associations"
 	"github.com/kolide/launcher/pkg/osquery/tables/wmitable"
 
 	"github.com/go-kit/kit/log"
@@ -31,8 +32,9 @@ func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, c
 	return []*table.Plugin{
 		ProgramIcons(),
 		secedit.TablePlugin(client, logger),
-		wmitable.TablePlugin(client, logger),
 		wifi_networks.TablePlugin(client, logger),
+		windows_associations.TablePlugin(client, logger),
+		wmitable.TablePlugin(client, logger),
 		dataflattentable.TablePluginExec(client, logger, "kolide_windows_updates",
 			dataflattentable.JsonType,
 			[]string{"powershell.exe", "-NoProfile", "-NonInteractive", wuUpdatesPowershell},
