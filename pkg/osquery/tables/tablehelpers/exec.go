a88973ca6fc251f68a401141710487bdc5abd2b3
seph <seph@directionless.org>
2021-06-03 20:56:30 -0400

    search paths and tests

--- pkg/osquery/tables/tablehelpers/exec.go
+++ pkg/osquery/tables/tablehelpers/exec.go
@@ -3,6 +3,7 @@ package tablehelpers
 import (
 	"bytes"
 	"context"
+	"os"
 	"os/exec"
 	"time"
 
@@ -11,30 +12,46 @@ import (
 	"github.com/pkg/errors"
 )
 
-func Exec(ctx context.Context, logger log.Logger, bin string, args []string) ([]byte, error) {
-	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+// Exec is a wrapper over exec.CommandContext. It does a couple of
+// additional things to help with table usage:
+// 1. It enforces a timeout.
+// 2. Second, it accepts an array of possible binaries locations, and if something is not
+//    found, it will go down the list.
+// 3. It moves the stderr into the return error, if needed.
+//
+// This is not suitable for high performance work -- it allocates new buffers each time.
+func Exec(ctx context.Context, logger log.Logger, timeoutSeconds int, possibleBins []string, args []string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(timeoutSeconds)*time.Second)
 	defer cancel()
 
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 
-	cmd := exec.CommandContext(ctx, bin, args...)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
+	for _, bin := range possibleBins {
+		stdout.Reset()
+		stderr.Reset()
 
-	level.Debug(logger).Log(
-		"msg", "execing",
-		"cmd", cmd.String(),
-	)
+		cmd := exec.CommandContext(ctx, bin, args...)
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
 
-	if err := cmd.Run(); err != nil {
-		level.Info(logger).Log(
-			"msg", "Error execing",
+		level.Debug(logger).Log(
+			"msg", "execing",
 			"cmd", cmd.String(),
-			"stderr", string(stderr.Bytes()),
 		)
-		return nil, errors.Wrapf(err, "calling %s. Got: %s", bin, string(stderr.Bytes()))
-	}
 
-	return stdout.Bytes(), nil
+		switch err := cmd.Run(); {
+		case err == nil:
+			return stdout.Bytes(), nil
+		case os.IsNotExist(err):
+			// try the next binary
+			continue
+		default:
+			// an actual error
+			return nil, errors.Wrapf(err, "exec '%s'. Got: '%s'", cmd.String(), string(stderr.Bytes()))
+		}
+
+	}
+	// Getting here means no binary was found
+	return nil, errors.Errorf("No binary found is specified paths: %v", possibleBins)
 }
