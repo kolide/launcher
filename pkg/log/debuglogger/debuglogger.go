97f35a6739cc0db4fb1f67e68ac9b0f97d457878
seph <seph@directionless.org>
2022-01-06 11:29:00 -0500

    debuglogger with filtering

---
+++ pkg/log/debuglogger/debuglogger.go
@@ -0,0 +1,53 @@
+package debuglogger
+
+import (
+	"fmt"
+
+	"github.com/go-kit/kit/log"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+const (
+	truncatedFormatString = "%s[TRUNCATED]"
+)
+
+type debugLogger struct {
+	logger log.Logger
+}
+
+func NewKitLogger(logFilePath string) log.Logger {
+	// This is meant as an always available debug tool. Thus we hardcode these options
+	lj := &lumberjack.Logger{
+		Filename:   logFilePath,
+		MaxSize:    2, // megabytes
+		MaxBackups: 3,
+	}
+
+	dl := debugLogger{
+		logger: log.NewJSONLogger(log.NewSyncWriter(lj)),
+	}
+
+	return dl
+}
+
+func (dl debugLogger) Log(keyvals ...interface{}) error {
+	filterResults(keyvals...)
+	return dl.logger.Log(keyvals...)
+}
+
+// filterResults filteres out the osquery results,
+// which just make a lot of noise in our debug logs.
+// It's a bit fragile, since it parses keyvals, but
+// hopefully that's good enough
+func filterResults(keyvals ...interface{}) {
+	// Consider switching on `method` as well?
+	for i := 0; i < len(keyvals); i += 2 {
+		if keyvals[i] == "results" && len(keyvals) > i+1 {
+			str, ok := keyvals[i+1].(string)
+			if ok && len(str) > 100 {
+				keyvals[i+1] = fmt.Sprintf(truncatedFormatString, str[0:99])
+			}
+		}
+	}
+
+}
---
+++ pkg/log/debuglogger/debuglogger_test.go
@@ -0,0 +1,70 @@
+package debuglogger
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/kolide/kit/stringutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	reallyLongString    = stringutil.RandomString(200)
+	truncatedLongString = fmt.Sprintf(truncatedFormatString, reallyLongString[0:99])
+)
+
+func TestFilterResults(t *testing.T) {
+	t.Parallel()
+
+	data := []interface{}{
+		"one", "two",
+		"results", reallyLongString,
+	}
+
+	filterResults(data...)
+	assert.Len(t, data, 4)
+	assert.Equal(t, data[0], "one")
+	assert.Equal(t, data[1], "two")
+	assert.Equal(t, data[2], "results")
+	assert.Len(t, data[3], 110)
+	assert.Contains(t, data[3], "[TRUNCATED]")
+	assert.Equal(t, data[3], truncatedLongString)
+}
+
+func TestKitLogging(t *testing.T) {
+	t.Parallel()
+
+	data := []interface{}{
+		"one", "two",
+		"results", reallyLongString,
+	}
+
+	expected := map[string]string{
+		"one":     "two",
+		"results": truncatedLongString,
+	}
+	expectedJson, err := json.Marshal(expected)
+	require.NoError(t, err, "json marshal expected")
+
+	tmpfile, err := ioutil.TempFile("", "test-debuglogger")
+	require.NoError(t, err, "make temp file")
+	defer os.Remove(tmpfile.Name())
+
+	// we only need a file path, not the file handle
+	tmpfile.Close()
+
+	logger := NewKitLogger(tmpfile.Name())
+
+	logger.Log(data...)
+
+	contents, err := os.ReadFile(tmpfile.Name())
+	require.NoError(t, err, "read temp file")
+
+	assert.Equal(t, expectedJson, bytes.TrimSuffix(contents, []byte("\n")))
+
+}
