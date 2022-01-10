13071d98cfa63717b744a263c61bb215d0ed9039
seph <seph@directionless.org>
2022-01-10 12:51:40 -0500

    windows

--- pkg/debug/signal_debug_windows.go
+++ pkg/debug/signal_debug_windows.go
@@ -18,63 +18,50 @@ import (
 // alternate mechanism is used.
 func AttachDebugHandler(triggerPath string, addrPath string, logger log.Logger) {
 	go func() {
-		_, err := startDebugServer(addrPath, logger)
-		if err != nil {
-			level.Info(logger).Log(
-				"msg", "error starting debug server",
-				"err", err,
-			)
-		}
-		level.Info(logger).Log("msg", "starting debug server")
-	}()
+		var serv *http.Server
+		var err error
 
-	if false {
-		go func() {
-			var serv *http.Server
-			var err error
-
-			for {
-				_, statErr := os.Stat(triggerPath)
-				switch {
-				case statErr == nil:
-					if serv != nil {
-						continue
-					}
-
-					serv, err = startDebugServer(addrPath, logger)
-					if err != nil {
-						level.Info(logger).Log(
-							"msg", "error starting debug server",
-							"err", err,
-						)
-						continue
-					}
-					level.Info(logger).Log("msg", "starting debug server")
-
-				case os.IsNotExist(statErr):
-					if serv == nil {
-						continue
-					}
-
-					if err := serv.Shutdown(context.Background()); err != nil {
-						level.Info(logger).Log(
-							"msg", "error shutting down debug server",
-							"err", err,
-						)
-						continue
-					}
-
-					level.Info(logger).Log("msg", "stopped debug server")
-				default:
-					level.Info(logger).Log(
-						"msg", "Error checking debug server trigger",
-						"err", statErr,
-					)
+		for {
+			_, statErr := os.Stat(triggerPath)
+			switch {
+			case statErr == nil:
+				if serv != nil {
+					continue
 				}
 
-				time.Sleep(30 * time.Second)
+				serv, err = startDebugServer(addrPath, logger)
+				if err != nil {
+					level.Info(logger).Log(
+						"msg", "error starting debug server",
+						"err", err,
+					)
+					continue
+				}
+				level.Info(logger).Log("msg", "starting debug server")
+
+			case os.IsNotExist(statErr):
+				if serv == nil {
+					continue
+				}
+
+				if err := serv.Shutdown(context.Background()); err != nil {
+					level.Info(logger).Log(
+						"msg", "error shutting down debug server",
+						"err", err,
+					)
+					continue
+				}
+
+				level.Info(logger).Log("msg", "stopped debug server")
+			default:
+				level.Info(logger).Log(
+					"msg", "Error checking debug server trigger",
+					"err", statErr,
+				)
 			}
 
-		}()
-	}
+			time.Sleep(30 * time.Second)
+		}
+
+	}()
 }
