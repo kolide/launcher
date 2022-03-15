c06209e481625f6129a1eacfb9f59339db8c03c6
James Pickett <james@kolide.co>
2022-03-15 14:08:26 -0700

    removed unneeded commented out function

--- pkg/osquery/table/launcher_info.go
+++ pkg/osquery/table/launcher_info.go
@@ -51,26 +51,3 @@ func generateLauncherInfoTable(db *bbolt.DB) table.GenerateFunc {
 		return results, nil
 	}
 }
-
-// func LauncherIdentifierTable(db *bbolt.DB) *table.Plugin {
-// 	columns := []table.ColumnDefinition{
-// 		table.TextColumn("identifier"),
-// 	}
-// 	return table.NewPlugin("kolide_launcher_identifier", columns, generateLauncherIdentifier(db))
-// }
-
-// func generateLauncherIdentifier(db *bbolt.DB) table.GenerateFunc {
-// 	return func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
-// 		identifier, err := osquery.IdentifierFromDB(db)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		results := []map[string]string{
-// 			map[string]string{
-// 				"identifier": identifier,
-// 			},
-// 		}
-
-// 		return results, nil
-// 	}
-// }
