98f1660880a6c5fdc0441c6b8a0887e897a02e90
James Pickett <james@kolide.co>
2022-03-15 11:55:00 -0700

    added launcher identifer to kolide_launcher_info, but not working as expected

--- pkg/osquery/table/table.go
+++ pkg/osquery/table/table.go
@@ -19,9 +19,9 @@ func LauncherTables(db *bbolt.DB, opts *launcher.Options) []osquery.OsqueryPlugi
 	return []osquery.OsqueryPlugin{
 		LauncherConfigTable(db),
 		LauncherDbInfo(db),
-		LauncherIdentifierTable(db),
 		TargetMembershipTable(db),
 		LauncherAutoupdateConfigTable(opts),
+		LauncherInfoTable(db),
 	}
 }
 
@@ -34,7 +34,6 @@ func PlatformTables(client *osquery.ExtensionManagerClient, logger log.Logger, c
 		ChromeUserProfiles(client, logger),
 		EmailAddresses(client, logger),
 		KeyInfo(client, logger),
-		LauncherInfoTable(),
 		OnePasswordAccounts(client, logger),
 		SlackConfig(client, logger),
 		SshKeys(client, logger),
