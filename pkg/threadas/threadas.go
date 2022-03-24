f22259d9e00b1f1fefeed61efc64b197e3f3f80c
seph <seph@directionless.org>
2022-03-24 09:24:50 -0400

    tests

--- pkg/threadas/threadas.go
+++ pkg/threadas/threadas.go
@@ -1,10 +1,15 @@
-// Package threadsetuid uses some thread trickery to facilitate
+//go:build darwin
+// +build darwin
+
+// Package threadas uses some thread trickery to facilitate
 // dropping permissions on a single thread. This allows us to call
 // underlying darwin APIs which change behavior based on the userid.
 //
 // Note that this changes permission on a single thread. It cannot
 // change permission on something like osquery (communication is over
-// a socket), and may not do the right thing if you bring in an exec.
+// a socket), and may not do the right thing if you bring in an
+// exec. This also means that if you spawn a new thread, you'll get
+// the parent process permissions.
 //
 // This is based on ideas from:
 //   * https://github.com/golang/go/issues/14592
@@ -13,21 +18,20 @@
 //
 // There's future work in supporting other platforms. Linux may have
 // `syscall(SYS_setresuid, ...)` for this.
-package threadsetuid
+package threadas
 
-import(
-	"time"
-	"syscall"
+import (
 	"fmt"
 	"runtime"
+	"syscall"
+	"time"
 )
-
 
 // ThreadAs will run a function, in a "thread", after using setuid to
 // change permissions on that thread. It uses `LockOSThread` so the
 // thread terminates after with the function, this is to clean up from
 // the permissions change.
-func ThreadAs(fn func()  error, timeout time.Duration, uid uint32, gid uint32)  error {
+func ThreadAs(fn func() error, timeout time.Duration, uid uint32, gid uint32) error {
 	// As we only support calling the function once, so we can
 	// buffer a single size. Makes it a bit easier to
 	// sequence starting the child and our listener.
@@ -56,9 +60,9 @@ func ThreadAs(fn func()  error, timeout time.Duration, uid uint32, gid uint32)  
 	}()
 
 	select {
-	case err := <- errChan:
+	case err := <-errChan:
 		return err
-	case <- time.After(timeout):
+	case <-time.After(timeout):
 		return fmt.Errorf("Timeout after %s", timeout)
 	}
 }
@@ -69,15 +73,14 @@ func ThreadAs(fn func()  error, timeout time.Duration, uid uint32, gid uint32)  
 //
 // There is some discussion in the man page that this is not suitable
 // for security isolation. It's recommended you read and understand that.
-func pthread_setugid_np(uid uint32, gid uint32) (error) {
+func pthread_setugid_np(uid uint32, gid uint32) error {
 	// syscall.RawSyscall blocks, while Syscall does not. Since we
 	// don't think this blocks, we can use the _slightly_ more
 	// performant RawSyscall
 	_, _, errNo := syscall.RawSyscall(syscall.SYS_SETTID, uintptr(uid), uintptr(gid), 0)
 	if errNo != 0 {
 		return errNo
-	} 
+	}
 
 	return nil
 }
-
