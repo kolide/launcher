d7ee5b278810052ddbfe89398b89a6716fa86ee0
James Pickett <james@kolide.co>
2022-04-13 15:24:44 -0700

    made wmitable_tests sequential to stop flakey test

--- pkg/osquery/tables/wmitable/wmitable_test.go
+++ pkg/osquery/tables/wmitable/wmitable_test.go
@@ -112,8 +112,8 @@ func TestQueries(t *testing.T) {
 
 	for _, tt := range tests {
 		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
+		t.Run(tt.name, func(t *testing.T) { //nolint:paralleltest
+			// parallel makes test flakey, probably race condition somewhere
 
 			mockQC := tablehelpers.MockQueryContext(map[string][]string{
 				"class":       []string{tt.class},
