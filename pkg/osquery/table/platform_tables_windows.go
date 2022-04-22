f86bf511fb3da114a37a4f4855ab561ed992274e
seph <seph@kolide.co>
2022-04-22 12:18:13 -0400

    remove unused

--- pkg/osquery/table/platform_tables_windows.go
+++ pkg/osquery/table/platform_tables_windows.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/go-kit/kit/log"
 	osquery "github.com/osquery/osquery-go"
-	"github.com/osquery/osquery-go/plugin/table"
 )
 
 func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, currentOsquerydBinaryPath string) []osquery.OsqueryPlugin {
