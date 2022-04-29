6f1829efc4dacb3a1582d548afabada0f23928cc
James Pickett <james@kolide.co>
2022-04-29 13:33:18 -0700

    updated live debugging so that files used by launcher are in ignored folder directly in repo

--- .vscode/pre-debug.sh
+++ .vscode/pre-debug.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-osquerydPath=$(which osqueryd)
-mkdir -p /tmp/launcher-root-dev/bin
-yes | cp $osquerydPath /tmp/launcher-root-dev/bin/osqueryd
-
+mkdir -p ./debug/bin
+yes | cp $(which osqueryd) ./debug/bin/osqueryd
 make deps
 make build
-
 yes | cp build/osquery-extension.ext cmd/launcher/osquery-extension.ext
-
