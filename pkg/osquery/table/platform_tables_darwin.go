abeabd003ec0c1a4499bd4bd58ca6804a0330e41
Fritz <fritz.ifert@gmail.com>
2022-08-15 16:59:01 -0400

    Add crowdstrike falconctl stats exec table for macOS (#858)

--- pkg/osquery/table/platform_tables_darwin.go
+++ pkg/osquery/table/platform_tables_darwin.go
@@ -95,6 +95,8 @@ func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, c
 		dataflattentable.TablePluginExec(client, logger,
 			"kolide_diskutil_list", dataflattentable.PlistType, []string{"/usr/sbin/diskutil", "list", "-plist"}),
 		dataflattentable.TablePluginExec(client, logger,
+			"kolide_falconctl_stats", dataflattentable.PlistType, []string{"/Applications/Falcon.app/Contents/Resources/falconctl", "stats", "-p"}),
+		dataflattentable.TablePluginExec(client, logger,
 			"kolide_apfs_list", dataflattentable.PlistType, []string{"/usr/sbin/diskutil", "apfs", "list", "-plist"}),
 		dataflattentable.TablePluginExec(client, logger,
 			"kolide_apfs_users", dataflattentable.PlistType, []string{"/usr/sbin/diskutil", "apfs", "listUsers", "/", "-plist"}),
