c49400c2b61292b0cccffd849265cc29310e6a3f
James Pickett <james@kolide.co>
2022-08-24 10:56:33 -0700

    added systray shutdown to launcher auto update shutdown, broke some functions out of the main runLuncher func, replaced errors.Wrap with fmt.Errorf

--- cmd/launcher/launcher.go
+++ cmd/launcher/launcher.go
@@ -41,59 +41,34 @@ func runLauncher(ctx context.Context, cancel func(), opts *launcher.Options) err
 	logger := log.With(ctxlog.FromContext(ctx), "caller", log.DefaultCaller)
 	level.Debug(logger).Log("msg", "runLauncher starting")
 
-	// determine the root directory, create one if it's not provided
-	rootDirectory := opts.RootDirectory
-	if rootDirectory == "" {
-		rootDirectory = filepath.Join(os.TempDir(), defaultRootDirectory)
-		if _, err := os.Stat(rootDirectory); os.IsNotExist(err) {
-			if err := os.Mkdir(rootDirectory, fs.DirMode); err != nil {
-				return errors.Wrap(err, "creating temporary root directory")
-			}
-		}
-		level.Info(logger).Log(
-			"msg", "using default system root directory",
-			"path", rootDirectory,
-		)
-	}
-
-	if err := os.MkdirAll(rootDirectory, 0700); err != nil {
-		return errors.Wrap(err, "creating root directory")
+	rootDirectory, err := initRootDirectory(logger, opts)
+	if err != nil {
+		return fmt.Errorf("init root directory: %w", err)
 	}
 
 	if _, err := osquery.DetectPlatform(); err != nil {
-		return errors.Wrap(err, "detecting platform")
+		return fmt.Errorf("detecting platform: %w", err)
 	}
 
 	debugAddrPath := filepath.Join(rootDirectory, "debug_addr")
 	debug.AttachDebugHandler(debugAddrPath, logger)
 	defer os.Remove(debugAddrPath)
 
-	// construct the appropriate http client based on security settings
-	httpClient := http.DefaultClient
-	if opts.InsecureTLS {
-		httpClient = &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: true,
-				},
-			},
-		}
-	}
+	httpClient := initHttpClient(opts)
 
 	// open the database for storing launcher data, we do it here
 	// because it's passed to multiple actors. Add a timeout to
 	// this. Note that the timeout is documented as failing
 	// unimplemented on windows, though empirically it seems to
 	// work.
-	boltOptions := &bbolt.Options{Timeout: time.Duration(30) * time.Second}
-	db, err := bbolt.Open(filepath.Join(rootDirectory, "launcher.db"), 0600, boltOptions)
+	db, err := initBoltDb(rootDirectory)
 	if err != nil {
-		return errors.Wrap(err, "open launcher db")
+		return fmt.Errorf("init bolt db: %w", err)
 	}
 	defer db.Close()
 
 	if err := writePidFile(filepath.Join(rootDirectory, "launcher.pid")); err != nil {
-		return errors.Wrap(err, "write launcher pid to file")
+		return fmt.Errorf("writing launcher pid to file: %w", err)
 	}
 
 	// If we have successfully opened the DB, and written a pid,
@@ -104,71 +79,45 @@ func runLauncher(ctx context.Context, cancel func(), opts *launcher.Options) err
 	// Try to ensure useful info in the logs
 	checkpoint.Run(logger, db, *opts)
 
-	// create the certificate pool
-	var rootPool *x509.CertPool
-	if opts.RootPEM != "" {
-		rootPool = x509.NewCertPool()
-		pemContents, err := ioutil.ReadFile(opts.RootPEM)
-		if err != nil {
-			return errors.Wrapf(err, "reading root certs PEM at path: %s", opts.RootPEM)
-		}
-		if ok := rootPool.AppendCertsFromPEM(pemContents); !ok {
-			return errors.Errorf("found no valid certs in PEM at path: %s", opts.RootPEM)
-		}
+	rootPool, err := createCertificatePool(opts)
+	if err != nil {
+		return fmt.Errorf("creating certificate pool: %w", err)
 	}
+
 	// create a rungroup for all the actors we create to allow for easy start/stop
 	var runGroup run.Group
 
-	// Create a channel for signals
-	sigChannel := make(chan os.Signal, 1)
-
-	// Add a rungroup to catch things on the sigChannel
-	// Attach a notifier for os.Interrupt
-	runGroup.Add(func() error {
-		signal.Notify(sigChannel, os.Interrupt)
-		select {
-		case <-sigChannel:
-			level.Info(logger).Log("msg", "beginnning shutdown via signal")
-			return nil
-		}
-	}, func(err error) {
-		level.Info(logger).Log("msg", "interrupted", "err", err)
-		level.Debug(logger).Log("msg", "interrupted", "err", err, "stack", fmt.Sprintf("%+v", err))
-		cancel()
-		close(sigChannel)
-	})
+	sigChannel := handleSignals(logger, cancel, &runGroup)
 
 	var client service.KolideService
-	{
-		switch opts.Transport {
-		case "grpc":
-			grpcConn, err := service.DialGRPC(opts.KolideServerURL, opts.InsecureTLS, opts.InsecureTransport, opts.CertPins, rootPool, logger)
-			if err != nil {
-				return errors.Wrap(err, "dialing grpc server")
-			}
-			defer grpcConn.Close()
-			client = service.NewGRPCClient(grpcConn, logger)
-			queryTargeter := createQueryTargetUpdater(logger, db, grpcConn)
-			runGroup.Add(queryTargeter.Execute, queryTargeter.Interrupt)
-		case "jsonrpc":
-			client = service.NewJSONRPCClient(opts.KolideServerURL, opts.InsecureTLS, opts.InsecureTransport, opts.CertPins, rootPool, logger)
-		case "osquery":
-			client = service.NewNoopClient(logger)
-		default:
-			return errors.New("invalid transport option selected")
+	switch opts.Transport {
+	case "grpc":
+		grpcConn, err := service.DialGRPC(opts.KolideServerURL, opts.InsecureTLS, opts.InsecureTransport, opts.CertPins, rootPool, logger)
+		if err != nil {
+			return fmt.Errorf("dialing grpc server: %w", err)
 		}
+		defer grpcConn.Close()
+		client = service.NewGRPCClient(grpcConn, logger)
+		queryTargeter := createQueryTargetUpdater(logger, db, grpcConn)
+		runGroup.Add(queryTargeter.Execute, queryTargeter.Interrupt)
+	case "jsonrpc":
+		client = service.NewJSONRPCClient(opts.KolideServerURL, opts.InsecureTLS, opts.InsecureTransport, opts.CertPins, rootPool, logger)
+	case "osquery":
+		client = service.NewNoopClient(logger)
+	default:
+		return errors.New("invalid transport option selected")
 	}
 
 	// init osquery instance history
 	err = osqueryInstanceHistory.InitHistory(db)
 	if err != nil {
-		return errors.Wrap(err, "error initializing osquery instance history")
+		return fmt.Errorf("init osquery instance history: %w", err)
 	}
 
 	// create the osquery extension for launcher. This is where osquery itself is launched.
 	extension, runnerRestart, runnerShutdown, err := createExtensionRuntime(ctx, db, client, opts)
 	if err != nil {
-		return errors.Wrap(err, "create extension with runtime")
+		return fmt.Errorf("creating extension with runtime: %w", err)
 	}
 	runGroup.Add(extension.Execute, extension.Interrupt)
 
@@ -183,13 +132,19 @@ func runLauncher(ctx context.Context, cancel func(), opts *launcher.Options) err
 	if opts.Control {
 		control, err := createControl(ctx, db, logger, opts)
 		if err != nil {
-			return errors.Wrap(err, "create control actor")
+			return fmt.Errorf("creating control actor: %w", err)
 		}
 		if control != nil {
 			runGroup.Add(control.Execute, control.Interrupt)
 		} else {
 			level.Info(logger).Log("msg", "got nil control actor. Ignoring")
 		}
+	}
+
+	var systrayRunner *systrayruntime.SystrayUsersProcessesRunner
+	if (opts.KolideServerURL == "k2device-preprod.kolide.com" || opts.KolideServerURL == "localhost:3443") && runtime.GOOS == "darwin" {
+		systrayRunner = systrayruntime.New(logger, time.Second*5)
+		runGroup.Add(systrayRunner.Execute, systrayRunner.Interrupt)
 	}
 
 	// If the autoupdater is enabled, enable it for both osquery and launcher
@@ -210,7 +165,7 @@ func runLauncher(ctx context.Context, cancel func(), opts *launcher.Options) err
 		// create an updater for osquery
 		osqueryUpdater, err := updater.NewUpdater(ctx, opts.OsquerydPath, runnerRestart, osqueryUpdaterconfig)
 		if err != nil {
-			return errors.Wrap(err, "create osquery updater")
+			return fmt.Errorf("creating osquery updater: %w", err)
 		}
 		runGroup.Add(osqueryUpdater.Execute, osqueryUpdater.Interrupt)
 
@@ -235,25 +190,115 @@ func runLauncher(ctx context.Context, cancel func(), opts *launcher.Options) err
 		launcherUpdater, err := updater.NewUpdater(
 			ctx,
 			launcherPath,
-			updater.UpdateFinalizer(logger, runnerShutdown),
+			updater.UpdateFinalizer(logger, func() error {
+				// reboot systray on auto update
+				if systrayRunner != nil {
+					systrayRunner.Interrupt(nil)
+				}
+				return runnerShutdown()
+			}),
 			launcherUpdaterconfig,
 		)
 		if err != nil {
-			return errors.Wrap(err, "create launcher updater")
+			return fmt.Errorf("creating launcher updater: %w", err)
 		}
 		runGroup.Add(launcherUpdater.Execute, launcherUpdater.Interrupt)
 	}
 
-	if (opts.KolideServerURL == "k2device-preprod.kolide.com" || opts.KolideServerURL == "localhost:3443") && runtime.GOOS == "darwin" {
-		systrayRunner := systrayruntime.New(logger, time.Second*5)
-		runGroup.Add(systrayRunner.Execute, systrayRunner.Interrupt)
-	}
-
 	err = runGroup.Run()
-	return errors.Wrap(err, "run service")
+	return fmt.Errorf("running service: %w", err)
 }
 
 func writePidFile(path string) error {
 	err := ioutil.WriteFile(path, []byte(strconv.Itoa(os.Getpid())), 0600)
-	return errors.Wrap(err, "writing pidfile")
+	if err != nil {
+		return fmt.Errorf("writing pid file: %w", err)
+	}
+	return nil
+}
+
+func initRootDirectory(logger log.Logger, opts *launcher.Options) (string, error) {
+	if opts.RootDirectory != "" {
+		return opts.RootDirectory, nil
+	}
+
+	rootDirectory := filepath.Join(os.TempDir(), defaultRootDirectory)
+	if _, err := os.Stat(rootDirectory); os.IsNotExist(err) {
+		if err := os.Mkdir(rootDirectory, fs.DirMode); err != nil {
+			return "", fmt.Errorf("creating temporary root directory: %w", err)
+		}
+	}
+	level.Info(logger).Log(
+		"msg", "using default system root directory",
+		"path", rootDirectory,
+	)
+
+	if err := os.MkdirAll(rootDirectory, 0700); err != nil {
+		return "", fmt.Errorf("creating root directory: %w", err)
+	}
+
+	return rootDirectory, nil
+}
+
+func initHttpClient(opts *launcher.Options) *http.Client {
+	// construct the appropriate http client based on security settings
+	httpClient := http.DefaultClient
+	if opts.InsecureTLS {
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+		}
+	}
+	return httpClient
+}
+
+func initBoltDb(dir string) (*bbolt.DB, error) {
+	boltOptions := &bbolt.Options{Timeout: time.Duration(30) * time.Second}
+	db, err := bbolt.Open(filepath.Join(dir, "launcher.db"), 0600, boltOptions)
+	if err != nil {
+		return nil, fmt.Errorf("opening launcher db: %w", err)
+	}
+	return db, nil
+}
+
+func createCertificatePool(opts *launcher.Options) (*x509.CertPool, error) {
+	// create the certificate pool
+	var rootPool *x509.CertPool
+	if opts.RootPEM != "" {
+		rootPool = x509.NewCertPool()
+		pemContents, err := ioutil.ReadFile(opts.RootPEM)
+		if err != nil {
+			return nil, fmt.Errorf("reading root certs PEM at path %s: %w", opts.RootPEM, err)
+		}
+		if ok := rootPool.AppendCertsFromPEM(pemContents); !ok {
+			return nil, fmt.Errorf("appending root certs PEM at path %s: %w", opts.RootPEM, err)
+		}
+	}
+	return rootPool, nil
+}
+
+func handleSignals(logger log.Logger, cancel func(), runGroup *run.Group) chan os.Signal {
+	// Create a channel for signals
+	sigChannel := make(chan os.Signal, 1)
+
+	// Add a rungroup to catch things on the sigChannel
+	// Attach a notifier for os.Interrupt
+	runGroup.Add(func() error {
+		signal.Notify(sigChannel, os.Interrupt)
+		select {
+		case <-sigChannel:
+			level.Info(logger).Log("msg", "beginnning shutdown via signal")
+			return nil
+		}
+	}, func(err error) {
+		level.Info(logger).Log("msg", "interrupted", "err", err)
+		level.Debug(logger).Log("msg", "interrupted", "err", err, "stack", fmt.Sprintf("%+v", err))
+		cancel()
+		close(sigChannel)
+	})
+
+	return sigChannel
 }
