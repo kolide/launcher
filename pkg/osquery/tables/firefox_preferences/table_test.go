5b06db081b7bd889d29c700c7612d7f354c69ce5
Hunter Braun <hunter.braun@gmail.com>
2022-08-24 18:02:39 -0500

    Add more test cases

--- pkg/osquery/tables/firefox_preferences/table_test.go
+++ pkg/osquery/tables/firefox_preferences/table_test.go
@@ -11,19 +11,38 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_generateData_HappyPath(t *testing.T) {
+func Test_generateData(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
 		name                    string
-		filepath                string
+		filepaths               []string
 		expectedResultsFilePath string
-		errContains             string
+		query string
 	}{
 		{
-			name:                    "happy path",
-			filepath:                "testdata/prefs.js",
-			expectedResultsFilePath: "testdata/output.json",
+			name: "no path",
+		},
+		{
+			name:                    "single path",
+			filepaths:               []string{"testdata/prefs.js"},
+			expectedResultsFilePath: "testdata/output.single_path.json",
+		},
+		{
+			name:                    "single path with query",
+			filepaths:               []string{"testdata/prefs.js"},
+			expectedResultsFilePath: "testdata/output.single_path_with_query.json",
+			query: "app.normandy.first_run",
+		},
+		{
+			name:                    "multiple paths",
+			filepaths:               []string{"testdata/prefs.js", "testdata/prefs2.js"},
+			expectedResultsFilePath: "testdata/output.multiple_paths.json",
+		},
+		{
+			name:                    "file with bad data",
+			filepaths:               []string{"testdata/prefs3.js"},
+			expectedResultsFilePath: "testdata/output.file_with_bad_data.json",
 		},
 	}
 
@@ -33,17 +52,22 @@ func Test_generateData_HappyPath(t *testing.T) {
 			t.Parallel()
 
 			constraints := make(map[string][]string)
-			constraints["path"] = append(constraints["path"], tt.filepath)
+			constraints["path"] = append(constraints["path"], tt.filepaths...)
+			if tt.query != "" {
+				constraints["query"] = append(constraints["query"], tt.query)
+			}
 
-			got, err := generateData(tablehelpers.MockQueryContext(constraints), log.NewNopLogger())
-			require.NoError(t, err)
-
-			wantBytes, err := os.ReadFile(tt.expectedResultsFilePath)
-			require.NoError(t, err)
+			got := generateData(tablehelpers.MockQueryContext(constraints), log.NewNopLogger())
 
 			var want []map[string]string
-			err = json.Unmarshal(wantBytes, &want)
-			require.NoError(t, err)
+
+			if len(tt.filepaths) != 0 {
+				wantBytes, err := os.ReadFile(tt.expectedResultsFilePath)
+				require.NoError(t, err)
+
+				err = json.Unmarshal(wantBytes, &want)
+				require.NoError(t, err)
+			}
 
 			assert.ElementsMatch(t, want, got)
 		})
