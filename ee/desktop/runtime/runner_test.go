32dd3e3679e9073185d80da305c6897c6b898ef4
james pickett <james@kolide.co>
2022-09-06 10:25:28 -0700

    gofmt

--- ee/desktop/runtime/runner_test.go
+++ ee/desktop/runtime/runner_test.go
@@ -20,7 +20,7 @@ func TestDesktopUserProcessRunner_Execute(t *testing.T) {
 	if runtime.GOOS == "linux" {
 		t.Skip("skipping linux test because it's not implemented")
 	}
-  
+
 	// When running this using the golang test harness, it will leave behind process if you do not build the binary first.
 	// On mac os you can find these by setting the executable path to an empty string before running the tests, then search
 	// the processes in a terminal using: ps aux -o ppid | runtime.test after the tests have completed, you'll also see the
