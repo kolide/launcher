4c811579a4c828b41d7d12a33a617c9dda05b4d9
james pickett <james@kolide.co>
2022-09-15 08:01:24 -0700

    fixed files and build tags for non windows

--- ee/desktop/runtime/runner_posix.go
+++ ee/desktop/runtime/runner_posix.go
@@ -3,6 +3,13 @@
 
 package runtime
 
+import (
+	"context"
+	"net"
+
+	"github.com/kolide/launcher/ee/desktop"
+)
+
 func dialContext(pid int) func(_ context.Context, _, _ string) (net.Conn, error) {
 	return func(_ context.Context, _, _ string) (net.Conn, error) {
 		return net.Dial("unix", desktop.DesktopSocketPath(pid))
--- ee/desktop/server/server_posix.go
+++ ee/desktop/server/server_posix.go
@@ -3,6 +3,8 @@
 
 package server
 
+import "net"
+
 func listener(socketPath string) (net.Listener, error) {
 	return net.Listen("unix", socketPath)
 }
