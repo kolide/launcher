f3fab1e31116c14a05b1900f91bdd84bb4ff0a9e
Hunter Braun <hunter.braun@gmail.com>
2022-09-20 17:29:55 -0500

    Handle special case for "tags" key

--- pkg/osquery/tables/crowdstrike/falconctlstats/table.go
+++ pkg/osquery/tables/crowdstrike/falconctlstats/table.go
@@ -1,7 +1,9 @@
+// TODO: Add linux build comments
 package falconctlstats
 
 import (
 	"context"
+	"encoding/json"
 	"regexp"
 	"strings"
 
@@ -21,7 +23,7 @@ type Table struct {
 // TODO: Change table name to kolide_falconctl_stats
 const tableName = "kolide_crowdstrike"
 
-var re = regexp.MustCompile(`\s?=\s?|\s`)
+var re = regexp.MustCompile(`\s?=\s?|\sis\s`)
 
 func TablePlugin(logger log.Logger) *osquerygotable.Plugin {
 	columns := dataflattentable.Columns()
@@ -35,33 +37,37 @@ func TablePlugin(logger log.Logger) *osquerygotable.Plugin {
 }
 
 func (t *Table) generate(ctx context.Context, queryContext osquerygotable.QueryContext) ([]map[string]string, error) {
-	// TODO: Replace this with the output of the real command
-	// output := "aid is not set, aph is not set, app is not set, rfm-state is not set, rfm-reason is not set, feature is not set, metadata-query=enable (unset default), version = 6.38.13501.0"
+	output := `cid="c1196fd3ca9944f39681d1c9c49124a7", aid="c2bbdbb81bc84d3c9bcb1542f16fb020", apd is not set, aph is not set, app is not set, rfm-state=true, rfm-reason=Unspecified, code=0xC0000225, trace is not set, feature= (hex bitmask: 0), billing is not set, tags=kolide-test-1,kolide-test-2, provisioning-token is not set.`
 
-	options := []string{
-		"-g",
-		"--rfm-state",
-		"--aid",
-		"--version",
-		"--metadata-query",
-		"--rfm-reason",
-		"--tags",
-		"--feature",
-		"--app",
-		"--aph",
-		"--provisioning-token",
-		"--systags",
-		"--cid",
-	}
-	output, err := tablehelpers.Exec(context.Background(), t.logger, 30, []string{"/opt/CrowdStrike/falconctl"}, options)
-	if err != nil {
-		panic(err)
-	}
+	// options := []string{
+	// 	"-g",
+	// 	"--cid",
+	// 	"--aid",
+	// 	"--apd",
+	// 	"--aph",
+	// 	"--app",
+	// 	"--rfm-state",
+	// 	"--rfm-reason",
+	// 	"--trace",
+	// 	"--feature",
+	// 	"--metadata-query",
+	// 	"--version",
+	// 	"--message-log",
+	// 	"--billing",
+	// 	"--tags",
+	// 	"--provisioning-token",
+	// 	"--systags",
+	// }
+	// output, err := tablehelpers.Exec(context.Background(), t.logger, 30, []string{"/opt/CrowdStrike/falconctl"}, options)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	return parse(queryContext, t.logger, string(output))
 }
 
 func parse(queryContext osquerygotable.QueryContext, logger log.Logger, output string) ([]map[string]string, error) {
+	// TODO: Only parse first line
 	// TODO: Add case for when CID not set
 	var results []map[string]string
 
@@ -75,10 +81,15 @@ func parse(queryContext osquerygotable.QueryContext, logger log.Logger, output s
 
 		for _, data := range dataArr {
 			a := re.Split(data, 2)
-			rawKeyVals[a[0]] = a[1]
+			if a[0] == "tags" {
+				rawKeyVals[a[0]] = strings.Split(a[1], ",")
+			} else {
+				rawKeyVals[a[0]] = a[1]
+			}
 		}
 
-		flatData, err := dataflatten.Flatten(rawKeyVals, flattenOpts...)
+		input, _ := json.Marshal(rawKeyVals)
+		flatData, err := dataflatten.Json(input, flattenOpts...)
 		if err != nil {
 			level.Debug(logger).Log(
 				"msg", "failed to flatten data",
