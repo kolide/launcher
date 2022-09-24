4e326dc557f80eda04c8896a82f7f18f0da91b2f
seph <seph@kolide.co>
2022-09-23 22:17:54 -0400

    merge exec and execparse

--- pkg/osquery/tables/dataflattentable/exec_and_parse.go
+++ pkg/osquery/tables/dataflattentable/exec_and_parse.go
@@ -2,17 +2,22 @@ package dataflattentable
 
 import (
 	"bytes"
-	"context"
-	"os"
-	"strings"
+	"fmt"
+	"io"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/kolide/launcher/pkg/dataflatten"
-	"github.com/kolide/launcher/pkg/osquery/tables/tablehelpers"
 	"github.com/osquery/osquery-go/plugin/table"
-	"github.com/pkg/errors"
 )
+
+type flattenBytesInt interface {
+	FlattenBytes([]byte, ...dataflatten.FlattenOpts) ([]dataflatten.Row, error)
+}
+
+type parserInt interface {
+	Parse(io.Reader) (any, error)
+}
 
 func NewExecAndParseTable(
 	logger log.Logger,
@@ -23,51 +28,32 @@ func NewExecAndParseTable(
 
 ) *table.Plugin {
 	t := &Table{
-		logger:     level.NewFilter(log.With(logger, "table", tableName), level.AllowInfo()),
-		tableName:  tableName,
-		dataParser: parser,
-		execArgs:   execArgs,
+		logger:           level.NewFilter(log.With(logger, "table", tableName), level.AllowInfo()),
+		tableName:        tableName,
+		flattenBytesFunc: flattenerFromParser(parser).FlattenBytes,
+		execArgs:         execArgs,
 	}
 
 	for _, opt := range opts {
 		opt(t)
 	}
 
-	return table.NewPlugin(t.tableName, Columns(), t.generateExecAndParse)
+	return table.NewPlugin(t.tableName, Columns(), t.generateExec)
 }
 
-func (t *Table) generateExecAndParse(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
-	var results []map[string]string
+type parserFlattener struct {
+	parser parserInt
+}
 
-	execBytes, err := t.exec(ctx)
+func flattenerFromParser(parser parserInt) flattenBytesInt {
+	return &parserFlattener{parser: parser}
+}
+
+func (p *parserFlattener) FlattenBytes(raw []byte, flattenOpts ...dataflatten.FlattenOpts) ([]dataflatten.Row, error) {
+	data, err := p.parser.Parse(bytes.NewReader(raw))
 	if err != nil {
-		// exec will error if there's no binary, so we never want to record that
-		if os.IsNotExist(errors.Cause(err)) {
-			return nil, nil
-		}
-
-		// If the exec failed for some reason, it's probably better to return no results, and log the,
-		// error. Returning an error here will cause a table failure, and thus break joins
-		level.Info(t.logger).Log("msg", "failed to exec", "err", err)
-		return nil, nil
+		return nil, fmt.Errorf("error parsing data: %w", err)
 	}
 
-	data, err := t.dataParser.Parse(bytes.NewReader(execBytes))
-	if err != nil {
-		level.Info(t.logger).Log("msg", "failed to parse", "err", err)
-		return nil, nil
-	}
-
-	for _, dataQuery := range tablehelpers.GetConstraints(queryContext, "query", tablehelpers.WithDefaults("*")) {
-		// As we work with this, we might find that Flatten is missing some type handling. Bouncing through JSON is
-		// a potential workaround
-		flattened, err := dataflatten.Flatten(data, dataflatten.WithLogger(t.logger), dataflatten.WithQuery(strings.Split(dataQuery, "/")))
-		if err != nil {
-			level.Info(t.logger).Log("msg", "Error flattening data", "err", err)
-			continue
-		}
-		results = append(results, ToMap(flattened, dataQuery, nil)...)
-	}
-
-	return results, nil
+	return dataflatten.Flatten(data, flattenOpts...)
 }
