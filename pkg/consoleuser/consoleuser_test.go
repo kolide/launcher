6517c081d5ebd05e735598e05af0d3395d8a396c
james pickett <james@kolide.co>
2022-10-05 07:33:01 -0700

    adds test for darwin console user

---
+++ pkg/consoleuser/consoleuser_test.go
@@ -0,0 +1,31 @@
+//go:build darwin
+// +build darwin
+
+package consoleuser
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCurrentUids(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "happy path",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			uids, err := CurrentUids(context.Background())
+			assert.NoError(t, err)
+			assert.GreaterOrEqual(t, len(uids), 1, "should have at least one console user")
+		})
+	}
+}
