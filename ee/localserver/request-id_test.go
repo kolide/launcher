e1b1deaa8d621283c7c6f6dd389a5f648f0ff9a7
james pickett <james@kolide.co>
2022-10-07 08:09:53 -0700

    simplified request-id tests

--- ee/localserver/request-id_test.go
+++ ee/localserver/request-id_test.go
@@ -22,45 +22,32 @@ import (
 
 func Test_localServer_requestIdHandler(t *testing.T) {
 	t.Parallel()
-	tests := []struct {
-		name string
-	}{
-		{
-			name: "happy path",
-		},
+
+	var logBytes bytes.Buffer
+	server := testServer(t, &logBytes)
+
+	req, err := http.NewRequest("", "", nil)
+	require.NoError(t, err)
+
+	handler := http.HandlerFunc(server.requestIdHandlerFunc)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	assert.Empty(t, logBytes.String())
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	// convert the response to a struct
+	var response requestIdsResponse
+	require.NoError(t, json.Unmarshal(rr.Body.Bytes(), &response))
+
+	// in the current CI environment (GitHub Actions) the linux runner
+	// does not have a console user, so we expect an empty list
+	if os.Getenv("CI") == "true" && runtime.GOOS == "linux" {
+		assert.Empty(t, response.ConsoleUsers)
+		return
 	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
 
-			var logBytes bytes.Buffer
-			server := testServer(t, &logBytes)
-
-			req, err := http.NewRequest("", "", nil)
-			require.NoError(t, err)
-
-			handler := http.HandlerFunc(server.requestIdHandlerFunc)
-			rr := httptest.NewRecorder()
-			handler.ServeHTTP(rr, req)
-
-			assert.Empty(t, logBytes.String())
-			assert.Equal(t, http.StatusOK, rr.Code)
-
-			// convert the response to a struct
-			var response requestIdsResponse
-			require.NoError(t, json.Unmarshal(rr.Body.Bytes(), &response))
-
-			// in the current CI environment (GitHub Actions) the linux runner
-			// does not have a console user, so we expect an empty list
-			if os.Getenv("CI") == "true" && runtime.GOOS == "linux" {
-				assert.Empty(t, response.ConsoleUsers)
-				return
-			}
-
-			assert.GreaterOrEqual(t, len(response.ConsoleUsers), 1, "should have at least one console user")
-		})
-	}
+	assert.GreaterOrEqual(t, len(response.ConsoleUsers), 1, "should have at least one console user")
 }
 
 func testServer(t *testing.T, logBytes *bytes.Buffer) *localServer {
