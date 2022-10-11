81fe6cc1651d37abf45b282147030b5ea5d316f5
CJ Barrett <cj@kolide.co>
2022-10-11 12:05:25 -0400

    Added a simple test to assert table generation doesn't fail.

---
+++ pkg/osquery/tables/macos_software_update/recommended_updates_table_test.go
@@ -0,0 +1,20 @@
+package macos_software_update
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/kolide/launcher/pkg/osquery/tables/tablehelpers"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_generateRecommendedUpdatesHappyPath(t *testing.T) {
+	table := Table{logger: log.NewNopLogger()}
+	t.Run("HappyPath", func(t *testing.T) {
+		_, err := table.generate(context.Background(), tablehelpers.MockQueryContext(nil))
+
+		// Since the output is dynamic and can be empty, just verify no error
+		require.NoError(t, err)
+	})
+}
