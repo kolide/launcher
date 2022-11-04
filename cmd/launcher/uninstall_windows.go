67323313c1f9fa7f0128a7375a2e2d0b90beb0ad
CJ Barrett <cj@kolide.co>
2022-11-04 10:25:24 -0400

    Removed Windows uninstall support.

--- cmd/launcher/uninstall_windows.go
+++ cmd/launcher/uninstall_windows.go
@@ -5,16 +5,10 @@ package main
 
 import (
 	"context"
-	"os/exec"
+	"github.com/pkg/errors"
 )
 
 func removeLauncher(ctx context.Context, identifier string) error {
-	// Launch the Windows Settings app using the ms-settings: URI scheme
-	// https://learn.microsoft.com/en-us/windows/uwp/launch-resume/launch-settings-app#apps
-	cmd := exec.CommandContext(ctx, "start", []string{"ms-settings:appsfeatures"}...)
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-
-	return nil
+	// Uninstall is not implemented for Windows - users have to use add/remove programs themselves
+	return errors.Errorf("Uninstall subcommand is not supported for Windows platforms.")
 }
