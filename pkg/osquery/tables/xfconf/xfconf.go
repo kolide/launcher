fbb63029cff1fa41fcf55abe66a3d5cc2f5d824f
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2022-11-22 16:40:33 -0500

    Add kolide_xfconf table for devices using xfce

---
+++ pkg/osquery/tables/xfconf/xfconf.go
@@ -0,0 +1,218 @@
+//go:build !windows
+// +build !windows
+
+package xfconf
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os/exec"
+	"os/user"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/kolide/launcher/pkg/osquery/tables/tablehelpers"
+	"github.com/osquery/osquery-go/plugin/table"
+)
+
+// Provides configuration settings for devices using XFCE desktop environment.
+// See https://docs.xfce.org/xfce/xfconf/xfconf-query for documentation on the
+// tool we use to query this data.
+
+const xfconfQueryPath = "/usr/bin/xfconf-query"
+
+type XfconfQuerier struct {
+	logger log.Logger
+}
+
+func TablePlugin(logger log.Logger) *table.Plugin {
+	columns := []table.ColumnDefinition{
+		table.TextColumn("channel"),
+		table.TextColumn("key"),
+		table.TextColumn("value"),
+		table.TextColumn("username"),
+	}
+
+	t := &XfconfQuerier{
+		logger: logger,
+	}
+
+	return table.NewPlugin("kolide_xfconf", columns, t.generate)
+}
+
+func (t *XfconfQuerier) generate(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+	var results []map[string]string
+
+	users := tablehelpers.GetConstraints(queryContext, "username")
+	if len(users) < 1 {
+		return results, errors.New("kolide_xfconf requires at least one username to be specified")
+	}
+
+	for _, username := range users {
+		u, err := user.Lookup(username)
+		if err != nil {
+			return nil, fmt.Errorf("finding user by username '%s': %w", username, err)
+		}
+
+		channels, err := t.getChannels(ctx, u, queryContext)
+		if err != nil {
+			return nil, fmt.Errorf("finding channels for user %s: %w", username, err)
+		}
+
+		for _, channel := range channels {
+			properties, err := t.getProperties(ctx, channel, u, queryContext)
+			if err != nil {
+				return nil, fmt.Errorf("finding properties on channel %s for user %s: %w", channel, username, err)
+			}
+
+			results = append(results, properties...)
+		}
+	}
+
+	return results, nil
+}
+
+func (t *XfconfQuerier) getChannels(ctx context.Context, u *user.User, queryContext table.QueryContext) ([]string, error) {
+	listChannelsCmd := exec.CommandContext(ctx, xfconfQueryPath, "--list")
+	var output bytes.Buffer
+	if err := setUserForCommandAndRun(listChannelsCmd, u, &output); err != nil {
+		return nil, fmt.Errorf("listing channels for user %s: %w", u.Name, err)
+	}
+
+	// Output looks like:
+	// Channels:
+	// 		xfce4-session
+	//		xsettings
+	//
+	// Skip first line, and strip whitespace
+	scanner := bufio.NewScanner(&output)
+	channels := make([]string, 0)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" || line == "Channels:" {
+			continue
+		}
+
+		channels = append(channels, line)
+	}
+
+	return tablehelpers.GetConstraints(queryContext, "channel", tablehelpers.WithAllowedValues(channels)), nil
+}
+
+func (t *XfconfQuerier) getProperties(ctx context.Context, channel string, u *user.User, queryContext table.QueryContext) ([]map[string]string, error) {
+	cmdArgs := []string{
+		"--channel", channel,
+	}
+
+	// If the query specified a property, then query for that only
+	propertyConstraints := tablehelpers.GetConstraints(queryContext, "key")
+	if len(propertyConstraints) == 1 {
+		cmdArgs = append(cmdArgs, "--property", propertyConstraints[0])
+	}
+
+	// Including --verbose will get us properties paired with their values
+	cmdArgs = append(cmdArgs, "--list", "--verbose")
+
+	listPropertiesCmd := exec.CommandContext(ctx, xfconfQueryPath, cmdArgs...)
+	var output bytes.Buffer
+	if err := setUserForCommandAndRun(listPropertiesCmd, u, &output); err != nil {
+		return nil, fmt.Errorf("listing properties on channel %s for user %s: %w", channel, u.Name, err)
+	}
+
+	return t.parseProperties(&output, channel, u), nil
+}
+
+func (t *XfconfQuerier) parseProperties(input io.Reader, channel string, u *user.User) []map[string]string {
+	var results []map[string]string
+
+	scanner := bufio.NewScanner(input)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+
+		// Each line is in the following format:
+		// /some/path/to/setting		setting-value
+		// Split the line on the whitespace to obtain the key and value.
+		parts := strings.Fields(line)
+		if len(parts) > 2 || len(parts) == 0 {
+			level.Error(t.logger).Log(
+				"msg", "unable to process line -- invalid number of values",
+				"line", line,
+			)
+			continue
+		}
+		fullKey := parts[0]
+
+		var val string
+		if len(parts) == 2 {
+			val = parts[1]
+		}
+
+		row := make(map[string]string)
+		row["channel"] = channel
+		row["key"] = fullKey
+		row["value"] = val
+		row["username"] = u.Name
+
+		results = append(results, row)
+	}
+
+	if err := scanner.Err(); err != nil {
+		level.Debug(t.logger).Log("msg", "scanner error", "err", err)
+	}
+
+	return results
+}
+
+func setUserForCommandAndRun(cmd *exec.Cmd, u *user.User, output *bytes.Buffer) error {
+	// Set the HOME for the cmd so that the command is executed as the intended user
+	cmd.Env = append(cmd.Env, fmt.Sprintf("HOME=%s", u.HomeDir))
+
+	// Check if the supplied UID is that of the current user
+	currentUser, err := user.Current()
+	if err != nil {
+		return fmt.Errorf("checking current user uid: %w", err)
+	}
+
+	// Nothing else to do here
+	if u.Uid == currentUser.Uid {
+		return nil
+	}
+
+	// Set UID, GID on command
+	uid, err := strconv.ParseInt(u.Uid, 10, 32)
+	if err != nil {
+		return fmt.Errorf("converting uid from string to int: %w", err)
+	}
+	gid, err := strconv.ParseInt(u.Gid, 10, 32)
+	if err != nil {
+		return fmt.Errorf("converting gid from string to int: %w", err)
+	}
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	cmd.SysProcAttr.Credential = &syscall.Credential{
+		Uid: uint32(uid),
+		Gid: uint32(gid),
+	}
+	cmd.Env = append(cmd.Env, "DBUS_SESSION_BUS_ADDRESS='unix:abstract=/tmp/dbus-p9JPVwAZyv,guid=3c7c8e978222bd60d55bdd6a637e3dce'")
+
+	// Set output
+	stderr := new(bytes.Buffer)
+	cmd.Stderr = stderr
+	cmd.Stdout = output
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error running xfconf-query for user %s with args %v and env %v; err is: %s: %w", u.Name, cmd.Args, cmd.Env, stderr.String(), err)
+	}
+
+	return nil
+}
---
+++ pkg/osquery/tables/xfconf/xfconf_test.go
@@ -0,0 +1,162 @@
+//go:build !windows
+// +build !windows
+
+package xfconf
+
+import (
+	"bytes"
+	"os/user"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/stretchr/testify/require"
+)
+
+type parseTestCase struct {
+	channelName    string
+	user           *user.User
+	input          string
+	expectedOutput []map[string]string
+}
+
+func getParseTestCases() []parseTestCase {
+	testUsername := "headless"
+	testUser := user.User{
+		Name: testUsername,
+	}
+
+	return []parseTestCase{
+		{
+			channelName: "xfce4-power-manager",
+			user:        &testUser,
+			input: `/xfce4-power-manager/lock-screen-suspend-hibernate  true
+/xfce4-power-manager/power-button-action            3`,
+			expectedOutput: []map[string]string{
+				{
+					"channel":  "xfce4-power-manager",
+					"key":      "/xfce4-power-manager/lock-screen-suspend-hibernate",
+					"value":    "true",
+					"username": testUsername,
+				},
+				{
+					"channel":  "xfce4-power-manager",
+					"key":      "/xfce4-power-manager/power-button-action",
+					"value":    "3",
+					"username": testUsername,
+				},
+			},
+		},
+		{
+			channelName: "thunar-volman",
+			user:        &testUser,
+			input: `/autobrowse/enabled        false
+/automount-drives/enabled  false
+/automount-media/enabled   false
+/autoopen/enabled          false
+/autorun/enabled           true`,
+			expectedOutput: []map[string]string{
+				{
+					"channel":  "thunar-volman",
+					"key":      "/autobrowse/enabled",
+					"value":    "false",
+					"username": testUsername,
+				},
+				{
+					"channel":  "thunar-volman",
+					"key":      "/automount-drives/enabled",
+					"value":    "false",
+					"username": testUsername,
+				},
+				{
+					"channel":  "thunar-volman",
+					"key":      "/automount-media/enabled",
+					"value":    "false",
+					"username": testUsername,
+				},
+				{
+					"channel":  "thunar-volman",
+					"key":      "/autoopen/enabled",
+					"value":    "false",
+					"username": testUsername,
+				},
+				{
+					"channel":  "thunar-volman",
+					"key":      "/autorun/enabled",
+					"value":    "true",
+					"username": testUsername,
+				},
+			},
+		},
+		{
+			channelName: "xfce4-session",
+			user:        &testUser,
+			input: `/general/FailsafeSessionName          Failsafe
+/general/LockCommand                  
+/sessions/Failsafe/Client0_Command    <<UNSUPPORTED>>
+/sessions/Failsafe/Client0_PerScreen  false
+/sessions/Failsafe/Client0_Priority   15
+/sessions/Failsafe/Count              1
+/sessions/Failsafe/IsFailsafe         true`,
+			expectedOutput: []map[string]string{
+				{
+					"channel":  "xfce4-session",
+					"key":      "/general/FailsafeSessionName",
+					"value":    "Failsafe",
+					"username": testUsername,
+				},
+				{
+					"channel":  "xfce4-session",
+					"key":      "/general/LockCommand",
+					"value":    "",
+					"username": testUsername,
+				},
+				{
+					"channel":  "xfce4-session",
+					"key":      "/sessions/Failsafe/Client0_Command",
+					"value":    "<<UNSUPPORTED>>",
+					"username": testUsername,
+				},
+				{
+					"channel":  "xfce4-session",
+					"key":      "/sessions/Failsafe/Client0_PerScreen",
+					"value":    "false",
+					"username": testUsername,
+				},
+				{
+					"channel":  "xfce4-session",
+					"key":      "/sessions/Failsafe/Client0_Priority",
+					"value":    "15",
+					"username": testUsername,
+				},
+				{
+					"channel":  "xfce4-session",
+					"key":      "/sessions/Failsafe/Count",
+					"value":    "1",
+					"username": testUsername,
+				},
+				{
+					"channel":  "xfce4-session",
+					"key":      "/sessions/Failsafe/IsFailsafe",
+					"value":    "true",
+					"username": testUsername,
+				},
+			},
+		},
+	}
+}
+
+func Test_parseProperties(t *testing.T) {
+	t.Parallel()
+
+	testCases := getParseTestCases()
+
+	xfconf := XfconfQuerier{
+		logger: log.NewNopLogger(),
+	}
+
+	for _, tt := range testCases {
+		input := bytes.NewReader([]byte(tt.input))
+		results := xfconf.parseProperties(input, tt.channelName, tt.user)
+		require.Equal(t, tt.expectedOutput, results)
+	}
+}
