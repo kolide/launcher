9c8760de639d0dab3eef93f750e147df4ba08d2b
seph <seph@kolide.co>
2023-02-10 14:52:05 -0500

    new files

--- ee/ui/assets/assets.go
+++ ee/ui/assets/assets.go
@@ -7,63 +7,63 @@ import (
 )
 
 
-//go:embed menubar-blocked-color-dark.ico
-var KolideMenuBlockedColorDarkIco []byte
-
-//go:embed menubar-blocked-color-dark.png
-var KolideMenuBlockedColorDarkPng []byte
-
-//go:embed menubar-blocked-color-light.ico
-var KolideMenuBlockedColorLightIco []byte
-
-//go:embed menubar-blocked-color-light.png
-var KolideMenuBlockedColorLightPng []byte
-
 //go:embed menubar-blocked-monochrome.ico
-var KolideMenuBlockedMonochromeIco []byte
+var MenubarBlockedMonochromeIco []byte
 
 //go:embed menubar-blocked-monochrome.png
-var KolideMenuBlockedMonochromePng []byte
+var MenubarBlockedMonochromePng []byte
 
-//go:embed menubar-default-dark.ico
-var KolideMenuDefaultDarkIco []byte
+//go:embed menubar-circle-x-darkmode.ico
+var MenubarCircleXDarkmodeIco []byte
 
-//go:embed menubar-default-dark.png
-var KolideMenuDefaultDarkPng []byte
+//go:embed menubar-circle-x-darkmode.png
+var MenubarCircleXDarkmodePng []byte
 
-//go:embed menubar-default-light.ico
-var KolideMenuDefaultLightIco []byte
+//go:embed menubar-circle-x-lightmode.ico
+var MenubarCircleXLightmodeIco []byte
 
-//go:embed menubar-default-light.png
-var KolideMenuDefaultLightPng []byte
+//go:embed menubar-circle-x-lightmode.png
+var MenubarCircleXLightmodePng []byte
 
-//go:embed menubar-missing-registration-dark.ico
-var KolideMenuMissingRegistrationDarkIco []byte
+//go:embed menubar-default-darkmode.ico
+var MenubarDefaultDarkmodeIco []byte
 
-//go:embed menubar-missing-registration-dark.png
-var KolideMenuMissingRegistrationDarkPng []byte
+//go:embed menubar-default-darkmode.png
+var MenubarDefaultDarkmodePng []byte
 
-//go:embed menubar-missing-registration-light.ico
-var KolideMenuMissingRegistrationLightIco []byte
+//go:embed menubar-default-lightmode.ico
+var MenubarDefaultLightmodeIco []byte
 
-//go:embed menubar-missing-registration-light.png
-var KolideMenuMissingRegistrationLightPng []byte
+//go:embed menubar-default-lightmode.png
+var MenubarDefaultLightmodePng []byte
 
-//go:embed menubar-will-block-color-dark.ico
-var KolideMenuWillBlockColorDarkIco []byte
+//go:embed menubar-translucent-darkmode.ico
+var MenubarTranslucentDarkmodeIco []byte
 
-//go:embed menubar-will-block-color-dark.png
-var KolideMenuWillBlockColorDarkPng []byte
+//go:embed menubar-translucent-darkmode.png
+var MenubarTranslucentDarkmodePng []byte
 
-//go:embed menubar-will-block-color-light.ico
-var KolideMenuWillBlockColorLightIco []byte
+//go:embed menubar-translucent-lightmode.ico
+var MenubarTranslucentLightmodeIco []byte
 
-//go:embed menubar-will-block-color-light.png
-var KolideMenuWillBlockColorLightPng []byte
+//go:embed menubar-translucent-lightmode.png
+var MenubarTranslucentLightmodePng []byte
+
+//go:embed menubar-triangle-exclamation-darkmode.ico
+var MenubarTriangleExclamationDarkmodeIco []byte
+
+//go:embed menubar-triangle-exclamation-darkmode.png
+var MenubarTriangleExclamationDarkmodePng []byte
+
+//go:embed menubar-triangle-exclamation-lightmode.ico
+var MenubarTriangleExclamationLightmodeIco []byte
+
+//go:embed menubar-triangle-exclamation-lightmode.png
+var MenubarTriangleExclamationLightmodePng []byte
 
 //go:embed menubar-will-block-monochrome.ico
-var KolideMenuWillBlockMonochromeIco []byte
+var MenubarWillBlockMonochromeIco []byte
 
 //go:embed menubar-will-block-monochrome.png
-var KolideMenuWillBlockMonochromePng []byte
+var MenubarWillBlockMonochromePng []byte
 
