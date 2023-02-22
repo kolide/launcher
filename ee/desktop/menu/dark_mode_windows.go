16c5d96326dec41704ec5046fe263989eaf55529
CJ Barrett <cj@kolide.co>
2023-02-22 11:15:07 -0500

    Windows dark mode support

--- ee/desktop/menu/dark_mode_windows.go
+++ ee/desktop/menu/dark_mode_windows.go
@@ -3,8 +3,23 @@
 
 package menu
 
+import (
+	"golang.org/x/sys/windows/registry"
+)
+
 func isDarkMode() bool {
-	return false
+	k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize`, registry.QUERY_VALUE)
+	if err != nil { // older version of Windows will not have this key
+		return false
+	}
+	defer k.Close()
+
+	useLight, _, err := k.GetIntegerValue("AppsUseLightTheme")
+	if err != nil { // older version of Windows will not have this value
+		return false
+	}
+
+	return useLight == 0
 }
 
 func RegisterThemeChangeListener(f func()) {
