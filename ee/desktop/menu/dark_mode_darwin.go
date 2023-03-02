4dd25581861f19a13b5aa96fa0158f6de37ebf14
CJ Barrett <cj@kolide.co>
2023-03-02 11:32:49 -0500

    Fixed dark mode support on macOS

--- ee/desktop/menu/dark_mode_darwin.go
+++ ee/desktop/menu/dark_mode_darwin.go
@@ -3,46 +3,6 @@
 
 package menu
 
-/*
-#cgo CFLAGS: -x objective-c
-#cgo LDFLAGS: -framework Foundation
-
-#include <AppKit/AppKit.h>
-
-bool isDarkMode();
-void watchTheme();
-*/
-import (
-	"C"
-)
-import (
-	"sync"
-)
-
-var (
-	themeChangeListeners []func()
-	once                 sync.Once
-)
-
 func isDarkMode() bool {
-	if C.isDarkMode() {
-		return true
-	}
-	return false
-}
-
-// RegisterThemeChangeListener registers a listener to be notified when OS theme (dark/light) changes
-func RegisterThemeChangeListener(f func()) {
-	themeChangeListeners = append(themeChangeListeners, f)
-
-	// Ensure we register ourselves as an observer only once
-	watchTheme := func() { C.watchTheme() }
-	once.Do(watchTheme)
-}
-
-//export themeChanged
-func themeChanged() {
-	for _, listener := range themeChangeListeners {
-		listener()
-	}
+	return systrayDarkMode
 }
