da53e925d2f3ced3928594a88088da107e18530c
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-03-10 18:25:00 -0500

    Fix log stream error (#1096)

--- ee/desktop/notify/notify_darwin.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UserNotifications/UserNotifications.h>
-#import <AppKit/AppKit.h>
-
-@interface NotificationDelegate: NSObject <UNUserNotificationCenterDelegate>
-
-@end
