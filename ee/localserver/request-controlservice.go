097d24789b23a16e9a1e4ea3726da56de82250a7
James Pickett <James-Pickett@users.noreply.github.com>
2023-03-21 07:40:40 -0700

    adds logic and endpoint to accelerate control server fetch interval (#1105)
    * adds logic and local server endpoint to accelerate control server fetch interval

--- ee/localserver/request-controlservice.go
+++ ee/localserver/request-controlservice.go
@@ -1,21 +1,58 @@
 package localserver
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
-func (ls *localServer) requestControlServiceFetchHandler() http.Handler {
-	return http.HandlerFunc(ls.requestControlServiceFetchFunc)
+func (ls *localServer) requestAccelerateControlHandler() http.Handler {
+	return http.HandlerFunc(ls.requestAccelerateControlFunc)
 }
 
-func (ls *localServer) requestControlServiceFetchFunc(w http.ResponseWriter, r *http.Request) {
+func (ls *localServer) requestAccelerateControlFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Body == nil {
+		sendClientError(w, "request body is nil")
+		return
+	}
+
 	if ls.controlService == nil {
 		sendClientError(w, fmt.Sprintf("control service not configured"))
 		return
 	}
 
-	if err := ls.controlService.Fetch(); err != nil {
-		sendClientError(w, fmt.Sprintf("error calling control service fetch: %s", err))
+	var body map[string]string
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		sendClientError(w, fmt.Sprintf("error unmarshaling request body: %s", err))
+		return
 	}
+
+	interval, err := durationFromMap("interval", body)
+	if err != nil {
+		sendClientError(w, fmt.Sprintf("error parsing interval: %s", err))
+		return
+	}
+
+	duration, err := durationFromMap("duration", body)
+	if err != nil {
+		sendClientError(w, fmt.Sprintf("error parsing duration: %s", err))
+		return
+	}
+
+	ls.controlService.AccelerateRequestInterval(interval, duration)
+}
+
+func durationFromMap(key string, body map[string]string) (time.Duration, error) {
+	rawDuration, ok := body[key]
+	if !ok || rawDuration == "" {
+		return 0, fmt.Errorf("no key [%s] found in body", key)
+	}
+
+	duration, err := time.ParseDuration(rawDuration)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing duration [%s]: %w", rawDuration, err)
+	}
+
+	return duration, nil
 }
