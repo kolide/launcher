2d8164d4eae0458166a9694f4b05ffa7c4462929
James Pickett <james@kolide.co>
2023-03-23 10:48:55 -0700

    fix test on windows

--- cmd/launcher/desktop_test.go
+++ cmd/launcher/desktop_test.go
@@ -11,8 +11,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_desktopMonitorParentProcess(t *testing.T) {
-	t.Parallel()
+func Test_desktopMonitorParentProcess(t *testing.T) { //nolint:paralleltest
+	// calling httptest.NewServer  by itself fails when parallel on windows with
+	// panic: httptest: failed to listen on a port: listen tcp6 [::1]:0: socket: The requested service provider could not be loaded or initialized.
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Body != nil {
