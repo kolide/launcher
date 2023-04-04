5e746db92e7bf8de8a7b608059f0553d069147b5
CJ Barrett <cj@kolide.co>
2023-04-04 13:48:53 -0400

    Removed unneeded file

--- pkg/agent/knapsack/knapsack_testing.go
+++
@@ -1,8 +0,0 @@
-package knapsack
-
-// func NewTestingKnapsack(t *testing.T, f types.Flags) types.Knapsack {
-// 	db := storageci.SetupDB(t)
-// 	stores, err := storageci.MakeStores(t, log.NewNopLogger(), db)
-// 	require.NoError(t, err)
-// 	return New(stores, db)
-// }
