32499d4f0113d8023d57669a3ef17d643f01c8c0
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-04-12 15:58:42 -0400

    Remove --no-scan from softwareupdate execparser

--- pkg/osquery/table/platform_tables_darwin.go
+++ pkg/osquery/table/platform_tables_darwin.go
@@ -117,6 +117,6 @@ func platformTables(client *osquery.ExtensionManagerClient, logger log.Logger, c
 		munki.ManagedInstalls(client, logger),
 		munki.MunkiReport(client, logger),
 		dataflattentable.NewExecAndParseTable(logger, "kolide_remotectl", remotectl.Parser, []string{`/usr/libexec/remotectl`, `dumpstate`}),
-		dataflattentable.NewExecAndParseTable(logger, "kolide_softwareupdate", softwareupdate.Parser, []string{`/usr/sbin/softwareupdate`, `--list`, `--no-scan`}),
+		dataflattentable.NewExecAndParseTable(logger, "kolide_softwareupdate", softwareupdate.Parser, []string{`/usr/sbin/softwareupdate`, `--list`}),
 	}
 }
