2fd2f32fb1d5d7381b17838c0ad119987a90bdb8
james pickett <james@kolide.co>
2023-04-28 11:33:51 -0700

    restructures desktop directory to differentiate between runner and user components

--- ee/desktop/user/server/server.go
+++ ee/desktop/user/server/server.go
@@ -16,7 +16,7 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/kolide/launcher/ee/desktop/notify"
+	"github.com/kolide/launcher/ee/desktop/user/notify"
 	"github.com/kolide/launcher/pkg/backoff"
 )
 
