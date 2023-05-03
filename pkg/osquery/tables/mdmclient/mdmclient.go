952e6ff7214b2bfd9233ae8e4b4524fd08409127
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-05-03 16:21:05 -0400

    Update mdmclient output parsing (#1176)

--- pkg/osquery/tables/mdmclient/mdmclient.go
+++ pkg/osquery/tables/mdmclient/mdmclient.go
@@ -13,6 +13,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/go-kit/kit/log"
@@ -27,6 +28,30 @@ import (
 const mdmclientPath = "/usr/libexec/mdmclient"
 
 const allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// headerRegex matches the header that may be included at the beginning of the mdmclient response,
+// which takes the following format:
+//
+//	=== CPF_GetInstalledProfiles === (<Device>)
+//	Number of <Device> profiles found: 6 (Filtered: 0)
+var headerRegex = regexp.MustCompile(`^=== CPF_GetInstalledProfiles === \(<Device>\)\nNumber of <Device> profiles found: \d+ \(Filtered: \d+\)\n`)
+
+// lengthBytesRegex matches entries like the PushToken or SignerCertificates entries, which we have to
+// manually adjust to be parseable as a plist. The first capture group gets the length property with
+// a comma after it, which we must replace with a semicolon. The second capture group gets the bytes
+// property, which needs to be quoted and have a semicolon appended.
+//
+// The PushToken line takes the following format:
+//
+// PushToken = {length = 32, bytes = 0x068b4535 172f7bd3 851facee c98e0d88 ... 38625271 61731ac3 };
+//
+// The SignerCertificates block takes the following format:
+//
+//	SignerCertificates =             (
+//		{length = 1402, bytes = 0x30820576 3082045e a0030201 02020809 ... afb8b2d1 abcdabcd },
+//		{length = 1052, bytes = 0x30820418 30820300 a0030201 02020804 ... 26cffc17 abcdabcd }
+//	);
+var lengthBytesRegex = regexp.MustCompile(`{length = (\d+,) bytes = (0[xX][0-9a-fA-F\.\s]+)}`)
 
 type Table struct {
 	client    *osquery.ExtensionManagerClient
@@ -109,7 +134,14 @@ func (t *Table) flattenOutput(dataQuery string, systemOutput []byte) ([]dataflat
 
 // transformOutput has some hackish rules to transform the output into a "proper" gnustep plist
 func (t *Table) transformOutput(in []byte) ([]byte, error) {
-	out := bytes.Replace(in, []byte("Daemon response: {"), []byte("DaemonResponse = {"), 1)
+	out := headerRegex.ReplaceAll(in, []byte{})
+
+	// We can't access the agent response when running launcher normally -- we get the error
+	// "[ERROR] Unable to target 'local user' via XPC when running as daemon". In that case,
+	// remove the null agent response.
+	out = bytes.Replace(out, []byte("Agent response: (null)\n"), []byte{}, 1)
+
+	out = bytes.Replace(out, []byte("Daemon response: {"), []byte("DaemonResponse = {"), 1)
 	out = bytes.Replace(out, []byte("Agent response: {"), []byte("AgentResponse = {"), 1)
 
 	// This would, honestly, be cleaner as a regex. The \n aren't
@@ -118,9 +150,66 @@ func (t *Table) transformOutput(in []byte) ([]byte, error) {
 	// the one that matches the response structures.
 	out = bytes.Replace(out, []byte("\n}\n"), []byte("\n};\n"), 2)
 
+	// Adjust the PushToken and SignerCertificates entries, if present
+	out = transformLengthByteEntriesInOutput(out)
+
 	var retOut []byte
 	retOut = append(retOut, "{\n"...)
 	retOut = append(retOut, out...)
 	retOut = append(retOut, "\n}\n"...)
 	return retOut, nil
+}
+
+// transformLengthByteEntriesInOutput adjusts the formatting of nested blocks with length/byte properties to be
+// parseable in a plist.
+//
+// Example --
+// original: `PushToken = {length = 32, bytes = 0x068b4535 172f7bd3 851facee c98e0d88 ... 38625271 61731ac3 };`
+// transformed: `PushToken = {length = 32; bytes = "0x068b4535 172f7bd3 851facee c98e0d88 ... 38625271 61731ac3 "; };`
+func transformLengthByteEntriesInOutput(out []byte) []byte {
+	matches := lengthBytesRegex.FindAllSubmatchIndex(out, -1)
+
+	if len(matches) == 0 {
+		return out
+	}
+
+	// Iterate backwards through matches to avoid messing up indices for earlier
+	// matches when performing insertions.
+	for i := len(matches) - 1; i >= 0; i -= 1 {
+		match := matches[i]
+		// First two items in `match` are start/end indices for entire line; second two items are
+		// start/end indices for `length` property (the first capture group); third two items are
+		// start/end indices for `bytes` property (the second capture group).
+		if len(match) != 6 {
+			continue
+		}
+
+		// Replace comma with semicolon for first capture group (e.g. transforming `length = 32,` to `length = 32;`)
+		lengthEndIndex := match[3]
+		out[lengthEndIndex-1] = ';'
+
+		// Quote second capture group and append a semicolon (e.g., transforming
+		// `bytes = 0x068b4535 172f7bd3 851facee c98e0d88 ... 38625271 61731ac3 ` to
+		// `bytes = "0x068b4535 172f7bd3 851facee c98e0d88 ... 38625271 61731ac3";`)
+		bytesStartIndex := match[4]
+		bytesEndIndex := match[5]
+
+		// Insert opening quote mark
+		out = insertAt(out, bytesStartIndex, '"')
+
+		// Insert closing quote mark after `bytes`
+		out = insertAt(out, bytesEndIndex+1, '"')
+
+		// Insert semicolon after previous insertion point
+		out = insertAt(out, bytesEndIndex+2, ';')
+	}
+
+	return out
+}
+
+func insertAt(original []byte, insertIndex int, value byte) []byte {
+	original = append(original[:insertIndex+1], original[insertIndex:]...)
+	original[insertIndex] = value
+
+	return original
 }
