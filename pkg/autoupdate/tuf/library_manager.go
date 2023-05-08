e0a2872a54b37cb974b27cc15feeff058b3179d5
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-05-08 10:08:41 -0400

    Combine libraries

--- pkg/autoupdate/tuf/library_manager.go
+++ pkg/autoupdate/tuf/library_manager.go
@@ -8,10 +8,15 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"regexp"
 	"runtime"
+	"sort"
+	"strings"
 	"time"
 
+	"github.com/Masterminds/semver"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/kolide/kit/fsutil"
@@ -22,6 +27,8 @@ import (
 	tufutil "github.com/theupdateframework/go-tuf/util"
 )
 
+var launcherVersionRegex = regexp.MustCompile(`launcher - version (\d+\.\d+\.\d+(?:-.+)?)\n`)
+
 type querier interface {
 	Query(query string) ([]map[string]string, error)
 }
@@ -31,9 +38,9 @@ type querier interface {
 // location in the library specified by the version associated with that update.
 // It also ensures that old updates are removed when they are no longer needed.
 type updateLibraryManager struct {
-	*readOnlyLibrary
 	mirrorUrl    string // dl.kolide.co
 	mirrorClient *http.Client
+	baseDir      string
 	stagingDir   string
 	osquerier    querier // used to query for current running osquery version
 	lock         *libraryLock
@@ -41,6 +48,15 @@ type updateLibraryManager struct {
 }
 
 func newUpdateLibraryManager(mirrorUrl string, mirrorClient *http.Client, baseDir string, osquerier querier, logger log.Logger) (*updateLibraryManager, error) {
+	ulm := updateLibraryManager{
+		mirrorUrl:    mirrorUrl,
+		mirrorClient: mirrorClient,
+		baseDir:      baseDir,
+		osquerier:    osquerier,
+		lock:         newLibraryLock(),
+		logger:       log.With(logger, "component", "tuf_autoupdater_library_manager"),
+	}
+
 	// Ensure the updates directory exists
 	if err := os.MkdirAll(baseDir, 0755); err != nil {
 		return nil, fmt.Errorf("could not make base directory for updates library: %w", err)
@@ -51,31 +67,33 @@ func newUpdateLibraryManager(mirrorUrl string, mirrorClient *http.Client, baseDi
 	if err != nil {
 		return nil, fmt.Errorf("could not make staged updates directory: %w", err)
 	}
+	ulm.stagingDir = stagingDir
 
 	// Create the update library
 	for _, binary := range binaries {
-		if err := os.MkdirAll(filepath.Join(baseDir, string(binary)), 0755); err != nil {
+		if err := os.MkdirAll(ulm.updatesDirectory(binary), 0755); err != nil {
 			return nil, fmt.Errorf("could not make updates directory for %s: %w", binary, err)
 		}
 	}
 
-	// Create the nested read-only library
-	readOnlyLibrary, err := newReadOnlyLibrary(baseDir, logger)
-	if err != nil {
-		return nil, fmt.Errorf("could not create read-only library: %w", err)
-	}
-
-	ulm := updateLibraryManager{
-		readOnlyLibrary: readOnlyLibrary,
-		mirrorUrl:       mirrorUrl,
-		mirrorClient:    mirrorClient,
-		stagingDir:      stagingDir,
-		osquerier:       osquerier,
-		lock:            newLibraryLock(),
-		logger:          log.With(logger, "component", "tuf_autoupdater_library_manager"),
-	}
-
 	return &ulm, nil
+}
+
+// updatesDirectory returns the update library location for the given binary.
+func (ulm *updateLibraryManager) updatesDirectory(binary autoupdatableBinary) string {
+	return filepath.Join(ulm.baseDir, string(binary))
+}
+
+// Available determines if the given target is already available in the update library.
+func (ulm *updateLibraryManager) Available(binary autoupdatableBinary, targetFilename string) bool {
+	executablePath := ulm.PathToTargetVersionExecutable(binary, targetFilename)
+	return autoupdate.CheckExecutable(context.TODO(), executablePath, "--version") == nil
+}
+
+// PathToTargetVersionExecutable returns the path to the executable for the desired version.
+func (ulm *updateLibraryManager) PathToTargetVersionExecutable(binary autoupdatableBinary, targetFilename string) string {
+	versionDir := filepath.Join(ulm.updatesDirectory(binary), versionFromTarget(binary, targetFilename))
+	return executableLocation(versionDir, binary)
 }
 
 // AddToLibrary adds the given target file to the library for the given binary,
@@ -322,4 +340,207 @@ func (ulm *updateLibraryManager) tidyUpdateLibrary(binary autoupdatableBinary, c
 
 		nonCurrentlyRunningVersionsKept += 1
 	}
+}
+
+// sortedVersionsInLibrary looks through the update library for the given binary to validate and sort all
+// available versions. It returns a sorted list of the valid versions, a list of invalid versions, and
+// an error only when unable to glob for versions.
+func (ulm *updateLibraryManager) sortedVersionsInLibrary(binary autoupdatableBinary) ([]string, []string, error) {
+	rawVersionsInLibrary, err := filepath.Glob(filepath.Join(ulm.updatesDirectory(binary), "*"))
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not glob for updates in library: %w", err)
+	}
+
+	versionsInLibrary := make([]*semver.Version, 0)
+	invalidVersions := make([]string, 0)
+	for _, rawVersionWithPath := range rawVersionsInLibrary {
+		rawVersion := filepath.Base(rawVersionWithPath)
+		v, err := semver.NewVersion(rawVersion)
+		if err != nil {
+			invalidVersions = append(invalidVersions, rawVersion)
+			continue
+		}
+
+		versionDir := filepath.Join(ulm.updatesDirectory(binary), rawVersion)
+		if err := autoupdate.CheckExecutable(context.TODO(), executableLocation(versionDir, binary), "--version"); err != nil {
+			invalidVersions = append(invalidVersions, rawVersion)
+			continue
+		}
+
+		versionsInLibrary = append(versionsInLibrary, v)
+	}
+
+	// Sort the versions (ascending order)
+	sort.Sort(semver.Collection(versionsInLibrary))
+
+	// Transform versions back into strings now that we've finished sorting them
+	versionsInLibraryStr := make([]string, len(versionsInLibrary))
+	for i, v := range versionsInLibrary {
+		versionsInLibraryStr[i] = v.Original()
+	}
+
+	return versionsInLibraryStr, invalidVersions, nil
+}
+
+// IsInstallVersion checks whether the version in the given target is the same as the one
+// contained in the installation.
+func (ulm *updateLibraryManager) IsInstallVersion(binary autoupdatableBinary, targetFilename string) bool {
+	installedVersion, _, err := ulm.installedVersion(binary)
+	if err != nil {
+		level.Debug(ulm.logger).Log("msg", "could not get installed version", "binary", binary, "err", err)
+		return false
+	}
+
+	return installedVersion.Original() == versionFromTarget(binary, targetFilename)
+}
+
+// installedVersion returns the version of, and the path to, the originally-installed binary.
+func (ulm *updateLibraryManager) installedVersion(binary autoupdatableBinary) (*semver.Version, string, error) {
+	pathToBinary := findInstallLocation(binary)
+	if pathToBinary == "" {
+		return nil, "", fmt.Errorf("could not find install location for `%s`", binary)
+	}
+
+	if cachedVersion, err := ulm.getCachedInstalledVersion(binary); err == nil {
+		return cachedVersion, pathToBinary, nil
+	}
+
+	cmd := exec.Command(pathToBinary, "--version")
+	cmd.Env = append(cmd.Env, "LAUNCHER_SKIP_UPDATES=TRUE") // Prevents launcher from fork-bombing
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, "", fmt.Errorf("could not execute %s --version: out %s, error %w", pathToBinary, string(out), err)
+	}
+
+	var v *semver.Version
+	switch binary {
+	case binaryLauncher:
+		v, err = parseLauncherVersion(out)
+	case binaryOsqueryd:
+		v, err = parseOsquerydVersion(out)
+	default:
+		return nil, "", fmt.Errorf("cannot parse version for unknown binary %s", binary)
+	}
+
+	if err != nil {
+		return nil, "", fmt.Errorf("could not parse binary install version: %w", err)
+	}
+
+	ulm.cacheInstalledVersion(binary, v)
+
+	return v, pathToBinary, nil
+}
+
+// getCachedInstalledVersion reads the install version from a cached file in the updates directory.
+func (ulm *updateLibraryManager) getCachedInstalledVersion(binary autoupdatableBinary) (*semver.Version, error) {
+	versionBytes, err := os.ReadFile(ulm.cachedInstalledVersionLocation(binary))
+	if err != nil {
+		return nil, fmt.Errorf("could not read cached installed version file: %w", err)
+	}
+
+	v, err := semver.NewVersion(string(versionBytes))
+	if err != nil {
+		return nil, fmt.Errorf("could not parse cached installed version file: %w", err)
+	}
+
+	return v, nil
+}
+
+// cacheInstalledVersion caches the install version in a file in the updates directory, to avoid
+// having to exec the binary to discover its version every time.
+func (ulm *updateLibraryManager) cacheInstalledVersion(binary autoupdatableBinary, installedVersion *semver.Version) {
+	if err := os.WriteFile(ulm.cachedInstalledVersionLocation(binary), []byte(installedVersion.Original()), 0644); err != nil {
+		level.Debug(ulm.logger).Log("msg", "could not cache installed version", "binary", binary, "err", err)
+	}
+}
+
+// cachedInstalledVersionLocation returns the location of the cached install version file.
+func (ulm *updateLibraryManager) cachedInstalledVersionLocation(binary autoupdatableBinary) string {
+	return filepath.Join(ulm.baseDir, fmt.Sprintf("%s-installed-version", binary))
+}
+
+// versionFromTarget extracts the semantic version for an update from its filename.
+func versionFromTarget(binary autoupdatableBinary, targetFilename string) string {
+	// The target is in the form `launcher-0.13.6.tar.gz` -- trim the prefix and the file extension to return the version
+	prefixToTrim := fmt.Sprintf("%s-", binary)
+
+	return strings.TrimSuffix(strings.TrimPrefix(targetFilename, prefixToTrim), ".tar.gz")
+}
+
+// parseLauncherVersion parses the launcher version from the output of `launcher --version`.
+func parseLauncherVersion(versionOutput []byte) (*semver.Version, error) {
+	matches := launcherVersionRegex.FindStringSubmatch(string(versionOutput))
+	if len(matches) < 2 {
+		return nil, fmt.Errorf("could not parse launcher version from output %s", string(versionOutput))
+	}
+	launcherInstallVersion, err := semver.NewVersion(matches[1])
+	if err != nil {
+		return nil, fmt.Errorf("could not parse launcher version %s as semver: %w", matches[1], err)
+	}
+
+	return launcherInstallVersion, nil
+}
+
+// parseOsquerydVersion parses the osqueryd version from the output of `osqueryd --version`.
+func parseOsquerydVersion(versionOutput []byte) (*semver.Version, error) {
+	versionStr := strings.TrimSpace(strings.TrimPrefix(string(versionOutput), "osqueryd version"))
+	osqueryInstallVersion, err := semver.NewVersion(versionStr)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse osquery version `%s` as semver: %w", versionStr, err)
+	}
+
+	return osqueryInstallVersion, nil
+}
+
+// findInstallLocation attempts to find the install location for the given binary, looking
+// in well-known locations.
+func findInstallLocation(binary autoupdatableBinary) string {
+	binaryName := string(binary)
+	if runtime.GOOS == "windows" {
+		binaryName = binaryName + ".exe"
+	}
+
+	// Places that we expect to see binaries installed
+	var likelyPaths []string
+
+	if binary == binaryOsqueryd {
+		likelyPaths = append(likelyPaths, executableLocation(`C:\Program Files\osquery`, binary))
+	}
+
+	likelyPaths = append(
+		likelyPaths,
+		executableLocation("/usr/local/kolide", binary),
+		executableLocation("/usr/local/kolide/bin", binary),
+		executableLocation("/usr/local/kolide-k2", binary),
+		executableLocation("/usr/local/kolide-k2/bin", binary),
+		executableLocation("/usr/local/bin", binary),
+		executableLocation(`C:\Program Files\Kolide\Launcher-kolide-k2\bin`, binary),
+	)
+
+	// We want to check the current executable path last, since that may pick up updates instead
+	if currentExecutablePath, err := os.Executable(); err == nil {
+		likelyPaths = append(likelyPaths, filepath.Join(filepath.Dir(currentExecutablePath), string(binary)))
+	}
+
+	for _, potentialPath := range likelyPaths {
+		potentialPath = filepath.Clean(potentialPath)
+
+		info, err := os.Stat(potentialPath)
+		if err != nil {
+			continue
+		}
+
+		if info.IsDir() {
+			continue
+		}
+
+		return potentialPath
+	}
+
+	// If we haven't found it anywhere else, look for it on the PATH
+	if potentialPath, err := exec.LookPath(binaryName); err == nil {
+		return potentialPath
+	}
+
+	return ""
 }
--- pkg/autoupdate/tuf/library_manager_test.go
+++ pkg/autoupdate/tuf/library_manager_test.go
@@ -47,6 +47,50 @@ func Test_newUpdateLibraryManager(t *testing.T) {
 	require.NotNil(t, testLibraryManager.osquerier, "osquerier not set on library manager")
 }
 
+func TestPathToTargetVersionExecutable(t *testing.T) {
+	t.Parallel()
+
+	testBaseDir := filepath.Join(t.TempDir(), "updates")
+	testLibrary, err := newUpdateLibraryManager("", nil, testBaseDir, newMockQuerier(t), log.NewNopLogger())
+	require.NoError(t, err, "expected no error when creating library")
+
+	testVersion := "1.0.7-30-abcdabcd"
+	testTargetFilename := fmt.Sprintf("launcher-%s.tar.gz", testVersion)
+	expectedPath := filepath.Join(testBaseDir, "launcher", testVersion, "launcher")
+	if runtime.GOOS == "darwin" {
+		expectedPath = filepath.Join(testBaseDir, "launcher", testVersion, "Kolide.app", "Contents", "MacOS", "launcher")
+	} else if runtime.GOOS == "windows" {
+		expectedPath = expectedPath + ".exe"
+	}
+
+	actualPath := testLibrary.PathToTargetVersionExecutable(binaryLauncher, testTargetFilename)
+	require.Equal(t, expectedPath, actualPath, "path mismatch")
+}
+
+func TestAvailable(t *testing.T) {
+	t.Parallel()
+
+	// Create update directories
+	testBaseDir := t.TempDir()
+
+	// Set up test library
+	testLibrary, err := newUpdateLibraryManager("", nil, testBaseDir, newMockQuerier(t), log.NewNopLogger())
+	require.NoError(t, err, "unexpected error creating new read-only library")
+
+	// Set up valid "osquery" executable
+	runningOsqueryVersion := "5.5.7"
+	runningTarget := fmt.Sprintf("osqueryd-%s.tar.gz", runningOsqueryVersion)
+	executablePath := testLibrary.PathToTargetVersionExecutable(binaryOsqueryd, runningTarget)
+	copyBinary(t, executablePath)
+	require.NoError(t, os.Chmod(executablePath, 0755))
+
+	// Query for the current osquery version
+	require.True(t, testLibrary.Available(binaryOsqueryd, runningTarget))
+
+	// Query for a different osqueryd version
+	require.False(t, testLibrary.Available(binaryOsqueryd, "osqueryd-5.6.7.tar.gz"))
+}
+
 func TestAddToLibrary(t *testing.T) {
 	t.Parallel()
 
@@ -185,13 +229,10 @@ func TestAddToLibrary_alreadyAdded(t *testing.T) {
 			}))
 			defer testMirror.Close()
 			testLibraryManager := &updateLibraryManager{
-				readOnlyLibrary: &readOnlyLibrary{
-					baseDir: testBaseDir,
-					logger:  log.NewNopLogger(),
-				},
 				mirrorUrl:    testMirror.URL,
 				mirrorClient: http.DefaultClient,
 				logger:       log.NewNopLogger(),
+				baseDir:      testBaseDir,
 				stagingDir:   t.TempDir(),
 				osquerier:    mockOsquerier,
 				lock:         newLibraryLock(),
@@ -545,11 +586,8 @@ func Test_tidyUpdateLibrary(t *testing.T) {
 				// Set up test library manager
 				testBaseDir := t.TempDir()
 				testLibraryManager := &updateLibraryManager{
-					readOnlyLibrary: &readOnlyLibrary{
-						baseDir: testBaseDir,
-						logger:  log.NewNopLogger(),
-					},
 					logger:     log.NewNopLogger(),
+					baseDir:    testBaseDir,
 					stagingDir: t.TempDir(),
 					lock:       newLibraryLock(),
 				}
@@ -589,6 +627,236 @@ func Test_tidyUpdateLibrary(t *testing.T) {
 			})
 		}
 	}
+}
+
+func Test_sortedVersionsInLibrary(t *testing.T) {
+	t.Parallel()
+
+	// Create update directories
+	testBaseDir := t.TempDir()
+	require.NoError(t, os.MkdirAll(filepath.Join(testBaseDir, "launcher"), 0755))
+	require.NoError(t, os.MkdirAll(filepath.Join(testBaseDir, "osqueryd"), 0755))
+
+	// Create an update in the library that is invalid because it doesn't have a valid version
+	invalidVersion := "not_a_semver_1"
+	require.NoError(t, os.MkdirAll(filepath.Join(testBaseDir, "launcher", invalidVersion), 0755))
+
+	// Create an update in the library that is invalid because it's corrupted
+	corruptedVersion := "1.0.6-11-abcdabcd"
+	corruptedVersionDirectory := filepath.Join(testBaseDir, "launcher", corruptedVersion)
+	corruptedVersionLocation := executableLocation(corruptedVersionDirectory, binaryLauncher)
+	require.NoError(t, os.MkdirAll(filepath.Dir(corruptedVersionLocation), 0755))
+	require.NoError(t, os.WriteFile(
+		filepath.Join(corruptedVersionLocation),
+		[]byte("not an executable"),
+		0755))
+
+	// Create two valid updates in the library
+	olderValidVersion := "0.13.5"
+	middleValidVersion := "1.0.5-11-abcdabcd"
+	newerValidVersion := "1.0.7"
+	for _, v := range []string{olderValidVersion, middleValidVersion, newerValidVersion} {
+		versionDir := filepath.Join(testBaseDir, "launcher", v)
+		executablePath := executableLocation(versionDir, binaryLauncher)
+		require.NoError(t, os.MkdirAll(filepath.Dir(executablePath), 0755))
+		copyBinary(t, executablePath)
+		require.NoError(t, os.Chmod(executablePath, 0755))
+		_, err := os.Stat(executablePath)
+		require.NoError(t, err, "did not create binary for test")
+		require.NoError(t, autoupdate.CheckExecutable(context.TODO(), executablePath, "--version"), "binary created for test is corrupt")
+	}
+
+	// Set up test library
+	testLibrary, err := newUpdateLibraryManager("", nil, testBaseDir, newMockQuerier(t), log.NewNopLogger())
+	require.NoError(t, err, "unexpected error creating new read-only library")
+
+	// Get sorted versions
+	validVersions, invalidVersions, err := testLibrary.sortedVersionsInLibrary(binaryLauncher)
+	require.NoError(t, err, "expected no error on sorting versions in library")
+
+	// Confirm invalid versions are the ones we expect
+	require.Equal(t, 2, len(invalidVersions))
+	require.Contains(t, invalidVersions, invalidVersion)
+	require.Contains(t, invalidVersions, corruptedVersion)
+
+	// Confirm valid versions are the ones we expect and that they're sorted in ascending order
+	require.Equal(t, 3, len(validVersions))
+	require.Equal(t, olderValidVersion, validVersions[0], "not sorted")
+	require.Equal(t, middleValidVersion, validVersions[1], "not sorted")
+	require.Equal(t, newerValidVersion, validVersions[2], "not sorted")
+}
+
+func Test_installedVersion_cached(t *testing.T) {
+	t.Parallel()
+
+	for _, binary := range binaries {
+		binary := binary
+		t.Run(string(binary), func(t *testing.T) {
+			t.Parallel()
+
+			// Create update directories
+			testBaseDir := t.TempDir()
+
+			// Set up test library
+			testLibrary, err := newUpdateLibraryManager("", nil, testBaseDir, newMockQuerier(t), log.NewNopLogger())
+			require.NoError(t, err, "unexpected error creating new read-only library")
+
+			// Create cached version file
+			expectedVersion := "5.5.5"
+			require.NoError(t, os.WriteFile(
+				filepath.Join(testLibrary.baseDir, fmt.Sprintf("%s-installed-version", binary)),
+				[]byte(expectedVersion),
+				0755))
+
+			// Create fake executable in current working directory
+			executablePath, err := os.Executable()
+			require.NoError(t, err)
+			testExecutablePath := filepath.Join(filepath.Dir(executablePath), string(binary))
+			require.NoError(t, os.WriteFile(testExecutablePath, []byte("test"), 0755))
+			t.Cleanup(func() {
+				os.Remove(testExecutablePath)
+			})
+
+			actualVersion, actualPath, err := testLibrary.installedVersion(binary)
+			require.NoError(t, err, "could not get installed version")
+			require.NotNil(t, actualVersion)
+			require.NotEqual(t, "", actualPath)
+		})
+	}
+}
+
+func Test_cacheInstalledVersion(t *testing.T) {
+	t.Parallel()
+
+	// Create update directories
+	testBaseDir := t.TempDir()
+
+	// Set up test library
+	testLibrary, err := newUpdateLibraryManager("", nil, testBaseDir, newMockQuerier(t), log.NewNopLogger())
+	require.NoError(t, err, "unexpected error creating new read-only library")
+
+	versionToCache, err := semver.NewVersion("1.2.3-45-abcdabcd")
+	require.NoError(t, err, "unexpected error parsing semver")
+
+	for _, binary := range binaries {
+		// Confirm cache file doesn't exist yet
+		expectedCacheFileLocation := testLibrary.cachedInstalledVersionLocation(binary)
+		_, err = os.Stat(expectedCacheFileLocation)
+		require.True(t, os.IsNotExist(err), "cache file exists but should not have been created yet")
+
+		// Cache it
+		testLibrary.cacheInstalledVersion(binary, versionToCache)
+
+		// Confirm cache file exists
+		_, err = os.Stat(expectedCacheFileLocation)
+		require.NoError(t, err, "cache file %s does not exist but should have been created", expectedCacheFileLocation)
+
+		// Compare versions
+		cachedVersion, err := testLibrary.getCachedInstalledVersion(binary)
+		require.NoError(t, err, "expected no error reading cached installed version")
+		require.True(t, versionToCache.Equal(cachedVersion), "versions do not match")
+	}
+}
+
+func Test_versionFromTarget(t *testing.T) {
+	t.Parallel()
+
+	testVersions := []struct {
+		target          string
+		binary          autoupdatableBinary
+		operatingSystem string
+		version         string
+	}{
+		{
+			target:          "launcher/darwin/launcher-0.10.1.tar.gz",
+			binary:          binaryLauncher,
+			operatingSystem: "darwin",
+			version:         "0.10.1",
+		},
+		{
+			target:          "launcher/windows/launcher-1.13.5.tar.gz",
+			binary:          binaryLauncher,
+			operatingSystem: "windows",
+			version:         "1.13.5",
+		},
+		{
+			target:          "launcher/linux/launcher-0.13.5-40-gefdc582.tar.gz",
+			binary:          binaryLauncher,
+			operatingSystem: "linux",
+			version:         "0.13.5-40-gefdc582",
+		},
+		{
+			target:          "osqueryd/darwin/osqueryd-5.8.1.tar.gz",
+			binary:          binaryOsqueryd,
+			operatingSystem: "darwin",
+			version:         "5.8.1",
+		},
+		{
+			target:          "osqueryd/windows/osqueryd-0.8.1.tar.gz",
+			binary:          binaryOsqueryd,
+			operatingSystem: "windows",
+			version:         "0.8.1",
+		},
+		{
+			target:          "osqueryd/linux/osqueryd-5.8.2.tar.gz",
+			binary:          binaryOsqueryd,
+			operatingSystem: "linux",
+			version:         "5.8.2",
+		},
+	}
+
+	for _, testVersion := range testVersions {
+		require.Equal(t, testVersion.version, versionFromTarget(testVersion.binary, filepath.Base(testVersion.target)))
+	}
+}
+
+func Test_parseLauncherVersion(t *testing.T) {
+	t.Parallel()
+
+	launcherVersionOutputDev := `launcher - version 1.0.7-45-g2abfc88-dirty
+	branch: 	becca/tuf-find-new-v2
+	revision: 	2abfc8883b96460603b49bc6f5cc44d5756890cf
+	build date: 	2023-05-04
+	build user: 	System Administrator (root)
+	go version: 	go1.19.5`
+	devVersion, err := parseLauncherVersion([]byte(launcherVersionOutputDev))
+	require.NoError(t, err, "should be able to parse launcher dev version without error")
+	require.NotNil(t, devVersion, "should have been able to parse launcher dev version as semver")
+	require.Equal(t, "1.0.7-45-g2abfc88-dirty", devVersion.Original(), "dev semver should match")
+
+	launcherVersionOutputNightly := `{"caller":"main.go:30","msg":"Launcher starting up","revision":"3e305bdb54c301759b62e9038faaa2cfea8abad1","severity":"info","ts":"2023-05-04T17:00:34.564523Z","version":"0.13.5-11-g3e305bd"}
+	launcher - version 0.13.5-11-g3e305bd
+	  branch: 	main
+	  revision: 	3e305bdb54c301759b62e9038faaa2cfea8abad1
+	  build date: 	2023-02-14
+	  build user: 	runner (runner)
+	  go version: 	go1.19.4`
+	nightlyVersion, err := parseLauncherVersion([]byte(launcherVersionOutputNightly))
+	require.NoError(t, err, "should be able to parse launcher nightly version without error")
+	require.NotNil(t, nightlyVersion, "should have been able to parse launcher nightly version as semver")
+	require.Equal(t, "0.13.5-11-g3e305bd", nightlyVersion.Original(), "nightly semver should match")
+
+	launcherVersionOutputStable := `launcher - version 1.0.3
+	  branch: 	main
+	  revision: 	3e305bdb54c301759b62e9038faaa2cfea8abad1
+	  build date: 	2023-02-14
+	  build user: 	runner (runner)
+	  go version: 	go1.19.4`
+	stableVersion, err := parseLauncherVersion([]byte(launcherVersionOutputStable))
+	require.NoError(t, err, "should be able to parse launcher stable version without error")
+	require.NotNil(t, stableVersion, "should have been able to parse launcher stable version as semver")
+	require.Equal(t, "1.0.3", stableVersion.Original(), "stable semver should match")
+}
+
+func Test_parseOsquerydVersion(t *testing.T) {
+	t.Parallel()
+
+	osquerydVersionOutput := `osqueryd version 5.8.1`
+
+	v, err := parseOsquerydVersion([]byte(osquerydVersionOutput))
+	require.NoError(t, err, "should be able to parse osqueryd version without error")
+	require.NotNil(t, v, "should have been able to parse osqueryd version as semver")
+	require.Equal(t, "5.8.1", v.Original(), "osqueryd semver should match")
 }
 
 func copyBinary(t *testing.T, executablePath string) {
