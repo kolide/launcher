328fa997fa0135c3ea830846de912a682e5e416f
seph <seph@kolide.co>
2023-05-14 21:31:44 -0400

    update tee logger

--- pkg/log/teelogger/teelogger.go
+++ pkg/log/teelogger/teelogger.go
@@ -2,14 +2,17 @@
 package teelogger
 
 import (
+	"sync"
+
 	"github.com/go-kit/kit/log"
 )
 
 type teeLogger struct {
 	loggers []log.Logger
+	lock    sync.Mutex
 }
 
-func New(loggers ...log.Logger) log.Logger {
+func New(loggers ...log.Logger) *teeLogger {
 
 	l := &teeLogger{
 		loggers: make([]log.Logger, 0),
@@ -21,6 +24,19 @@ func New(loggers ...log.Logger) log.Logger {
 		l.loggers = append(l.loggers, logger)
 	}
 	return l
+}
+
+// Add merges additional loggers into the tee
+func (l *teeLogger) Add(loggers ...log.Logger) {
+	l.lock.Lock()
+	defer l.lock.Unlock()
+
+	for _, logger := range loggers {
+		if logger == nil {
+			continue
+		}
+		l.loggers = append(l.loggers, logger)
+	}
 }
 
 // Log will log to each logger. If any of them error, it will return a
