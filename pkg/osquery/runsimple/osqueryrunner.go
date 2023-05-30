7804df1ce4e0d866f28ec930c12b898a991f924e
seph <seph@kolide.co>
2023-05-30 17:45:31 -0400

    simple executor

---
+++ pkg/osquery/runsimple/osqueryrunner.go
@@ -0,0 +1,137 @@
+package runsimple
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"os/exec"
+)
+
+// osqueryRunner is a very simple osquery runtime manager. It's designed to start and stop osquery. It has
+// no interactions with the osquery socket, it is purely a process manager.
+type osqueryProcess struct {
+	osquerydPath  string
+	rootDirectory string
+	stdout        io.Writer
+	stderr        io.Writer
+	stdin         io.Reader
+	sql           []byte
+
+	cmd *exec.Cmd
+}
+
+type osqueryProcessOpt func(*osqueryProcess)
+
+// WithRootDirectory is a functional option which allows the user to define the
+// path where filesystem artifacts will be stored. This may include pidfiles,
+// RocksDB database files, etc. If this is not defined, a temporary directory
+// will be used.
+func WithRootDirectory(path string) osqueryProcessOpt {
+	return func(p *osqueryProcess) {
+		p.rootDirectory = path
+	}
+}
+
+// WithStdout is a functional option which allows the user to define where the
+// stdout of the osquery process should be directed. By default, the output will
+// be discarded. This should only be configured once.
+func WithStdout(w io.Writer) osqueryProcessOpt {
+	return func(p *osqueryProcess) {
+		p.stdout = w
+	}
+}
+
+// WithStderr is a functional option which allows the user to define where the
+// stderr of the osquery process should be directed. By default, the output will
+// be discarded. This should only be configured once.
+func WithStderr(w io.Writer) osqueryProcessOpt {
+	return func(p *osqueryProcess) {
+		p.stderr = w
+	}
+}
+
+func WithStdin(r io.Reader) osqueryProcessOpt {
+	return func(p *osqueryProcess) {
+		p.stdin = r
+	}
+}
+
+func RunSql(sql []byte) osqueryProcessOpt {
+	return func(p *osqueryProcess) {
+		// TODO: When we're passing in via stdin, we need to ensure we have a trailing semicolon. Such is the
+		// life of shell nonsense.
+		p.sql = append(sql, []byte("\n;")...)
+	}
+}
+
+func newOsqueryProcess(osquerydPath string, opts ...osqueryProcessOpt) (*osqueryProcess, error) {
+	p := &osqueryProcess{
+		osquerydPath: osquerydPath,
+	}
+
+	for _, opt := range opts {
+		opt(p)
+	}
+
+	if p.stdin != nil && p.sql != nil {
+		return nil, errors.New("Cannot specify both stdin and sql")
+	}
+
+	return p, nil
+}
+
+func (p *osqueryProcess) Execute(ctx context.Context) error {
+	// TODO: Not totally sure on ctx here. I think that osquery should probably have it's own context, but also
+	// that it's a better method signature.
+
+	// cmd.Start does not block
+	// Need to call cmd.Wait after it
+	// So how do we manage the start, health, wait in the rest of the control flow?
+
+	args := []string{}
+
+	if p.sql != nil {
+		args = append(args, []string{
+			"-S",
+			"--config_path", "/dev/null",
+			"--disable_events",
+			"--disable_database",
+			"--disable_audit",
+			"--ephemeral",
+			"--json",
+		}...)
+
+		p.stdin = bytes.NewReader(p.sql)
+	} else {
+		panic("Not supported wothout specified sql")
+	}
+
+	p.cmd = exec.CommandContext(ctx, p.osquerydPath, args...)
+
+	// It's okay for these to be nil, so we can just set them without checking.
+	p.cmd.Stdin = p.stdin
+	p.cmd.Stdout = p.stdout
+	p.cmd.Stderr = p.stderr
+
+	//fmt.Printf("About to run %s\n", p.cmd.String())
+	return p.cmd.Run()
+}
+
+/*
+
+func (p *osqueryProcess) Stop() error {
+	proc := p.cmd.Process
+	if proc == nil {
+		return errors.New("No process. Missing start?")
+	}
+
+	err := cmd.Process.Signal(interrupt)
+	if err == nil {
+		err = ctx.Err() // Report ctx.Err() as the reason we interrupted.
+	} else if err.Error() == "os: process already finished" {
+		errc <- nil
+	}
+
+}
+*/
---
+++ pkg/osquery/runsimple/osqueryrunner_test.go
@@ -0,0 +1,108 @@
+package runsimple
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_OsquerySingleSqlNoIO(t *testing.T) {
+	osquerydPath := os.Getenv("OSQUERYD_PATH")
+
+	if osquerydPath == "" {
+		t.Skip("No osquery. Not implemented")
+	}
+
+	osq, err := newOsqueryProcess(osquerydPath, RunSql([]byte("select 1")))
+	require.NoError(t, err)
+
+	require.NoError(t, osq.Execute(context.TODO()))
+}
+
+func Test_OsquerySingleSqlErr(t *testing.T) {
+	osquerydPath := os.Getenv("OSQUERYD_PATH")
+
+	if osquerydPath == "" {
+		t.Skip("No osquery. Not implemented")
+	}
+
+	tests := []struct {
+		name      string
+		sql       string
+		expectErr bool
+		jsonMulti bool
+	}{
+		{
+			name:      "Bad SQL",
+			sql:       "this is not sql;",
+			expectErr: true,
+		},
+		{
+			name:      "Bad SQL, no semicolon",
+			sql:       "this is not sql, no semicolon",
+			expectErr: true,
+		},
+		{
+			name: "select 1",
+			sql:  "select 1",
+		},
+		{
+			name: "select 1;",
+			sql:  "select 1",
+		},
+		{
+			name: "multiselect",
+			sql:  "select 1; select 2",
+		},
+		{
+			name:      "comments",
+			sql:       "select 1; select 2; \n--this is a comment\nselect 3",
+			jsonMulti: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			// No parallel, to many execs
+
+			var stdout bytes.Buffer
+			var stderr bytes.Buffer
+
+			osq, err := newOsqueryProcess(
+				osquerydPath,
+				RunSql([]byte(tt.sql)),
+				WithStdout(&stdout),
+				WithStderr(&stderr),
+			)
+			require.NoError(t, err)
+
+			if tt.expectErr {
+				require.Error(t, osq.Execute(context.TODO()))
+				require.Contains(t, stderr.String(), "Error")
+				return
+			}
+
+			require.NoError(t, osq.Execute(context.TODO()))
+
+			if tt.jsonMulti {
+				// This is discouraged, and hard to test. So let's bail for now
+				return
+			}
+
+			var result []map[string]string
+			require.NoError(t, json.Unmarshal(stdout.Bytes(), &result))
+
+			fmt.Println(stderr.String())
+			spew.Dump(result)
+
+		})
+	}
+
+}
