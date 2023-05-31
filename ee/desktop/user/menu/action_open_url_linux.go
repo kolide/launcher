2fc2032dd06bb107047124f72e2bdfc90d58be8e
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-05-31 09:37:00 -0400

    Default to x-www-browser over xdg-open when available for handling menu actions

--- ee/desktop/user/menu/action_open_url_linux.go
+++ ee/desktop/user/menu/action_open_url_linux.go
@@ -4,11 +4,24 @@
 package menu
 
 import (
+	"fmt"
 	"os/exec"
 )
 
-// open opens the specified URL in the default browser of the user
-// See https://stackoverflow.com/a/39324149/1705598
+// We default to x-www-browser first because, if available, it appears to be better at picking
+// the correct default browser.
+var browserLaunchers = []string{"x-www-browser", "xdg-open"}
+
 func open(url string) error {
-	return exec.Command("xdg-open", url).Start()
+	errList := make([]error, 0)
+	for _, browserLauncher := range browserLaunchers {
+		cmd := exec.Command(browserLauncher, url)
+		if err := cmd.Start(); err != nil {
+			errList = append(errList, fmt.Errorf("could not open browser with %s: %w", browserLauncher, err))
+		} else {
+			// Successfully opened URL, nothing else to do here
+			return nil
+		}
+	}
+	return fmt.Errorf("could not successfully open browser with any given launchers: %+v", errList)
 }
