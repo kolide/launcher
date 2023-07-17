d4bc7bc12d882cd362cbebe6df1eb51ce4052b0d
seph <seph@kolide.co>
2023-07-17 14:27:40 -0400

    remove menu flare

--- ee/desktop/user/menu/action_flare.go
+++ ee/desktop/user/menu/action_flare.go
@@ -1,66 +1,22 @@
 package menu
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-
 	"github.com/go-kit/kit/log/level"
-
-	"github.com/mitchellh/go-homedir"
 )
 
-// Performs the launcher flare action
+// actionFlare performs the launcher flare action. This will make more sense once flare is uploading
 type actionFlare struct {
 	URL string `json:"url"`
 }
 
 func (a actionFlare) Perform(m *menu) {
-	cmd, err := m.flareCommand(m.hostname)
-	if err != nil {
-		level.Error(m.logger).Log("msg", "failed to exec launcher flare", "err", err)
+	if err := runFlare(); err != nil {
+		level.Error(m.logger).Log("msg", "error creating flare", "err", err)
 	}
-	cmd.Start()
 }
 
-// flareCommand invokes the launcher flare executable with the appropriate env vars
-func (m *menu) flareCommand(hostname string) (*exec.Cmd, error) {
-	executablePath, err := os.Executable()
-	if err != nil {
-		return nil, fmt.Errorf("error getting executable path: %w", err)
-	}
-
-	desktopDir, err := homedir.Expand("~/Desktop")
-	cmd := exec.Command(executablePath, "flare")
-	cmd.Env = []string{
-		fmt.Sprintf("HOSTNAME=%s", hostname),
-		fmt.Sprintf("KOLIDE_LAUNCHER_TAR_DIR_PATH=%s", desktopDir),
-	}
-
-	stdErr, err := cmd.StderrPipe()
-	if err != nil {
-		return nil, fmt.Errorf("getting stderr pipe: %w", err)
-	}
-
-	stdOut, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, fmt.Errorf("getting stdout pipe: %w", err)
-	}
-
-	go func() {
-		combined := io.MultiReader(stdErr, stdOut)
-		scanner := bufio.NewScanner(combined)
-
-		for scanner.Scan() {
-			level.Info(m.logger).Log(
-				"uid", os.Getuid(),
-				"subprocess", "flare",
-				"msg", scanner.Text(),
-			)
-		}
-	}()
-
-	return cmd, nil
+func runFlare() error {
+	// TODO: can't create a flare without knapsack, and ideally, being root. This probably is going to be easiest if we use
+	// IPC to send the request for flare back to the parent instead of running it inline here. But it's a bit TBD
+	return nil
 }
