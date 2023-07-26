59c2cb1a30f3961855a28377837ccd3c80d7d468
james pickett <james@kolide.co>
2023-07-26 13:03:09 -0500

    made test file names same as runner

--- ee/desktop/runner/runner_test.go
+++ ee/desktop/runner/runner_test.go
@@ -320,7 +320,7 @@ func TestDesktopUsersProcessesRunner_setupSocketPath(t *testing.T) {
 
 	// create some fake socket files to get cleaned up
 	for i := 0; i < 3; i++ {
-		socketPath := filepath.Join(socketDir, fmt.Sprintf("%s%d", nonWindowsDesktopSocketPrefix, i))
+		socketPath := filepath.Join(socketDir, fmt.Sprintf("%s_%d", nonWindowsDesktopSocketPrefix, i))
 		require.NoError(t, os.WriteFile(socketPath, []byte{}, 0600))
 	}
 
