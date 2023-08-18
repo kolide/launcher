8e3b511bdbe8ef817745d76765e2f5ba296809f1
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-08-18 12:22:37 -0400

    Add timing info

--- pkg/debug/checkups/osquery.go
+++ pkg/debug/checkups/osquery.go
@@ -32,16 +32,18 @@ func (o *osqueryCheckup) Run(ctx context.Context, extraWriter io.Writer) error {
 	defer cmdCancel()
 
 	cmd := exec.CommandContext(cmdCtx, osqueryPath, "--version")
+	startTime := time.Now().UnixMilli()
 	out, err := cmd.CombinedOutput()
+	executionTime := time.Now().UnixMilli() - startTime
 	if err != nil {
 		o.status = Failing
-		o.summary = fmt.Sprintf("could not run %s --version: output %s, error %+v", osqueryPath, string(out), err)
+		o.summary = fmt.Sprintf("could not run %s --version: output %s, error %+v, took %d ms", osqueryPath, string(out), err, executionTime)
 		return fmt.Errorf("running %s version: %w", osqueryPath, err)
 	}
 
 	versionOutput := strings.TrimSpace(string(out))
 	o.status = Passing
-	o.summary = fmt.Sprintf("%s (%s)", versionOutput, osqueryPath)
+	o.summary = fmt.Sprintf("%s (%s) -- took %d ms", versionOutput, osqueryPath, executionTime)
 
 	return nil
 }
