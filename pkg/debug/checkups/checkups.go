c5342fb9a6878069a0fa11f441c24ec99f8e8e71
James Pickett <James-Pickett@users.noreply.github.com>
2023-09-06 17:57:43 -0700

    add quarantine check to scan for quarantine files and meddlesome processes (#1333)

--- pkg/debug/checkups/checkups.go
+++ pkg/debug/checkups/checkups.go
@@ -109,6 +109,7 @@ func checkupsFor(k types.Knapsack, target targetBits) []checkupInt {
 		{&osqueryCheckup{k: k}, doctorSupported | flareSupported},
 		{&launcherFlags{}, doctorSupported | flareSupported},
 		{&gnomeExtensions{}, doctorSupported | flareSupported},
+		{&quarantine{}, doctorSupported | flareSupported},
 	}
 
 	checkupsToRun := make([]checkupInt, 0)
