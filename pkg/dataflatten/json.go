b73d4a3916c3a85bfb15fbc9fbb293f810f995cd
Micah-Kolide <109157253+Micah-Kolide@users.noreply.github.com>
2023-10-10 11:47:29 -0700

    Add utf16 decoding to json dataflatten

--- pkg/dataflatten/json.go
+++ pkg/dataflatten/json.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func JsonFile(file string, opts ...FlattenOpts) ([]Row, error) {
@@ -11,6 +14,16 @@ func JsonFile(file string, opts ...FlattenOpts) ([]Row, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Attempt to decode utf16 data.
+	valid_json := json.Valid(rawdata)
+	if !valid_json {
+		rawdata, _, err = transform.Bytes(unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewDecoder(), rawdata)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return Json(rawdata, opts...)
 }
 
