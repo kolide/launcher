07012fa8ca70cda0cfc92262b381b19baf46cc41
james pickett <james@kolide.co>
2023-10-17 14:14:33 -0500

    only uninstall on darwin

--- pkg/launcher/uninstall/uninstall.go
+++ pkg/launcher/uninstall/uninstall.go
@@ -3,6 +3,7 @@ package uninstall
 import (
 	"errors"
 	"os"
+	"runtime"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
@@ -12,6 +13,11 @@ import (
 
 func Uninstall(logger log.Logger, knapsack types.Knapsack) {
 	logger = log.With(logger, "component", "uninstall")
+
+	if runtime.GOOS != "darwin" {
+		level.Info(logger).Log("msg", "uninstall is currently only supported on darwin")
+		return
+	}
 
 	if err := knapsack.ConfigStore().Delete([]byte(osquery.NodeKeyKey)); err != nil {
 		level.Error(logger).Log("msg", "deleting node key", "err", err)
