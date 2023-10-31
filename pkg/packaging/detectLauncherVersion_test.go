f99bb1144387739a1242da97ec3e59cfd0433da4
zackattack01 <zack@kolide.co>
2023-10-31 12:50:10 -0400

    update version string format for darwin builds

--- pkg/packaging/detectLauncherVersion_test.go
+++ pkg/packaging/detectLauncherVersion_test.go
@@ -31,26 +31,55 @@ func TestFormatVersion(t *testing.T) {
 	t.Parallel()
 
 	var tests = []struct {
-		in  string
-		out string
+		in       string
+		platform PlatformFlavor
+		out      string
 	}{
 		{
-			in:  "0.9.2-26-g6146437",
-			out: "0.9.2.26",
+			in:       "0.9.2-26-g6146437",
+			platform: Windows,
+			out:      "0.9.2.26",
 		},
 		{
-			in:  "0.9.3-44",
-			out: "0.9.3.44",
+			in:       "0.9.3-44",
+			platform: Windows,
+			out:      "0.9.3.44",
 		},
 
 		{
-			in:  "0.9.5",
-			out: "0.9.5.0",
+			in:       "0.9.5",
+			platform: Windows,
+			out:      "0.9.5.0",
+		},
+		{
+			in:       "0.9.2-26-g6146437",
+			platform: Darwin,
+			out:      "0.9.2",
+		},
+		{
+			in:       "0.9.3-44",
+			platform: Darwin,
+			out:      "0.9.3",
+		},
+		{
+			in:       "v0.9.5",
+			platform: Darwin,
+			out:      "0.9.5",
+		},
+		{
+			in:       "v10.8.2-1002df",
+			platform: Darwin,
+			out:      "10.8.2",
+		},
+		{
+			in:       "0.9.2-26-g6146437",
+			platform: Linux,
+			out:      "0.9.2-26-g6146437",
 		},
 	}
 
 	for _, tt := range tests {
-		version, err := formatVersion(tt.in)
+		version, err := formatVersion(tt.in, tt.platform)
 		require.NoError(t, err)
 		require.Equal(t, tt.out, version)
 	}
