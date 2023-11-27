b5aec1363f17569785e74371799d6af50889742e
james pickett <james@kolide.co>
2023-11-27 14:56:39 -0800

    use dialer the way the net pkg does

--- ee/desktop/user/client/dialer_posix.go
+++ ee/desktop/user/client/dialer_posix.go
@@ -10,7 +10,7 @@ import (
 
 func dialContext(socketPath string) func(_ context.Context, _, _ string) (net.Conn, error) {
 	return func(ctx context.Context, _, _ string) (net.Conn, error) {
-		d := net.Dialer{}
+		var d net.Dialer
 		return d.DialContext(ctx, "unix", socketPath)
 	}
 }
