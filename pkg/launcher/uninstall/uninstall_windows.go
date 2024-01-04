494bbdd0dcc61324760844a47f066213eea5383f
James Pickett <james@kolide.co>
2024-01-04 14:55:31 -0800

    add windows disable autostart

--- pkg/launcher/uninstall/uninstall_windows.go
+++ pkg/launcher/uninstall/uninstall_windows.go
@@ -2,9 +2,33 @@ package uninstall
 
 import (
 	"context"
-	"errors"
+	"fmt"
+
+	"golang.org/x/sys/windows/svc/mgr"
 )
 
 func disableAutoStart(ctx context.Context) error {
-	return errors.New("not implemented")
+	svcMgr, err := mgr.Connect()
+	if err != nil {
+		return fmt.Errorf("connecting to windows service manager: %w", err)
+	}
+	defer svcMgr.Disconnect()
+
+	launcherSvc, err := svcMgr.OpenService("LauncherKolideK2Svc")
+	if err != nil {
+		return fmt.Errorf("opening launcher service: %w", err)
+	}
+	defer launcherSvc.Close()
+
+	cfg, err := launcherSvc.Config()
+	if err != nil {
+		return fmt.Errorf("getting launcher service config: %w", err)
+	}
+
+	cfg.StartType = mgr.StartManual
+	if err := launcherSvc.UpdateConfig(cfg); err != nil {
+		return fmt.Errorf("updating launcher service config: %w", err)
+	}
+
+	return nil
 }
