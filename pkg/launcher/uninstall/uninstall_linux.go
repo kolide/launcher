083516fe7f370e7aee8da75bc7ef1d1e3ee09bee
james-pickett <james@kolide.co>
2024-01-05 08:59:53 -0800

    add disable autostart linux

--- pkg/launcher/uninstall/uninstall_linux.go
+++ pkg/launcher/uninstall/uninstall_linux.go
@@ -2,9 +2,22 @@ package uninstall
 
 import (
 	"context"
-	"errors"
+	"fmt"
+
+	"github.com/kolide/launcher/ee/allowedcmd"
 )
 
 func disableAutoStart(ctx context.Context) error {
-	return errors.New("not implemented")
+	// the --now flag will disable and stop the service
+	cmd, err := allowedcmd.Systemctl(ctx, "disable", "--now", "launcher.kolide-k2.service")
+	if err != nil {
+		return fmt.Errorf("creating systemctl cmd: %w", err)
+	}
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("disabling auto start: %w: %s", err, out)
+	}
+
+	return nil
 }
