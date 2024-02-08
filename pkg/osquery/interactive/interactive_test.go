d911ffff88f1a57fff92df65a8e0e3469cdf3276
seph <seph@kolide.co>
2024-02-08 09:19:26 -0500

    fix test

--- pkg/osquery/interactive/interactive_test.go
+++ pkg/osquery/interactive/interactive_test.go
@@ -14,6 +14,7 @@ import (
 	"runtime"
 	"testing"
 
+	"github.com/go-kit/log"
 	"github.com/kolide/kit/fsutil"
 	"github.com/kolide/launcher/pkg/packaging"
 	"github.com/stretchr/testify/require"
@@ -83,7 +84,7 @@ func TestProc(t *testing.T) {
 			require.NoError(t, downloadOsquery(rootDir))
 			osquerydPath := filepath.Join(rootDir, "osqueryd")
 
-			proc, _, err := StartProcess(rootDir, osquerydPath, tt.osqueryFlags)
+			proc, _, err := StartProcess(log.NewNopLogger(), rootDir, osquerydPath, tt.osqueryFlags)
 
 			if tt.errContainsStr != "" {
 				require.Error(t, err)
