3305c3bd4586fe68be909dde3e5aad2748929489
james pickett <james@kolide.co>
2024-02-08 13:20:59 -0800

    split init logs into different files per platform

--- ee/debug/checkups/init_logs.go
+++ ee/debug/checkups/init_logs.go
@@ -3,12 +3,7 @@ package checkups
 import (
 	"archive/zip"
 	"context"
-	"fmt"
 	"io"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"runtime"
 )
 
 type InitLogs struct {
@@ -17,7 +12,7 @@ type InitLogs struct {
 }
 
 func (c *InitLogs) Name() string {
-	return "InitLogs"
+	return "Init Logs"
 }
 
 func (c *InitLogs) Run(ctx context.Context, fullFH io.Writer) error {
@@ -31,14 +26,7 @@ func (c *InitLogs) Run(ctx context.Context, fullFH io.Writer) error {
 	logZip := zip.NewWriter(fullFH)
 	defer logZip.Close()
 
-	switch runtime.GOOS {
-	case "darwin":
-		writeDarwinInitLogs(logZip)
-	case "windows":
-		writeWindowsInitLogs(ctx, logZip)
-	}
-
-	return nil
+	return writeInitLogs(ctx, logZip)
 }
 
 func (c *InitLogs) Status() Status {
@@ -55,40 +43,4 @@ func (c *InitLogs) ExtraFileName() string {
 
 func (c *InitLogs) Data() any {
 	return nil
-}
-
-func writeDarwinInitLogs(logZip *zip.Writer) {
-	stdMatches, _ := filepath.Glob("/var/log/kolide-k2/*")
-
-	for _, f := range stdMatches {
-		out, err := logZip.Create(filepath.Base(f))
-
-		if err != nil {
-			continue
-		}
-
-		in, err := os.Open(f)
-		if err != nil {
-			fmt.Fprintf(out, "error reading file:\n%s", err)
-			continue
-		}
-		defer in.Close()
-
-		io.Copy(out, in)
-	}
-}
-
-func writeWindowsInitLogs(ctx context.Context, logZip *zip.Writer) {
-	cmdStr := `Get-WinEvent -FilterHashtable @{LogName='Application'; ProviderName='launcher'} | ConvertTo-Json`
-	cmd := exec.CommandContext(ctx, "powershell", "-Command", cmdStr) //nolint:forbidigo // This is just a debugging tool, not launcher proper
-
-	outFile, err := logZip.Create("windows_launcher_events.json")
-	if err != nil {
-		return
-	}
-
-	cmd.Stderr = outFile
-	cmd.Stdout = outFile
-
-	cmd.Run()
 }
---
+++ ee/debug/checkups/init_logs_windows.go
@@ -0,0 +1,28 @@
+package checkups
+
+import (
+	"archive/zip"
+	"context"
+	"fmt"
+
+	"github.com/kolide/launcher/ee/allowedcmd"
+)
+
+func writeInitLogs(ctx context.Context, logZip *zip.Writer) error {
+	cmdStr := `Get-WinEvent -FilterHashtable @{LogName='Application'; ProviderName='launcher'} | ConvertTo-Json`
+
+	cmd, err := allowedcmd.Powershell(ctx, cmdStr...)
+	if err != nil {
+		return fmt.Errorf("creating powershell command: %w", err)
+	}
+
+	outFile, err := logZip.Create("windows_launcher_events.json")
+	if err != nil {
+		return fmt.Errorf("creating windows_launcher_events.json: %w", err)
+	}
+
+	cmd.Stderr = outFile
+	cmd.Stdout = outFile
+
+	return cmd.Run()
+}
