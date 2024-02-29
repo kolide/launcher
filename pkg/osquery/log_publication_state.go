7fe0e41c5efc7435d3747d3b2c4a2e44888f5408
zackattack01 <zack@kolide.co>
2024-02-29 13:15:53 -0500

    PR review: add and clarify comments

--- pkg/osquery/log_publication_state.go
+++ pkg/osquery/log_publication_state.go
@@ -16,7 +16,9 @@ const (
 // logPublicationState holds stateful logic to influence the log batch publication size
 // depending on prior successes or failures. The primary intent here is to prevent repeatedly
 // consuming the entire network bandwidth available to devices that are unable to ship
-// the standard maxBytesPerBatch inside of the connection timeout (currently 30 seconds, enforced cloud side)
+// the standard maxBytesPerBatch inside of the connection timeout (currently 30 seconds, enforced cloud side).
+// Note that we always expect these batches to be sent sequentially, BeginBatch -> EndBatch
+// this would need rework (likely state locking and batch ID tracking) to support concurrent batch publications
 type logPublicationState struct {
 	// maxBytesPerBatch is passed in from the extension opts and respected
 	// as a fixed upper limit for batch size, regardless of publication success/failure rates
@@ -24,7 +26,7 @@ type logPublicationState struct {
 	// currentMaxBytesPerBatch represents the (stateful) upper limit being enforced
 	currentMaxBytesPerBatch int
 	// currentBatchBufferFilled is used to indicate when a batch's success can be used
-	// to increase the threshold (we should avoid increasing threshold for a successful partial batch of logs)
+	// to increase the threshold (we only want to increase the threshold after sending full, not partial, batches successfully)
 	currentBatchBufferFilled bool
 	currentBatchStartTime    time.Time
 }
@@ -36,6 +38,9 @@ func NewLogPublicationState(maxBytesPerBatch int) *logPublicationState {
 	}
 }
 
+// BeginBatch sets the opening state before attempting to publish a batch of logs. Specifically, it must
+// - note the time (to determine if an error later was timeout related)
+// - note whether this batch is full (to determine whether success should increase the limit on success later)
 func (lps *logPublicationState) BeginBatch(startTime time.Time, bufferFilled bool) {
 	lps.currentBatchStartTime = startTime
 	lps.currentBatchBufferFilled = bufferFilled
