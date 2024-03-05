00bffdd590e4dac2a20f22859537948663775566
Zack Olson <zack@olsons.email>
2024-03-05 15:01:42 -0500

    Update ee/debug/checkups/checkups.go
    Co-authored-by: seph <seph@kolide.co>

--- ee/debug/checkups/checkups.go
+++ ee/debug/checkups/checkups.go
@@ -119,7 +119,7 @@ func checkupsFor(k types.Knapsack, target targetBits) []checkupInt {
 		{&serverDataCheckup{k: k}, doctorSupported | flareSupported | logSupported},
 		{&osqDataCollector{k: k}, doctorSupported | flareSupported},
 		{&osqRestartCheckup{k: k}, doctorSupported | flareSupported},
-		{&uninstallHistoryCheckup{k: k}, doctorSupported | flareSupported},
+		{&uninstallHistoryCheckup{k: k}, flareSupported},
 	}
 
 	checkupsToRun := make([]checkupInt, 0)
