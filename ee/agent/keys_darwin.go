78a0cd433d353e268a38f50f5363712d82cac38e
james pickett <james@kolide.co>
2024-03-07 13:10:27 -0800

    remove unneeded code

--- ee/agent/keys_darwin.go
+++ ee/agent/keys_darwin.go
@@ -4,8 +4,6 @@
 package agent
 
 import (
-	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"log/slog"
@@ -15,32 +13,9 @@ import (
 )
 
 func setupHardwareKeys(slogger *slog.Logger, store types.GetterSetterDeleter) (keyInt, error) {
-
-	// fetch any existing key data
-	_, pubData, err := fetchKeyData(store)
-	if err != nil {
-		return nil, err
-	}
-
 	ses, err := secureenclavesigner.New(slogger, store)
 	if err != nil {
 		return nil, fmt.Errorf("creating secureenclave signer: %w", err)
-	}
-
-	if pubData != nil {
-		if err := json.Unmarshal(pubData, &ses); err != nil {
-			// data is corrupt or not in the expected format, clear it
-			slogger.Log(context.TODO(), slog.LevelError,
-				"could not unmarshal stored key data, clearing key data and generating new keys",
-				"err", err,
-			)
-			clearKeyData(slogger, store)
-
-			ses, err = secureenclavesigner.New(slogger, store)
-			if err != nil {
-				return nil, fmt.Errorf("creating secureenclave signer: %w", err)
-			}
-		}
 	}
 
 	// this is kind of weird, but we need to call public to ensure the key is generated
