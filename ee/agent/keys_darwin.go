9698074274507de5797769e936d1c493087468d8
james pickett <james@kolide.co>
2024-03-13 07:06:25 -0700

    revert revert of secure enclave keys

--- ee/agent/keys_darwin.go
+++ ee/agent/keys_darwin.go
@@ -4,46 +4,24 @@
 package agent
 
 import (
-	"errors"
+	"context"
+	"fmt"
 	"log/slog"
 
 	"github.com/kolide/launcher/ee/agent/types"
+	"github.com/kolide/launcher/ee/secureenclavesigner"
+	"github.com/kolide/launcher/pkg/traces"
 )
 
-// nolint:unused
-func setupHardwareKeys(slogger *slog.Logger, store types.GetterSetterDeleter) (keyInt, error) {
-	// We're seeing issues where launcher hangs (and does not complete startup) on the
-	// Sonoma Beta 2 release when trying to interact with the secure enclave below, on
-	// CreateKey. Since we don't expect this to work at the moment anyway, we are
-	// short-circuiting and returning early for now.
-	return nil, errors.New("secure enclave is not currently supported")
+func setupHardwareKeys(ctx context.Context, slogger *slog.Logger, store types.GetterSetterDeleter) (keyInt, error) {
+	ctx, span := traces.StartSpan(ctx)
+	defer span.End()
 
-	/*
-		_, pubData, err := fetchKeyData(store)
-		if err != nil {
-			return nil, err
-		}
+	ses, err := secureenclavesigner.New(ctx, slogger, store)
+	if err != nil {
+		traces.SetError(span, fmt.Errorf("creating secureenclave signer: %w", err))
+		return nil, fmt.Errorf("creating secureenclave signer: %w", err)
+	}
 
-		if pubData == nil {
-			level.Info(logger).Log("msg", "Generating new keys")
-
-			var err error
-			pubData, err = secureenclave.CreateKey()
-			if err != nil {
-				return nil, fmt.Errorf("creating key: %w", err)
-			}
-
-			if err := storeKeyData(store, nil, pubData); err != nil {
-				clearKeyData(logger, store)
-				return nil, fmt.Errorf("storing key: %w", err)
-			}
-		}
-
-		k, err := secureenclave.New(pubData)
-		if err != nil {
-			return nil, fmt.Errorf("creating secureenclave signer: %w", err)
-		}
-
-		return k, nil
-	*/
+	return ses, nil
 }
--- ee/debug/shipper/shipper.go
+++ ee/debug/shipper/shipper.go
@@ -13,6 +13,7 @@ import (
 	"net/url"
 	"os"
 	"os/user"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -206,7 +207,11 @@ func signHttpRequest(req *http.Request, body []byte) {
 	}
 
 	sign(agent.LocalDbKeys(), control.HeaderKey, control.HeaderSignature, req)
-	sign(agent.HardwareKeys(), control.HeaderKey2, control.HeaderSignature2, req)
+
+	// hardware signing is not implemented for darwin
+	if runtime.GOOS != "darwin" {
+		sign(agent.HardwareKeys(), control.HeaderKey2, control.HeaderSignature2, req)
+	}
 }
 
 func launcherData(k types.Knapsack, note string) ([]byte, error) {
