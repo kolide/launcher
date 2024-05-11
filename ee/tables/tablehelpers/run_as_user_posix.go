4571bbcc7d4e87d4a4f5f3a34a50306c286dae76
Micah-Kolide <109157253+Micah-Kolide@users.noreply.github.com>
2024-05-10 17:19:28 -0700

    Format code

--- ee/tables/tablehelpers/run_as_user_posix.go
+++ ee/tables/tablehelpers/run_as_user_posix.go
@@ -49,7 +49,7 @@ func WithUid(uid string) ExecOps {
 		}
 
 		// Set PWD and HOME to the running user's home directory for supporting executes that use them as the prefix to create temp files.
-		cmd.Env = append(cmd.Environ(), "PWD=" + runningUser.HomeDir, "HOME=" + runningUser.HomeDir)
+		cmd.Env = append(cmd.Environ(), "PWD="+runningUser.HomeDir, "HOME="+runningUser.HomeDir)
 
 		return nil
 	}
