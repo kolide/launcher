ac9a49befc55c89b419181a81e016d255138c717
zackattack01 <zack@kolide.co>
2024-05-17 16:08:10 -0400

    test updates

--- ee/agent/storage/sqlite/logstore_sqlite_test.go
+++ ee/agent/storage/sqlite/logstore_sqlite_test.go
@@ -2,14 +2,15 @@ package agentsqlite
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
-	"github.com/kolide/launcher/ee/agent/flags/keys"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAppendValue(t *testing.T) {
+func TestAppendAndIterateValues(t *testing.T) {
 	t.Parallel()
 
 	testRootDir := t.TempDir()
@@ -17,16 +18,28 @@ func TestAppendValue(t *testing.T) {
 	s, err := OpenRW(context.TODO(), testRootDir, WatchdogLogStore)
 	require.NoError(t, err, "creating test store")
 
-	flagKey := []byte(keys.UpdateChannel.String())
-	flagVal := []byte("beta")
+	startTime := time.Now()
+	expectedLogCount := 5
+	for i := 0; i < expectedLogCount; i++ {
+		currTime := startTime.Add(time.Duration(i) * time.Minute)
+		logEntry := fmt.Sprintf(`{"time":"%s", "msg":"testMessage%d"}`, currTime.Format(time.RFC3339), i)
+		require.NoError(t, s.AppendValue(currTime.Unix(), []byte(logEntry)), "expected no error appending value row")
+	}
 
-	startTime := time.Now().Unix()
-	logEntry := `{"time":"2024-05-13T18:29:31.7829101Z", "msg":"testmsg"}`
-	require.NoError(t, s.AppendValue(startTime, []byte(logEntry)), "expected no error appending value row")
+	logsSeen := 0
+	err = s.ForEach(func(rowid, timestamp int64, v []byte) error {
+		logRecord := make(map[string]any)
 
-	returnedVal, err := s.Get(flagKey)
-	require.NoError(t, err, "expected no error getting value")
-	require.Equal(t, flagVal, returnedVal, "flag value mismatch")
+		require.NoError(t, json.Unmarshal(v, &logRecord), "expected to be able to unmarshal row value")
+		expectedTime := startTime.Add(time.Duration(logsSeen) * time.Minute)
+		require.Equal(t, expectedTime.Unix(), timestamp, "expected log timestamp to match")
+
+		logsSeen++
+		return nil
+	})
+
+	require.NoError(t, err, "expected no error iterating over new logs")
+	require.Equal(t, expectedLogCount, logsSeen, "did not see expected count of logs during iteration")
 
 	require.NoError(t, s.Close())
 }
