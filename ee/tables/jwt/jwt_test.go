17360f9079b0c5042a25ae4c4cf4f0e7f417e589
Micah-Kolide <109157253+Micah-Kolide@users.noreply.github.com>
2024-06-05 16:27:42 -0700

    Finish rsa256 test

--- ee/tables/jwt/jwt_test.go
+++ ee/tables/jwt/jwt_test.go
@@ -22,8 +22,9 @@ func TestTransformOutput(t *testing.T) {
 	var tests = []struct {
 		name string
 		path string
-		keys map[string]string
 		raw  []string
+		keys map[string]string
+		res  map[string]string
 	}{
 		{
 			name: "rsa256 JWT",
@@ -31,12 +32,22 @@ func TestTransformOutput(t *testing.T) {
 			keys: map[string]string{
 				"test": rsa256_key,
 			},
+			res: map[string]string{
+				"parent":          "",
+				"key":             "verified",
+				"fullkey":         "verified",
+				"value":           "client_valid",
+				"query":           "*",
+				"path":            "testdata/rsa256.raw",
+				"include_raw_jwt": "false",
+				"signing_keys":    "{\"test\":\"-----BEGIN PUBLIC KEY-----\\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu1SU1LfVLPHCozMxH2Mo\\n4lgOEePzNm0tRgeLezV6ffAt0gunVTLw7onLRnrq0/IzW7yWR7QkrmBL7jTKEn5u\\n+qKhbwKfBstIs+bMY2Zkp18gnTxKLxoS2tFczGkPLPgizskuemMghRniWaoLcyeh\\nkd3qqGElvW/VDL5AaWTg0nLVkjRo9z+40RQzuVaE8AkAFmxZzow3x+VJYKdjykkJ\\n0iT9wCS0DRTXu269V264Vf/3jvredZiKRkgwlL9xNAwxXFg0x/XFw005UWVRIkdg\\ncKWTjpBP2dPwVZ4WWC+9aGVd+Gyn1o0CLelf4rEjGoXbAAEgAqeGUxrcIlbjXfbc\\nmwIDAQAB\\n-----END PUBLIC KEY-----\"}",
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		tt := tt
-		
+
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -49,7 +60,9 @@ func TestTransformOutput(t *testing.T) {
 			})
 
 			rows, err := jwtTable.generate(context.TODO(), mockQC)
+
 			require.NoError(t, err)
+			require.Contains(t, rows, tt.res, "generated rows should contain the expected result")
 		})
 	}
 }
