3d0c13547a9ccb15ed80ec675fdea628fefa37a3
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2024-06-28 16:01:26 -0400

    Add a table test

--- ee/katc/table_test.go
+++ ee/katc/table_test.go
@@ -1,12 +1,133 @@
 package katc
 
 import (
+	"context"
+	"database/sql"
+	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
+	"github.com/golang/snappy"
+	"github.com/google/uuid"
+	"github.com/kolide/launcher/pkg/log/multislogger"
 	"github.com/osquery/osquery-go/plugin/table"
 	"github.com/stretchr/testify/require"
+
+	_ "modernc.org/sqlite"
 )
+
+func Test_generate_SqliteBackedIndexedDB(t *testing.T) {
+	t.Parallel()
+
+	// This test validates generation of table results. It uses a sqlite-backed
+	// IndexedDB as a source, which means it also exercises functionality from
+	// sqlite.go, snappy.go, and structured_clone.go.
+
+	// First, set up the data we expect to retrieve.
+	expectedColumn := "uuid"
+	u, err := uuid.NewRandom()
+	require.NoError(t, err, "generating test UUID")
+	expectedColumnValue := u.String()
+
+	// Serialize the row data, reversing the deserialization operation in
+	// structured_clone.go.
+	serializedUuid := []byte(expectedColumnValue)
+	serializedObj := append([]byte{
+		// Header
+		0x00, 0x00, 0x00, 0x00, // header tag data -- discarded
+		0x00, 0x00, 0xf1, 0xff, // LE `tagHeader`
+		// Begin object
+		0x00, 0x00, 0x00, 0x00, // object tag data -- discarded
+		0x08, 0x00, 0xff, 0xff, // LE `tagObject`
+		// Begin UUID key
+		0x04, 0x00, 0x00, 0x80, // LE data about upcoming string: length 4 (remaining bytes), is ASCII
+		0x04, 0x00, 0xff, 0xff, // LE `tagString`
+		0x75, 0x75, 0x69, 0x64, // "uuid"
+		0x00, 0x00, 0x00, 0x00, // padding to get to 8-byte word boundary
+		// End UUID key
+		// Begin UUID value
+		0x24, 0x00, 0x00, 0x80, // LE data about upcoming string: length 36 (remaining bytes), is ASCII
+		0x04, 0x00, 0xff, 0xff, // LE `tagString`
+	},
+		serializedUuid...,
+	)
+	serializedObj = append(serializedObj,
+		0x00, 0x00, 0x00, 0x00, // padding to get to 8-byte word boundary for UUID string
+		// End UUID value
+		0x00, 0x00, 0x00, 0x00, // tag data -- discarded
+		0x13, 0x00, 0xff, 0xff, // LE `tagEndOfKeys` 0xffff0013
+	)
+
+	// Now compress the serialized row data, reversing the decompression operation
+	// in snappy.go
+	compressedObj := snappy.Encode(nil, serializedObj)
+
+	// Now, create a sqlite database to store this data in.
+	databaseDir := t.TempDir()
+	sourceFilepath := filepath.Join(databaseDir, "test.sqlite")
+	f, err := os.Create(sourceFilepath)
+	require.NoError(t, err, "creating source db")
+	require.NoError(t, f.Close(), "closing source db file")
+	conn, err := sql.Open("sqlite", sourceFilepath)
+	require.NoError(t, err)
+	_, err = conn.Exec(`CREATE TABLE object_data(data TEXT NOT NULL PRIMARY KEY) WITHOUT ROWID;`)
+	require.NoError(t, err, "creating test table")
+
+	// Insert compressed object into the database
+	_, err = conn.Exec("INSERT INTO object_data (data) VALUES (?);", compressedObj)
+	require.NoError(t, err, "inserting into sqlite database")
+	require.NoError(t, conn.Close(), "closing sqlite database")
+
+	// At long last, our source is adequately configured.
+	// Move on to constructing our KATC table.
+	cfg := katcTableConfig{
+		Source: katcSourceType{
+			name:     sqliteSourceType,
+			dataFunc: sqliteData,
+		},
+		Platform: runtime.GOOS,
+		Columns:  []string{expectedColumn},
+		Path:     filepath.Join(databaseDir, "*.sqlite"), // All sqlite files in the test directory
+		Query:    "SELECT data FROM object_data;",
+		RowTransformSteps: []rowTransformStep{
+			{
+				name:          snappyDecodeTransformStep,
+				transformFunc: snappyDecode,
+			},
+			{
+				name:          structuredCloneDeserializeTransformStep,
+				transformFunc: structuredCloneDeserialize,
+			},
+		},
+	}
+	testTable, _ := newKatcTable("test_katc_table", cfg, multislogger.NewNopLogger())
+
+	// Make a query context restricting the source to our exact source sqlite database
+	queryContext := table.QueryContext{
+		Constraints: map[string]table.ConstraintList{
+			sourcePathColumnName: {
+				Constraints: []table.Constraint{
+					{
+						Operator:   table.OperatorEquals,
+						Expression: sourceFilepath,
+					},
+				},
+			},
+		},
+	}
+
+	// At long last: run a query
+	results, err := testTable.generate(context.TODO(), queryContext)
+	require.NoError(t, err)
+
+	// Validate results
+	require.Equal(t, 1, len(results), "only one row expected")
+	require.Contains(t, results[0], sourcePathColumnName, "missing source_path column")
+	require.Equal(t, sourceFilepath, results[0][sourcePathColumnName])
+	require.Contains(t, results[0], expectedColumn, "expected column missing")
+	require.Equal(t, expectedColumnValue, results[0][expectedColumn], "data mismatch")
+}
 
 func Test_checkSourcePathConstraints(t *testing.T) {
 	t.Parallel()
