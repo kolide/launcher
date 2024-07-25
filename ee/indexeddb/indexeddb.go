affb8c06e4303d1ed395f884c6b1c67ecbe58ff8
RebeccaMahany <rebeccamahany@gmail.com>
2024-07-25 15:41:54 -0400

    Log both open and recover errors when opening indexeddb

--- ee/indexeddb/indexeddb.go
+++ ee/indexeddb/indexeddb.go
@@ -46,14 +46,14 @@ func QueryIndexeddbObjectStore(dbLocation string, dbName string, objectStoreName
 		DisableSeeksCompaction: true, // no need to perform compaction
 		Strict:                 opt.StrictAll,
 	}
-	db, err := leveldb.OpenFile(tempDbCopyLocation, opts)
-	if err != nil {
+	db, dbOpenErr := leveldb.OpenFile(tempDbCopyLocation, opts)
+	if dbOpenErr != nil {
 		// Perform recover in case we missed something while copying
-		db, err = leveldb.RecoverFile(tempDbCopyLocation, opts)
-	}
-
-	if err != nil {
-		return nil, fmt.Errorf("opening db: %w", err)
+		var dbRecoverErr error
+		db, dbRecoverErr = leveldb.RecoverFile(tempDbCopyLocation, opts)
+		if dbRecoverErr != nil {
+			return nil, fmt.Errorf("opening db: `%+v`; recovering db: %w", dbOpenErr, dbRecoverErr)
+		}
 	}
 	defer db.Close()
 
