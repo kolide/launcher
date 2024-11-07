24f33dccf4c4111bf3150e140f401f933d4e814b
zackattack01 <zack@kolide.co>
2024-11-07 12:23:42 -0500

    wire watchdog task logic into controller

--- ee/watchdog/controller_windows.go
+++ ee/watchdog/controller_windows.go
@@ -7,12 +7,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
 	"path/filepath"
 	"slices"
+	"strings"
 	"time"
 
+	"github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
 	"github.com/kolide/launcher/ee/agent/flags/keys"
 	agentsqlite "github.com/kolide/launcher/ee/agent/storage/sqlite"
 	"github.com/kolide/launcher/ee/agent/types"
@@ -26,8 +30,7 @@ import (
 const (
 	launcherWatchdogServiceName string = `LauncherKolideWatchdogSvc`
 	launcherServiceName         string = `LauncherKolideK2Svc`
-
-	serviceDoesNotExistError string = "The specified service does not exist as an installed service."
+	taskDateFormat              string = "2006-01-02T15:04:05"
 )
 
 // WatchdogController is responsible for:
@@ -178,58 +181,37 @@ func (wc *WatchdogController) ServiceEnabledChanged(enabled bool) {
 		return
 	}
 
-	var serviceManager *mgr.Mgr
-	var err error
-
-	if err := backoff.WaitFor(func() error {
-		serviceManager, err = mgr.Connect()
-		if err != nil {
-			return fmt.Errorf("err connecting to service control manager: %w", err)
-		}
-
-		return nil
-	}, 5*time.Second, 500*time.Millisecond); err != nil {
-		wc.slogger.Log(ctx, slog.LevelError,
-			"timed out connecting to service control manager",
-			"err", err,
-		)
-
-		return
-	}
-
-	defer serviceManager.Disconnect()
+	// TODO generate task name based on identifier
+	taskName := "LauncherKolideK2WakerTask"
 
 	if !enabled {
-		// TODO replace with remove task
-		err := RemoveService(serviceManager)
-		if err != nil {
-			if err.Error() == serviceDoesNotExistError {
-				return
-			}
-
+		if err := RemoveWatchdogTask(taskName); err != nil {
 			wc.slogger.Log(ctx, slog.LevelWarn,
-				"encountered error removing watchdog service",
+				"encountered error removing watchdog task",
 				"err", err,
 			)
 
 			return
 		}
 
-		wc.slogger.Log(ctx, slog.LevelInfo, "removed watchdog service")
+		wc.slogger.Log(ctx, slog.LevelInfo, "removed watchdog task")
 
 		return
 	}
 
 	// we're enabling the watchdog task- we can safely always reinstall our latest version here
-	if err = wc.installWatchdogTask(); err != nil {
+	if err := InstallWatchdogTask(taskName); err != nil {
 		wc.slogger.Log(ctx, slog.LevelError,
 			"encountered error installing watchdog task",
 			"err", err,
 		)
 	}
+
+	wc.slogger.Log(ctx, slog.LevelInfo, "completed watchdog scheduled task installation")
 }
 
-func (wc *WatchdogController) getExecutablePath() (string, error) {
+// TODO make this interpolate identifier in path generation
+func getExecutablePath() (string, error) {
 	defaultBinDir := launcher.DefaultPath(launcher.BinDirectory)
 	defaultLauncherLocation := filepath.Join(defaultBinDir, "launcher.exe")
 	// do some basic sanity checking to prevent installation from a bad path
@@ -241,9 +223,238 @@ func (wc *WatchdogController) getExecutablePath() (string, error) {
 	return defaultLauncherLocation, nil
 }
 
-func (wc *WatchdogController) installWatchdogTask() error {
-	ctx := context.TODO()
-	installedExePath, err := wc.getExecutablePath()
+func InstallWatchdogTask(taskName string) error {
+	// Initialize COM
+	ole.CoInitialize(0)
+	defer ole.CoUninitialize()
+
+	// Create a Task Scheduler object
+	schedService, err := oleutil.CreateObject("Schedule.Service")
+	if err != nil {
+		return fmt.Errorf("creating schedule service object: %w", err)
+	}
+	defer schedService.Release()
+
+	// Get the Task Scheduler service interface
+	scheduler, err := schedService.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		return err
+	}
+	defer scheduler.Release()
+
+	// Connect to the local machine
+	_, err = oleutil.CallMethod(scheduler, "Connect")
+	if err != nil {
+		return fmt.Errorf("failed to connect to Task Scheduler: %w", err)
+	}
+
+	// Get the root task folder
+	rootFolderVar, err := oleutil.CallMethod(scheduler, "GetFolder", `\`)
+	if err != nil {
+		return fmt.Errorf("failed to get root folder: %w", err)
+	}
+
+	rootFolder := rootFolderVar.ToIDispatch()
+	defer rootFolder.Release()
+
+	// Create a new task definition
+	taskDefinitionTemplate, err := oleutil.CallMethod(scheduler, "NewTask", 0)
+	if err != nil {
+		return fmt.Errorf("failed to create new task definition: %w", err)
+	}
+
+	taskDefinition := taskDefinitionTemplate.ToIDispatch()
+	defer taskDefinition.Release()
+
+	installationDate := time.Now().Format(taskDateFormat)
+
+	// Get task registration info
+	regInfoProp, err := oleutil.GetProperty(taskDefinition, "RegistrationInfo")
+	if err != nil {
+		return fmt.Errorf("getting registration info property: %w", err)
+	}
+	regInfo := regInfoProp.ToIDispatch()
+	defer regInfo.Release()
+
+	if _, err = oleutil.PutProperty(regInfo, "Description", "Kolide agent waker"); err != nil {
+		return fmt.Errorf("setting reginfo description: %w", err)
+	}
+
+	if _, err := oleutil.PutProperty(regInfo, "Author", "Kolide, Inc."); err != nil {
+		return fmt.Errorf("setting reginfo author: %w", err)
+	}
+
+	if _, err := oleutil.PutProperty(regInfo, "Date", installationDate); err != nil {
+		return fmt.Errorf("setting reginfo date: %w", err)
+	}
+
+	principalProp, err := oleutil.GetProperty(taskDefinition, "Principal")
+	if err != nil {
+		return fmt.Errorf("getting principal property: %w", err)
+	}
+
+	principal := principalProp.ToIDispatch()
+	defer principal.Release()
+
+	// see all principal settings here https://learn.microsoft.com/en-us/windows/win32/api/taskschd/nn-taskschd-iprincipal
+	// 1=TASK_RUNLEVEL_HIGHEST
+	if _, err := oleutil.PutProperty(principal, "RunLevel", uint(1)); err != nil {
+		return fmt.Errorf("setting run level: %w", err)
+	}
+
+	// Get the task settings
+	settingsProp, err := oleutil.GetProperty(taskDefinition, "Settings")
+	if err != nil {
+		return fmt.Errorf("getting settings property: %w", err)
+	}
+
+	settings := settingsProp.ToIDispatch()
+	defer settings.Release()
+
+	// TODO check all of these errors
+	// see all available task settings here https://learn.microsoft.com/en-us/windows/win32/api/taskschd/nn-taskschd-itasksettings
+	// task will be enabled on creation
+	if _, err = oleutil.PutProperty(settings, "Enabled", true); err != nil {
+		return fmt.Errorf("setting enabled flag: %w", err)
+	}
+
+	// start the task at any time after its scheduled time has passed
+	if _, err = oleutil.PutProperty(settings, "StartWhenAvailable", true); err != nil {
+		return fmt.Errorf("setting StartWhenAvailable flag: %w", err)
+	}
+
+	// task will be started even if the computer is running on batteries
+	if _, err = oleutil.PutProperty(settings, "DisallowStartIfOnBatteries", false); err != nil {
+		return fmt.Errorf("setting DisallowStartIfOnBatteries flag: %w", err)
+	}
+
+	// task will be continue even if the computer changes power source to battery
+	if _, err = oleutil.PutProperty(settings, "StopIfGoingOnBatteries", false); err != nil {
+		return fmt.Errorf("setting StopIfGoingOnBatteries flag: %w", err)
+	}
+
+	// see compatibility options here https://learn.microsoft.com/en-us/windows/win32/api/taskschd/ne-taskschd-task_compatibility
+	// 2=TASK_COMPATIBILITY_V2 - recommended unless you need to support Windows XP, Windows Server 2003, or Windows 2000
+	if _, err = oleutil.PutProperty(settings, "Compatibility", uint(2)); err != nil {
+		return fmt.Errorf("setting Compatibility flag: %w", err)
+	}
+
+	idleSettingsProp, err := oleutil.GetProperty(settings, "IdleSettings")
+	if err != nil {
+		return fmt.Errorf("getting idle settings property: %w", err)
+	}
+
+	idleSettings := idleSettingsProp.ToIDispatch()
+	defer idleSettings.Release()
+
+	// see idle settings here https://learn.microsoft.com/en-us/windows/win32/taskschd/taskschedulerschema-idlesettings-settingstype-element
+	// do not stop the task if an idle condition ends before the task is completed
+	if _, err = oleutil.PutProperty(idleSettings, "StopOnIdleEnd", false); err != nil {
+		return fmt.Errorf("setting StopOnIdleEnd idlesetting: %w", err)
+	}
+
+	// Define the trigger
+	triggersProp, err := oleutil.GetProperty(taskDefinition, "Triggers")
+	if err != nil {
+		return fmt.Errorf("getting triggers property: %w", err)
+	}
+
+	triggers := triggersProp.ToIDispatch()
+	defer triggers.Release()
+	// see trigger types here https://learn.microsoft.com/en-us/windows/win32/api/taskschd/ne-taskschd-task_trigger_type2
+	createTriggerResp, err := oleutil.CallMethod(triggers, "Create", uint(0)) // 0=TASK_TRIGGER_EVENT
+	if err != nil {
+		log.Fatalf("encountered error creating trigger: %s", err.Error())
+	}
+
+	trigger := createTriggerResp.ToIDispatch()
+	defer trigger.Release()
+
+	if _, err = oleutil.PutProperty(trigger, "ExecutionTimeLimit", "PT1M"); err != nil {
+		return fmt.Errorf("setting execution time limit property")
+	}
+
+	// found the guid here https://github.com/capnspacehook/taskmaster/blob/1629df7c85e96aab410af7f1747ba264d3276505/fill.go#L168
+	eventTrigger, err := trigger.QueryInterface(ole.NewGUID("{d45b0167-9653-4eef-b94f-0732ca7af251}"))
+	if err != nil {
+		return fmt.Errorf("getting trigger interface: %w", err)
+	}
+	defer eventTrigger.Release()
+
+	eventSubscriptionTemplate := `
+<QueryList>
+	<Query Path="System">
+		<Select Path="System">*[System[Provider[@Name='Microsoft-Windows-Kernel-Power'] and (EventID=%d or EventID=%d)]]</Select>
+		<Select Path="System">*[System[Provider[@Name='Microsoft-Windows-Power-Troubleshooter'] and (EventID=%d)]]</Select>
+	</Query>
+</QueryList>
+`
+	eventSubscription := fmt.Sprintf(eventSubscriptionTemplate, 507, 107, 1)
+
+	if _, err = oleutil.PutProperty(eventTrigger, "Subscription", eventSubscription); err != nil {
+		return fmt.Errorf("setting subscription property: %w", err)
+	}
+	// see details for how this string is created here: https://learn.microsoft.com/en-us/windows/win32/taskschd/eventtrigger-delay
+	// PT1M here means 1 minute
+	if _, err = oleutil.PutProperty(eventTrigger, "Delay", "PT1M"); err != nil {
+		return fmt.Errorf("setting event trigger delay: %w", err)
+	}
+
+	// add another trigger, this one time based- repeat every 30 minutes
+	createTimeTriggerResp, err := oleutil.CallMethod(triggers, "Create", uint(1)) // 1=TASK_TRIGGER_TIME
+	if err != nil {
+		return fmt.Errorf("error creating time trigger object: %w", err)
+	}
+
+	timeTrigger := createTimeTriggerResp.ToIDispatch()
+	defer timeTrigger.Release()
+
+	if _, err := oleutil.PutProperty(timeTrigger, "Enabled", true); err != nil {
+		return fmt.Errorf("setting time trigger enabled: %w", err)
+	}
+
+	// set the execution timeout, PT1M=1 minute
+	if _, err := oleutil.PutProperty(timeTrigger, "ExecutionTimeLimit", "PT1M"); err != nil {
+		return fmt.Errorf("setting time trigger execution time limit: %w", err)
+	}
+
+	if _, err = oleutil.PutProperty(timeTrigger, "StartBoundary", installationDate); err != nil {
+		return fmt.Errorf("setting time trigger start boundary: %w", err)
+	}
+
+	repetitionObj, err := oleutil.GetProperty(timeTrigger, "Repetition")
+	if err != nil {
+		return fmt.Errorf("getting time trigger repetition property: %w", err)
+	}
+
+	repetition := repetitionObj.ToIDispatch()
+	defer repetition.Release()
+
+	// set the repetition interval. PT30M=every 30 minutes
+	if _, err = oleutil.PutProperty(repetition, "Interval", "PT30M"); err != nil {
+		return fmt.Errorf("setting time trigger interval: %w", err)
+	}
+
+	// Define the action
+	actionsProp, err := oleutil.GetProperty(taskDefinition, "Actions")
+	if err != nil {
+		return fmt.Errorf("getting actions property: %w", err)
+	}
+
+	actions := actionsProp.ToIDispatch()
+	defer actions.Release()
+
+	// see action types here https://learn.microsoft.com/en-us/windows/win32/api/taskschd/ne-taskschd-task_action_type
+	// 0=TASK_ACTION_EXEC
+	execActionTemplate, err := oleutil.CallMethod(actions, "Create", uint(0))
+	if err != nil {
+		return fmt.Errorf("creating event action: %w", err)
+	}
+
+	execAction := execActionTemplate.ToIDispatch()
+	defer execAction.Release()
+
+	installedExePath, err := getExecutablePath()
 	if err != nil {
 		return fmt.Errorf("determining watchdog executable path: %w", err)
 	}
@@ -251,12 +462,75 @@ func (wc *WatchdogController) installWatchdogTask() error {
 	serviceArgs := []string{"watchdog"}
 	// add any original service arguments from the main launcher service invocation (currently running)
 	// this is likely just a pointer to the launcher.flags file but we want to ensure that the watchdog
-	// has insight into the same options for early service configuration, logging, etc.
+	// has insight into the same options for service name determination based on identifier, logging setup, etc.
 	serviceArgs = append(serviceArgs, os.Args[2:]...)
 
-	// TODO add task installation logic
+	if _, err = oleutil.PutProperty(execAction, "Path", installedExePath); err != nil {
+		return fmt.Errorf("setting action path: %w", err)
+	}
 
-	wc.slogger.Log(ctx, slog.LevelInfo, "completed watchdog scheduled task installation")
+	argString := strings.Join(serviceArgs, " ")
+	if _, err = oleutil.PutProperty(execAction, "Arguments", argString); err != nil {
+		return fmt.Errorf("setting action arguments: %w", err)
+	}
+
+	// Register the task
+	_, err = oleutil.CallMethod(rootFolder, "RegisterTaskDefinition",
+		taskName,       // Task name
+		taskDefinition, // Task definition
+		uint(6),        // Flags: 6=TASK_CREATE_OR_UPDATE see https://learn.microsoft.com/en-us/windows/win32/api/taskschd/ne-taskschd-task_creation
+		"SYSTEM",       // User: run as system
+		nil,            // password (nil for the current user, we expect this installed from SYSTEM)
+		uint(5),        // 5=TASK_LOGON_SERVICE_ACCOUNT see https://learn.microsoft.com/en-us/windows/win32/api/taskschd/ne-taskschd-task_logon_type
+		nil,            // SDDL (security descriptor definition language string, nil for our purposes here)
+	)
+
+	if err != nil {
+		return fmt.Errorf("registering task definition: %w", err)
+	}
+
+	return nil
+}
+
+func RemoveWatchdogTask(taskName string) error {
+	// Initialize COM
+	ole.CoInitialize(0)
+	defer ole.CoUninitialize()
+
+	// Create a Task Scheduler object
+	schedService, err := oleutil.CreateObject("Schedule.Service")
+	if err != nil {
+		return fmt.Errorf("creating schedule service object: %w", err)
+	}
+	defer schedService.Release()
+
+	// Get the Task Scheduler service interface
+	scheduler, err := schedService.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		return err
+	}
+	defer scheduler.Release()
+
+	// Connect to the local machine
+	_, err = oleutil.CallMethod(scheduler, "Connect")
+	if err != nil {
+		return fmt.Errorf("failed to connect to Task Scheduler: %w", err)
+	}
+
+	// Get the root task folder
+	rootFolderVar, err := oleutil.CallMethod(scheduler, "GetFolder", `\`)
+	if err != nil {
+		return fmt.Errorf("failed to get root folder: %w", err)
+	}
+
+	rootFolder := rootFolderVar.ToIDispatch()
+	defer rootFolder.Release()
+
+	// Delete the task
+	_, err = oleutil.CallMethod(rootFolder, "DeleteTask", taskName, 0)
+	if err != nil {
+		return fmt.Errorf("failed to delete task %s: %w", taskName, err)
+	}
 
 	return nil
 }
