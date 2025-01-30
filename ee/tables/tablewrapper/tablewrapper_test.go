05289c1b39efa375aef162577a76a59a64631140
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2025-01-30 17:00:55 -0500

    Allow a maximum of five ongoing queries per table (#2078)

--- ee/tables/tablewrapper/tablewrapper_test.go
+++ ee/tables/tablewrapper/tablewrapper_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNew(t *testing.T) {
+func TestCall(t *testing.T) {
 	t.Parallel()
 
 	expectedName := "test_table"
@@ -37,7 +37,7 @@ func TestNew(t *testing.T) {
 	require.Equal(t, expectedRow, resp.Response[0])
 }
 
-func TestNew_handlesTimeout(t *testing.T) {
+func TestCall_handlesTimeout(t *testing.T) {
 	t.Parallel()
 
 	expectedName := "test_table"
@@ -71,4 +71,116 @@ func TestNew_handlesTimeout(t *testing.T) {
 		t.Error("generate did not return within timeout")
 		t.FailNow()
 	}
+}
+
+func TestCall_allowsConcurrentRequests(t *testing.T) {
+	t.Parallel()
+
+	expectedName := "test_table"
+	overrideTimeout := 3 * time.Second
+	expectedRow := map[string]string{
+		"somekey": "somevalue",
+	}
+	expectedRows := []map[string]string{
+		expectedRow,
+	}
+
+	w := New(multislogger.NewNopLogger(), expectedName, nil, func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+		time.Sleep(100 * time.Millisecond) // very short wait -- generate will not time out
+		return expectedRows, nil
+	}, WithGenerateTimeout(overrideTimeout))
+
+	// The table name must be set correctly
+	require.Equal(t, expectedName, w.Name())
+
+	// Concurrent requests to the table must succeed
+	resultChan := make(chan osquery.ExtensionResponse)
+	for i := 0; i < numWorkers; i += 1 {
+		go func() {
+			resultChan <- w.Call(context.TODO(), map[string]string{"action": "generate", "context": "{}"})
+		}()
+	}
+
+	// Make sure we get five successful responses
+	for i := 0; i < numWorkers; i += 1 {
+		select {
+		case resp := <-resultChan:
+			require.Equal(t, int32(0), resp.Status.Code, resp.Status.Message) // success
+			require.Equal(t, 1, len(resp.Response))
+			require.Equal(t, expectedRow, resp.Response[0])
+		case <-time.After(overrideTimeout + 1*time.Second):
+			t.Error("generate did not return within timeout")
+			t.FailNow()
+		}
+	}
+}
+
+func TestCall_limitsExcessiveConcurrentRequests(t *testing.T) {
+	t.Parallel()
+
+	expectedName := "test_table"
+	overrideTimeout := 4 * time.Second
+	expectedRow := map[string]string{
+		"somekey": "somevalue",
+	}
+	expectedRows := []map[string]string{
+		expectedRow,
+	}
+
+	w := New(multislogger.NewNopLogger(), expectedName, nil, func(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+		time.Sleep(overrideTimeout + 1*time.Second) // generate should always time out
+		return expectedRows, nil
+	}, WithGenerateTimeout(overrideTimeout))
+
+	// The table name must be set correctly
+	require.Equal(t, expectedName, w.Name())
+
+	// The first five requests to the table must should time out. Any subsequent requests
+	// that occur while the first five have not yet timed out should immediately return
+	// an error.
+	gotWorkerChan := make(chan osquery.ExtensionResponse, numWorkers)
+	for i := 0; i < numWorkers; i += 1 {
+		go func() {
+			gotWorkerChan <- w.Call(context.TODO(), map[string]string{"action": "generate", "context": "{}"})
+		}()
+		time.Sleep(100 * time.Millisecond) // small sleep to make it easier to enforce ordering of calls
+	}
+	noWorkersAvailableChan := make(chan osquery.ExtensionResponse, numWorkers)
+	for i := 0; i < numWorkers; i += 1 {
+		go func() {
+			noWorkersAvailableChan <- w.Call(context.TODO(), map[string]string{"action": "generate", "context": "{}"})
+		}()
+		time.Sleep(100 * time.Millisecond) // small sleep to make it easier to enforce ordering of calls
+	}
+
+	// The first five requests to the table should have timed out
+	for i := 0; i < numWorkers; i += 1 {
+		select {
+		case resp := <-gotWorkerChan:
+			require.Equal(t, int32(1), resp.Status.Code)                // failure
+			require.Contains(t, resp.Status.Message, "timed out after") // matches `querying %s timed out after %s (queried columns: %v)`
+		case <-time.After(overrideTimeout + 1*time.Second):
+			t.Error("generate did not return within timeout")
+			t.FailNow()
+		}
+	}
+
+	// The next few requests to the table should also not succeed because workers
+	// could not be acquired in time -- but we still expect them to return in a timely manner
+	for i := 0; i < numWorkers; i += 1 {
+		select {
+		case resp := <-noWorkersAvailableChan:
+			require.Equal(t, int32(1), resp.Status.Code)                     // failure
+			require.Contains(t, resp.Status.Message, "no workers available") // matches `no workers available (limit %d)`
+		case <-time.After(overrideTimeout + 1*time.Second):
+			t.Error("generate did not return within timeout")
+			t.FailNow()
+		}
+	}
+
+	// Sleep to ensure the workers are now all available -- make another request and see it hit the regular timeout
+	time.Sleep(overrideTimeout) // 1 second should be sufficient, but we sleep the full override timeout to avoid test flakiness
+	resp := w.Call(context.TODO(), map[string]string{"action": "generate", "context": "{}"})
+	require.Equal(t, int32(1), resp.Status.Code)                // failure
+	require.Contains(t, resp.Status.Message, "timed out after") // matches `querying %s timed out after %s (queried columns: %v)`
 }
