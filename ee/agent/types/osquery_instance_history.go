e7cab09030c9661128178eade36ffc709b55803c
zack olson <zack@kolide.co>
2025-02-11 11:56:55 -0500

    clean up new osquery instance history types interfaces and regen knapsack mocks

--- ee/agent/types/osquery_instance_history.go
+++ ee/agent/types/osquery_instance_history.go
@@ -7,26 +7,16 @@ type OsqueryInstanceTracker interface {
 	SetOsqueryHistory(osqHistory OsqueryHistorian)
 }
 
-// OsqueryHistory is the interface that our history.History implements, allowing various paths towards
+// OsqueryHistory is the interface that our history.History implements, allowing any paths towards
 // reading and writing various parts of our collected history. all instance history manipulation should go through here
 type OsqueryHistorian interface {
 	NewInstance(registrationId string, runId string) error
 	GetHistory() ([]map[string]string, error)
-	// LatestInstance() (*OsqueryInstanceStats, error)
 	LatestInstanceIDByRegistrationID(registrationId string) (string, error)
-	// LatestInstanceByRegistrationID(registrationId string)
 	LatestInstanceUptimeMinutes() (int64, error)
 	SetConnected(runID string, querier Querier) error
 	SetExited(runID string, exitError error) error
 }
-
-// TODO move these all to top level history operations, all updates set through there
-// OsqueryInstanceStats is the interface that our history.Instance implements, allowing callers to update
-// various states throughout the osquery instance management lifecycle
-// type OsqueryInstanceStats interface {
-// 	Connected(querier Querier) error
-// 	Exited(exitError error) error
-// }
 
 type Querier interface {
 	Query(query string) ([]map[string]string, error)
