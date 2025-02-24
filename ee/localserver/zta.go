b46e721f1f7dfd438c8c3a9626e417e30f2008e2
RebeccaMahany <rebeccamahany@gmail.com>
2025-02-24 10:35:02 -0500

    Add allowlisted origins

--- ee/localserver/zta.go
+++ ee/localserver/zta.go
@@ -13,7 +13,18 @@ var (
 
 	// allowlistedZtaOriginsLookup contains the complete list of origins that are permitted to access the /zta endpoint.
 	allowlistedZtaOriginsLookup = map[string]struct{}{
-		"https://app.kolide.com": {}, // TODO RM -- placeholder, awaiting actual values
+		// Release extension
+		"chrome-extension://gejiddohjgogedgjnonbofjigllpkmbf":         {},
+		"chrome-extension://khgocmkkpikpnmmkgmdnfckapcdkgfaf":         {},
+		"chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa":         {},
+		"chrome-extension://dppgmdbiimibapkepcbdbmkaabgiofem":         {},
+		"moz-extension://dfbae458-fb6f-4614-856e-094108a80852":        {},
+		"moz-extension://25fc87fa-4d31-4fee-b5c1-c32a7844c063":        {},
+		"moz-extension://d634138d-c276-4fc8-924b-40a0ea21d284":        {},
+		"safari-web-extension://7cba6ce0-bc55-43bd-b2a3-562b703ecbea": {},
+		// Development
+		"chrome-extension://hjlinigoblmkhjejkmbegnoaljkphmgo":  {},
+		"moz-extension://0a75d802-9aed-41e7-8daa-24c067386e82": {},
 	}
 )
 
