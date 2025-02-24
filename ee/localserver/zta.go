7eb33b1903ff9696f786965dd2129b627e0755a3
RebeccaMahany <rebeccamahany@gmail.com>
2025-02-24 12:30:00 -0500

    Add special handling for safari web extension origins

--- ee/localserver/zta.go
+++ ee/localserver/zta.go
@@ -14,20 +14,21 @@ var (
 	// allowlistedZtaOriginsLookup contains the complete list of origins that are permitted to access the /zta endpoint.
 	allowlistedZtaOriginsLookup = map[string]struct{}{
 		// Release extension
-		"chrome-extension://gejiddohjgogedgjnonbofjigllpkmbf":         {},
-		"chrome-extension://khgocmkkpikpnmmkgmdnfckapcdkgfaf":         {},
-		"chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa":         {},
-		"chrome-extension://dppgmdbiimibapkepcbdbmkaabgiofem":         {},
-		"moz-extension://dfbae458-fb6f-4614-856e-094108a80852":        {},
-		"moz-extension://25fc87fa-4d31-4fee-b5c1-c32a7844c063":        {},
-		"moz-extension://d634138d-c276-4fc8-924b-40a0ea21d284":        {},
-		"safari-web-extension://7cba6ce0-bc55-43bd-b2a3-562b703ecbea": {},
+		"chrome-extension://gejiddohjgogedgjnonbofjigllpkmbf":  {},
+		"chrome-extension://khgocmkkpikpnmmkgmdnfckapcdkgfaf":  {},
+		"chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa":  {},
+		"chrome-extension://dppgmdbiimibapkepcbdbmkaabgiofem":  {},
+		"moz-extension://dfbae458-fb6f-4614-856e-094108a80852": {},
+		"moz-extension://25fc87fa-4d31-4fee-b5c1-c32a7844c063": {},
+		"moz-extension://d634138d-c276-4fc8-924b-40a0ea21d284": {},
 		// Development and internal builds
 		"chrome-extension://hjlinigoblmkhjejkmbegnoaljkphmgo":  {},
 		"moz-extension://0a75d802-9aed-41e7-8daa-24c067386e82": {},
 		"chrome-extension://hiajhnnfoihkhlmfejoljaokdpgboiea":  {},
 		"chrome-extension://kioanpobaefjdloichnjebbdafiloboa":  {},
 	}
+
+	safariWebExtensionScheme = "safari-web-extension://"
 )
 
 func (ls *localServer) requestZtaInfoHandler() http.Handler {
@@ -43,9 +44,11 @@ func (ls *localServer) requestZtaInfoHandlerFunc(w http.ResponseWriter, r *http.
 		return
 	}
 
-	// Validate origin
+	// Validate origin. We cannot validate Safari against an allowlist because the UUID
+	// is generated randomly at each Safari startup, so for now, we also allow origins
+	// with scheme safari-web-extension.
 	requestOrigin := r.Header.Get("Origin")
-	if _, ok := allowlistedZtaOriginsLookup[requestOrigin]; !ok {
+	if _, ok := allowlistedZtaOriginsLookup[requestOrigin]; !ok && !strings.HasPrefix(requestOrigin, safariWebExtensionScheme) {
 		escapedOrigin := strings.ReplaceAll(strings.ReplaceAll(requestOrigin, "\n", ""), "\r", "") // remove any newlines
 		ls.slogger.Log(r.Context(), slog.LevelInfo,
 			"received zta request with origin not in allowlist",
