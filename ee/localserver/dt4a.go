3f3a65b473c770c63aec005f62538969f3d9c19a
RebeccaMahany <rebeccamahany@gmail.com>
2025-03-20 11:39:57 -0400

    Move origin check to middleware

--- ee/localserver/dt4a.go
+++ ee/localserver/dt4a.go
@@ -12,29 +12,9 @@ import (
 
 var (
 	localserverDt4aInfoKey = []byte("localserver_zta_info")
-
-	// allowlistedDt4aOriginsLookup contains the complete list of origins that are permitted to access the /dt4a endpoint.
-	allowlistedDt4aOriginsLookup = map[string]struct{}{
-		// Release extension
-		"chrome-extension://gejiddohjgogedgjnonbofjigllpkmbf":  {},
-		"chrome-extension://khgocmkkpikpnmmkgmdnfckapcdkgfaf":  {},
-		"chrome-extension://aeblfdkhhhdcdjpifhhbdiojplfjncoa":  {},
-		"chrome-extension://dppgmdbiimibapkepcbdbmkaabgiofem":  {},
-		"moz-extension://dfbae458-fb6f-4614-856e-094108a80852": {},
-		"moz-extension://25fc87fa-4d31-4fee-b5c1-c32a7844c063": {},
-		"moz-extension://d634138d-c276-4fc8-924b-40a0ea21d284": {},
-		// Development and internal builds
-		"chrome-extension://hjlinigoblmkhjejkmbegnoaljkphmgo":  {},
-		"moz-extension://0a75d802-9aed-41e7-8daa-24c067386e82": {},
-		"chrome-extension://hiajhnnfoihkhlmfejoljaokdpgboiea":  {},
-		"chrome-extension://kioanpobaefjdloichnjebbdafiloboa":  {},
-		"chrome-extension://bkpbhnjcbehoklfkljkkbbmipaphipgl":  {},
-	}
 )
 
 const (
-	safariWebExtensionScheme = "safari-web-extension://"
-
 	accelerateInterval = 5 * time.Second
 	accelerateDuration = 5 * time.Minute
 )
@@ -47,9 +27,9 @@ func (ls *localServer) requestDt4aInfoHandlerFunc(w http.ResponseWriter, r *http
 	r, span := traces.StartHttpRequestSpan(r, "path", r.URL.Path)
 	defer span.End()
 
-	// Validate origin. We expect to either have the origin set to an allowlisted value, or to be
-	// present but empty, or to be missing. We will not allow a request with a nonempty origin
-	// that is not in the allowlist.
+	// This check is superfluous with the check in the middleware -- but we still have one
+	// unauthenticated endpoint that points directly to this handler, so we're leaving the check
+	// in both places for now. We can remove it once /zta is removed.
 	requestOrigin := r.Header.Get("Origin")
 	if requestOrigin != "" {
 		if _, ok := allowlistedDt4aOriginsLookup[requestOrigin]; !ok && !strings.HasPrefix(requestOrigin, safariWebExtensionScheme) {
@@ -96,22 +76,6 @@ func (ls *localServer) requestDt4aAccelerationHandler() http.Handler {
 func (ls *localServer) requestDt4aAccelerationHandlerFunc(w http.ResponseWriter, r *http.Request) {
 	r, span := traces.StartHttpRequestSpan(r, "path", r.URL.Path)
 	defer span.End()
-
-	// Validate origin. We expect to either have the origin set to an allowlisted value, or to be
-	// present but empty, or to be missing. We will not allow a request with a nonempty origin
-	// that is not in the allowlist.
-	requestOrigin := r.Header.Get("Origin")
-	if requestOrigin != "" {
-		if _, ok := allowlistedDt4aOriginsLookup[requestOrigin]; !ok && !strings.HasPrefix(requestOrigin, safariWebExtensionScheme) {
-			escapedOrigin := strings.ReplaceAll(strings.ReplaceAll(requestOrigin, "\n", ""), "\r", "") // remove any newlines
-			ls.slogger.Log(r.Context(), slog.LevelInfo,
-				"received dt4a request with origin not in allowlist",
-				"req_origin", escapedOrigin,
-			)
-			w.WriteHeader(http.StatusForbidden)
-			return
-		}
-	}
 
 	ls.accelerate(r.Context())
 
