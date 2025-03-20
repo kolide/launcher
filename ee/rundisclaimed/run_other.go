bb22d0a43478bb23e8861d8a856b039dc8044b24
zack olson <zack@kolide.co>
2025-03-20 16:11:43 -0400

    c code working with piping stdout + stderr

---
+++ ee/rundisclaimed/run_other.go
@@ -0,0 +1,14 @@
+//go:build !darwin
+// +build !darwin
+
+package rundisclaimed
+
+import (
+	"errors"
+
+	"github.com/kolide/launcher/pkg/log/multislogger"
+)
+
+func Run(_ *multislogger.MultiSlogger, args []string) error {
+	return errors.New("run disclaimed is only implemented for macOS")
+}
