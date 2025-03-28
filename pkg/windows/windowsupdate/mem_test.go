94da7196d5c8c5504e3e709a8bb3ca7b2f458baf
RebeccaMahany <rebeccamahany@gmail.com>
2025-03-28 09:30:08 -0400

    New test for windowsupdate

---
+++ pkg/windows/windowsupdate/mem_test.go
@@ -0,0 +1,61 @@
+package windowsupdate
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/scjalliance/comshim"
+	"github.com/shirou/gopsutil/v3/process"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMemory(t *testing.T) {
+	fmt.Println("starting")
+	takeAndPrintMemoryUsageSnapshot(t)
+
+	comshim.Add(1)
+	defer comshim.Done()
+
+	session, err := NewUpdateSession()
+	require.NoError(t, err)
+
+	searcher, err := session.CreateUpdateSearcher()
+	require.NoError(t, err)
+
+	_, err = searcher.Search("Type='Software'")
+	require.NoError(t, err)
+
+	fmt.Println("done")
+	takeAndPrintMemoryUsageSnapshot(t)
+}
+
+func takeAndPrintMemoryUsageSnapshot(t *testing.T) {
+	var stats runtime.MemStats
+	runtime.ReadMemStats(&stats)
+
+	currentGoMemory := goMemoryUsage(&stats)
+	currentNonGoMemory := nonGoMemoryUsage(t, &stats)
+
+	fmt.Printf("Go memory: %d\tNon-go memory: %d\n", currentGoMemory, currentNonGoMemory)
+}
+
+// Non-Go memory usage (e.g. cgo) can maybe be estimated as Process RSS - Go memory
+func nonGoMemoryUsage(t *testing.T, m *runtime.MemStats) uint64 {
+	currentPid := os.Getpid()
+	currentProcess, err := process.NewProcess(int32(currentPid))
+	require.NoError(t, err)
+	memInfo, err := currentProcess.MemoryInfo()
+	require.NoError(t, err)
+	return memInfo.RSS - goMemoryUsage(m)
+}
+
+/*
+[T]he following expression accurately reflects the value the runtime attempts to maintain as the limit:
+
+runtime.MemStats.Sys − runtime.MemStats.HeapReleased
+*/
+func goMemoryUsage(m *runtime.MemStats) uint64 {
+	return m.Sys - m.HeapReleased
+}
