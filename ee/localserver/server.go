e27eaafd9e0c639fac4483dfc0029f9e73b63abe
James Pickett <jamesandrewpickett@gmail.com>
2025-04-15 10:19:00 -0700

    add dt4a registration plumbing, stub out middleware

--- ee/localserver/server.go
+++ ee/localserver/server.go
@@ -124,7 +124,12 @@ func New(ctx context.Context, k types.Knapsack, presenceDetector presenceDetecto
 	// TODO: make this authenticated or remove
 	mux.Handle("/zta", ls.requestDt4aInfoHandler())
 
-	mux.Handle("/v3/dt4a", dt4aAuthMiddleware.Wrap(ls.requestDt4aInfoHandler()))
+	mux.Handle("/v3/dt4a", dt4aAuthMiddleware.Wrap(
+		ls.dt4aRegistrationMiddleware(
+			ls.requestDt4aInfoHandler(),
+		)),
+	)
+
 	mux.Handle("/v3/accelerate", dt4aAuthMiddleware.Wrap(ls.requestDt4aAccelerationHandler()))
 
 	// uncomment to test without going through middleware
