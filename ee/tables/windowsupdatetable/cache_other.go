0fc4cb55f9d33299a0c2d746ed9c8aa07a328050
RebeccaMahany <rebeccamahany@gmail.com>
2025-04-28 10:56:53 -0400

    Add actor to periodically cache Windows updates data

---
+++ ee/tables/windowsupdatetable/cache_other.go
@@ -0,0 +1,39 @@
+//go:build !windows
+// +build !windows
+
+package windowsupdatetable
+
+import (
+	"log/slog"
+	"sync/atomic"
+	"time"
+
+	"github.com/kolide/launcher/ee/agent/types"
+)
+
+type noOpWindowsUpdatesCacher struct {
+	interrupt   chan struct{}
+	interrupted atomic.Bool
+}
+
+func NewWindowsUpdatesCacher(_ types.GetterSetter, _ time.Duration, _ *slog.Logger) *noOpWindowsUpdatesCacher {
+	return &noOpWindowsUpdatesCacher{
+		interrupt: make(chan struct{}),
+	}
+}
+
+func (n *noOpWindowsUpdatesCacher) Execute() error {
+	<-n.interrupt
+	return nil
+}
+
+func (n *noOpWindowsUpdatesCacher) Interrupt(_ error) {
+	// Only perform shutdown tasks on first call to interrupt -- no need to repeat on potential extra calls.
+	if n.interrupted.Load() {
+		return
+	}
+
+	n.interrupted.Store(true)
+
+	n.interrupt <- struct{}{}
+}
