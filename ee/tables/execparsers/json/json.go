e1cdaf955a6cc07617e3cbefcfb296b210822a70
Cesar Araujo <cesar.araujo@agilebits.com>
2025-05-23 12:11:08 -0500

    Add JSON parser and test data for Linux and Darwin platforms

--- ee/tables/execparsers/json/json.go
+++ ee/tables/execparsers/json/json.go
@@ -1,17 +1,14 @@
 //go:build darwin || linux
 // +build darwin linux
 
-package mapxml
+package json
 
 import (
-	"bufio"
 	"io"
 )
 
 // parser implements the parser interface for XML data from nix-env
 type parser struct {
-	scanner      *bufio.Scanner
-	lastReadLine string
 }
 
 // Parser is the singleton instance of the parser
@@ -25,5 +22,5 @@ func New() *parser {
 // Parse implements the parser interface
 // It takes a reader containing XML data from nix-env and returns a structured representation
 func (p *parser) Parse(reader io.Reader) (any, error) {
-	return p.parseXml(reader)
+	return p.parseJson(reader)
 }
