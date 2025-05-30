ee7a6bf755c626e6074fc88019bd47e54bd39ee1
Cesar Araujo <cesar.araujo@agilebits.com>
2025-05-30 16:18:03 -0500

    removed singleton to avoid sideaffects

--- ee/tables/execparsers/key_value/key_value.go
+++ ee/tables/execparsers/key_value/key_value.go
@@ -4,35 +4,26 @@ import (
 	"io"
 )
 
-// parser implements the parser interface for key-value data
 type parser struct {
 	kvDelimiter string
 }
 
-// Parser is the singleton instance of the parser
-var Parser = New()
-
-// New creates a new parser instance with default delimiter
 func New() *parser {
 	return &parser{
 		kvDelimiter: "=", // default delimiter
 	}
 }
 
-// NewWithDelimiter creates a new parser instance with a custom delimiter
 func NewWithDelimiter(delimiter string) *parser {
 	return &parser{
 		kvDelimiter: delimiter,
 	}
 }
 
-// SetDelimiter sets the key-value delimiter for the parser
 func (p *parser) SetDelimiter(delimiter string) {
 	p.kvDelimiter = delimiter
 }
 
-// Parse implements the parser interface
-// It takes a reader containing key-value data and returns a structured representation
 func (p *parser) Parse(reader io.Reader) (any, error) {
 	return p.parseKeyValue(reader)
 }
