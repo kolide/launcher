8b3c4a13a99612687b7085c25583a4b315d5a60c
Micah-Kolide <micah@kolide.co>
2025-06-12 10:58:00 -0700

    Add new tables

--- ee/disclaim/run_disclaimed_darwin.go
+++ ee/disclaim/run_disclaimed_darwin.go
@@ -98,6 +98,13 @@ var allowedCmdGenerators = map[string]allowedCmdGenerator{
 		},
 		generate: allowedcmd.Zscli,
 	},
+	"microsoft_defender_atp": {
+		allowedOpts: map[string]struct{}{
+			"health":        {},
+			"--output json": {},
+		},
+		generate: allowedcmd.MicrosoftDefenderATP,
+	},
 }
 
 func RunDisclaimed(_ *multislogger.MultiSlogger, args []string) error {
