07d677fc5949e7304e7cd3406c7494b88bed12af
RebeccaMahany <rebeccamahany@gmail.com>
2025-07-10 11:43:36 -0400

    Remove flaky check from Test_checkPerformance while we're here

--- ee/debug/checkups/performance_test.go
+++ ee/debug/checkups/performance_test.go
@@ -18,10 +18,10 @@ func Test_checkPerformance(t *testing.T) {
 	stats := perf.data["stats"]
 	perfStats, ok := stats.(*performance.PerformanceStats)
 	require.True(t, ok, "expected stats to be PerformanceStats")
+	// Note that we don't check CPU because it is often 0 in CI, resulting in a flaky test
 	require.NotEmpty(t, perfStats.Exe, "expected exe to be set on performance stats")
 	require.Greater(t, perfStats.Pid, 0, "expected pid to be set")
 	require.NotNil(t, perfStats.MemInfo, "expected MemInfo to be set")
-	require.Greater(t, perfStats.CPUPercent, float64(0.0), "expected cpu percentage to be set")
 	require.Greater(t, perfStats.MemInfo.RSS, uint64(0), "expected RSS to be set")
 	require.Greater(t, perfStats.MemInfo.VMS, uint64(0), "expected VMS to be set")
 	require.Greater(t, perfStats.MemInfo.MemPercent, float32(0.0), "expected mem percent to be set")
