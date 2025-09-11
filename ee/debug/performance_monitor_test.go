0148f551e5247ac8029a18a4ad910c88d9a3f869
RebeccaMahany <rebeccamahany@gmail.com>
2025-09-11 11:00:50 -0400

    Add tests

--- ee/debug/performance_monitor_test.go
+++ ee/debug/performance_monitor_test.go
@@ -1,12 +1,15 @@
 package debug
 
 import (
+	"context"
 	"errors"
 	"log/slog"
 	"testing"
 	"time"
 
 	typesmocks "github.com/kolide/launcher/ee/agent/types/mocks"
+	"github.com/kolide/launcher/ee/performance"
+	"github.com/kolide/launcher/pkg/log/multislogger"
 	"github.com/kolide/launcher/pkg/threadsafebuffer"
 	"github.com/stretchr/testify/require"
 )
@@ -56,4 +59,115 @@ func TestInterrupt_Multiple(t *testing.T) {
 	}
 
 	require.Equal(t, expectedInterrupts, receivedInterrupts)
+}
+
+func Test_checkPerformance_notEnabled(t *testing.T) {
+	t.Parallel()
+
+	testKnapsack := typesmocks.NewKnapsack(t)
+	testKnapsack.On("Slogger").Return(multislogger.NewNopLogger())
+	testKnapsack.On("PerformanceMonitoringEnabled").Return(false)
+
+	p := NewPerformanceMonitor(testKnapsack)
+
+	p.checkPerformance()
+
+	// Expect that we did not trigger a flare
+	require.Equal(t, int64(0), p.lastFlareSent.Load())
+}
+
+func Test_shouldTriggerFlareUpload(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range []struct {
+		testCaseName      string
+		cpuPercent        float64
+		goMemUsage        uint64
+		lastSentTimestamp int64
+		expectFlareUpload bool
+		expectedReason    string
+	}{
+		{
+			testCaseName:      "CPU too high, flare not triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare + 5,
+			goMemUsage:        golangMemUsageThresholdForFlare - 800,
+			lastSentTimestamp: time.Now().Unix() - minimumFlareResendIntervalSeconds - 5000,
+			expectFlareUpload: true,
+			expectedReason:    "high CPU usage",
+		},
+		{
+			testCaseName:      "memory too high, flare not triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare - 3,
+			goMemUsage:        golangMemUsageThresholdForFlare + 1500,
+			lastSentTimestamp: time.Now().Unix() - minimumFlareResendIntervalSeconds - 1000,
+			expectFlareUpload: true,
+			expectedReason:    "high Golang memory usage",
+		},
+		{
+			testCaseName:      "CPU and memory too high, flare not triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare + 4,
+			goMemUsage:        golangMemUsageThresholdForFlare + 6000,
+			lastSentTimestamp: time.Now().Unix() - minimumFlareResendIntervalSeconds - 600,
+			expectFlareUpload: true,
+			expectedReason:    "high Golang memory and CPU usage",
+		},
+		{
+			testCaseName:      "CPU and memory within allowable range, flare not triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare - 2,
+			goMemUsage:        golangMemUsageThresholdForFlare - 4000,
+			lastSentTimestamp: time.Now().Unix() - minimumFlareResendIntervalSeconds - 300,
+			expectFlareUpload: false,
+		},
+		{
+			testCaseName:      "CPU too high, flare triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare + 6,
+			goMemUsage:        golangMemUsageThresholdForFlare - 1000,
+			lastSentTimestamp: time.Now().Unix() - (minimumFlareResendIntervalSeconds - 5000),
+			expectFlareUpload: false,
+		},
+		{
+			testCaseName:      "memory too high, flare triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare - 1,
+			goMemUsage:        golangMemUsageThresholdForFlare + 2000,
+			lastSentTimestamp: time.Now().Unix() - (minimumFlareResendIntervalSeconds - 1000),
+			expectFlareUpload: false,
+		},
+		{
+			testCaseName:      "CPU and memory too high, flare triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare + 7,
+			goMemUsage:        golangMemUsageThresholdForFlare + 3000,
+			lastSentTimestamp: time.Now().Unix() - (minimumFlareResendIntervalSeconds - 600),
+			expectFlareUpload: false,
+		},
+		{
+			testCaseName:      "CPU and memory within allowable range, flare triggered recently",
+			cpuPercent:        cpuUsageThresholdForFlare - 2,
+			goMemUsage:        golangMemUsageThresholdForFlare - 900,
+			lastSentTimestamp: time.Now().Unix() - (minimumFlareResendIntervalSeconds - 300),
+			expectFlareUpload: false,
+		},
+	} {
+		tt := tt
+		t.Run(tt.testCaseName, func(t *testing.T) {
+			t.Parallel()
+
+			testKnapsack := typesmocks.NewKnapsack(t)
+			testKnapsack.On("Slogger").Return(multislogger.NewNopLogger())
+
+			p := NewPerformanceMonitor(testKnapsack)
+			p.lastFlareSent.Store(tt.lastSentTimestamp)
+
+			testStats := &performance.PerformanceStats{
+				CPUPercent: tt.cpuPercent,
+				MemInfo: &performance.MemInfo{
+					GoMemUsage: tt.goMemUsage,
+				},
+			}
+
+			performUpload, uploadReason := p.shouldTriggerFlareUpload(context.TODO(), testStats)
+
+			require.Equal(t, tt.expectFlareUpload, performUpload)
+			require.Equal(t, tt.expectedReason, uploadReason)
+		})
+	}
 }
