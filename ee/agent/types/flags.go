c74f00c46d57783b5026e3dbeb9626a323cf2774
zack olson <zack@kolide.co>
2025-10-16 14:20:45 -0400

    better flag key name

--- ee/agent/types/flags.go
+++ ee/agent/types/flags.go
@@ -242,9 +242,8 @@ type Flags interface {
 	// LocalDevelopmentPath points to a local build of launcher to use instead of the one selected from the autoupdate library
 	LocalDevelopmentPath() string
 
-	// LauncherWatchdogEnabled controls whether launcher installs/runs, or stops/removes the launcher watchdog service
+	// LauncherWatchdogEnabled controls whether launcher installs/runs, or stops/removes the launcher watchdog service.
 	// note that this was a temporary flag and will be removed in favor of LauncherWatchdogDisabled
-	// during the transition, watchdog will be installed only if enabled is true and disabled is false.
 	// when we fully cut over, installation behavior will be based purely on the disabled flag
 	SetLauncherWatchdogEnabled(enabled bool) error
 	LauncherWatchdogEnabled() bool
