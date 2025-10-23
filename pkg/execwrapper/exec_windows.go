fd8352ddc9a071f57feb0535a7025bca9584f743
RebeccaMahany <rebeccamahany@gmail.com>
2025-10-23 11:06:10 -0400

    Improve execwrapper logging + log to system logs too

--- pkg/execwrapper/exec_windows.go
+++ pkg/execwrapper/exec_windows.go
@@ -6,18 +6,13 @@ package execwrapper
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
-	"github.com/kolide/launcher/pkg/contexts/ctxlog"
 )
 
-func Exec(ctx context.Context, argv0 string, argv []string, envv []string) error {
-	logger := log.With(ctxlog.FromContext(ctx), "caller", log.DefaultCaller)
-
+func Exec(ctx context.Context, slogger *slog.Logger, argv0 string, argv []string, envv []string) error {
 	cmd := exec.CommandContext(ctx, argv0, argv[1:]...) //nolint:forbidigo // execwrapper is used exclusively to exec launcher, and we trust the autoupdate library to find the correct path.
 	cmd.Env = envv
 
@@ -25,26 +20,24 @@ func Exec(ctx context.Context, argv0 string, argv []string, envv []string) error
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	level.Debug(logger).Log(
-		"msg", "preparing to run command",
-		"cmd", strings.Join(cmd.Args, " "),
+	fullCmd := strings.Join(cmd.Args, " ")
+	slogger = slogger.With("cmd", fullCmd, "component", "execwrapper")
+	slogger.Log(ctx, slog.LevelInfo,
+		"preparing to run command",
 	)
 
 	// Now run it. This is faking exec, we need to distinguish
-	// between a failure to execute, and a failure in in the called program.
+	// between a failure to execute, and a failure in the called program.
 	// I think https://github.com/golang/go/issues/26539 adds this functionality.
 	err := cmd.Run()
 
 	if cmd.ProcessState.ExitCode() == -1 {
-		if err == nil {
-			return fmt.Errorf("Unknown error trying to exec %s (and nil err)", strings.Join(cmd.Args, " "))
-		}
-		return fmt.Errorf("Unknown error trying to exec %s: %w", strings.Join(cmd.Args, " "), err)
+		return fmt.Errorf("execing %s returned exit code -1 and state %s: %w", fullCmd, cmd.ProcessState.String(), err)
 	}
 
 	if err != nil {
-		level.Info(logger).Log(
-			"msg", "got error on exec",
+		slogger.Log(ctx, slog.LevelError,
+			"got error on exec",
 			"err", err,
 		)
 	}
