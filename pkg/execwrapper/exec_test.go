9923a51df2ceaefafdb180a160405eab9d144b4a
James Pickett <james@kolide.co>
2025-10-27 14:21:12 -0700

    find echo for specific platforms

--- pkg/execwrapper/exec_test.go
+++ pkg/execwrapper/exec_test.go
@@ -18,9 +18,20 @@ func TestExec(t *testing.T) {
 		Level: slog.LevelDebug,
 	}))
 
+	var command string
+	var args []string
+
+	if runtime.GOOS == "windows" {
+		command = "cmd.exe"
+		args = []string{"/c", "echo", "test string"}
+	} else {
+		command = "/bin/sh"
+		args = []string{"-c", "echo test string"}
+	}
+
 	// Exec expects the process to continue running (because it expects to be running launcher),
 	// so any exit that is not a subcommand or the windows "svc" subcommand, will be an error. Therefore, we expect an error here.
-	err := Exec(t.Context(), slogger, "echo", []string{"test string"}, os.Environ(), false)
+	err := Exec(t.Context(), slogger, command, args, os.Environ(), false)
 	require.Error(t, err)
 
 	// We should expect at least SOMETHING to be logged on Windows
@@ -37,13 +48,22 @@ func TestExecSubcommand(t *testing.T) {
 		Level: slog.LevelDebug,
 	}))
 
+	var command string
+	var args []string
+
+	if runtime.GOOS == "windows" {
+		command = "cmd.exe"
+		args = []string{"/c", "echo", "test string"}
+	} else {
+		command = "/bin/sh"
+		args = []string{"-c", "echo test string"}
+	}
+
 	// Exec expects the process to continue running (because it expects to be running launcher),
 	// so any exit that is not a subcommand or the windows "svc" subcommand, will be an error. Therefore, we expect an error here.
-	err := Exec(t.Context(), slogger, "echo", []string{"test string"}, os.Environ(), true)
+	err := Exec(t.Context(), slogger, command, args, os.Environ(), true)
 	require.NoError(t, err)
 
-	// We should expect at least SOMETHING to be logged on Windows
-	if runtime.GOOS == "windows" {
-		require.Greater(t, len(logBytes.String()), 0)
-	}
+	// We should expect at least SOMETHING to be logged
+	require.Greater(t, len(logBytes.String()), 0)
 }
