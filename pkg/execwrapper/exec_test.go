f49cf1257c14dd308231f764dc68485a6f09713f
James Pickett <jamesandrewpickett@gmail.com>
2025-10-28 13:22:21 -0700

    more test improvements

--- pkg/execwrapper/exec_test.go
+++ pkg/execwrapper/exec_test.go
@@ -18,30 +18,30 @@ func TestExec(t *testing.T) {
 		Level: slog.LevelDebug,
 	}))
 
-	var command string
-	var args []string
+	// by not setting command and args on non-windows, we will trigger an error in syscall.Exec
+	// otherwise we'll never return from Exec on non-windows becuase syscall.Exec replaces the current process
+	command := ""
+	args := []string{}
 
+	// on windows we want to actually run something that will exit cleanly
 	if runtime.GOOS == "windows" {
 		command = "cmd.exe"
 		// Exec removes the first argument, so add a blank to make sure our args get processed
 		args = []string{"", "/c", "echo", "test string"}
-	} else {
-		// by not setting command and args on non-windows, we will trigger an error in syscall.Exec
-		command = ""
-		args = []string{}
 	}
 
-	// Exec expects the process to continue running (because it expects to be running launcher),
-	// so any exit that is not a subcommand or the windows "svc" subcommand, will be an error. Therefore, we expect an error here.
+	// non-windows will give us an error here because syscall.Exec will fail
+	// echo on windows will exit cleanly, but we expect an error because we set isNonSvcSubCommand to false
 	err := Exec(t.Context(), slogger, command, args, os.Environ(), false)
 	require.Error(t, err)
 
-	// your eyes do not decive you, we expect an exit status 0 in the logs even though Exec returned an error
-	// this is because Exec is used by the main launcher process to run new versions of launcher, if the newer version
-	// exits for ANY reason we want that treated as an error so the main process will exit with an error so that
-	// windows service manager knows to restart it
-	require.Contains(t, logBytes.String(), "exit status 0")
-
+	if runtime.GOOS == "windows" {
+		// your eyes do not decive you, we expect an exit status 0 in the logs even though Exec returned an error
+		// this is because Exec is used by the main launcher process to run new versions of launcher, if the newer version
+		// exits for ANY reason we want that treated as an error so the main process will exit with an error so that
+		// windows service manager knows to restart it
+		require.Contains(t, logBytes.String(), "exit status 0")
+	}
 }
 
 func TestExecSubcommand(t *testing.T) {
@@ -52,16 +52,13 @@ func TestExecSubcommand(t *testing.T) {
 		Level: slog.LevelDebug,
 	}))
 
-	var command string
-	var args []string
+	command := "/bin/echo"
+	args := []string{"echo", "test string"}
 
 	if runtime.GOOS == "windows" {
 		command = "cmd.exe"
 		// Exec removes the first argument, so add a blank to make sure our args get processed
 		args = []string{"", "/c", "echo", "test string"}
-	} else {
-		command = "/bin/echo"
-		args = []string{"echo", "test string"}
 	}
 
 	// flag this call as a non-svc subcommand so if it exist with out error, we do not return an error
