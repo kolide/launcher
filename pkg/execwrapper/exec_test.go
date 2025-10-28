1653d82164f55e344d46dcb841aec2dacd47c376
James Pickett <james@kolide.co>
2025-10-28 10:50:08 -0700

    better tests and comments

--- pkg/execwrapper/exec_test.go
+++ pkg/execwrapper/exec_test.go
@@ -29,6 +29,9 @@ func TestExec(t *testing.T) {
 		args = []string{"-c", "echo test string"}
 	}
 
+	// Exec removes the first argument, so add a blank to make sure our args get processed
+	args = append([]string{""}, args...)
+
 	// Exec expects the process to continue running (because it expects to be running launcher),
 	// so any exit that is not a subcommand or the windows "svc" subcommand, will be an error. Therefore, we expect an error here.
 	err := Exec(t.Context(), slogger, command, args, os.Environ(), false)
@@ -37,6 +40,12 @@ func TestExec(t *testing.T) {
 	// We should expect at least SOMETHING to be logged on Windows
 	if runtime.GOOS == "windows" {
 		require.Greater(t, len(logBytes.String()), 0)
+
+		// your eyes do not decive you, we expect an exit status 0 in the logs even though Exec returned an error
+		// this is because Exec is used by the main launcher process to run new versions of launcher, if the newer version
+		// exits for ANY reason we want that treated as an error so the main process will exit with an error so that
+		// windows service manager knows to restart it
+		require.Contains(t, logBytes.String(), "exit status 0")
 	}
 }
 
@@ -59,11 +68,16 @@ func TestExecSubcommand(t *testing.T) {
 		args = []string{"-c", "echo test string"}
 	}
 
-	// Exec expects the process to continue running (because it expects to be running launcher),
-	// so any exit that is not a subcommand or the windows "svc" subcommand, will be an error. Therefore, we expect an error here.
+	// Exec removes the first argument, so add a blank to make sure our args get processed
+	args = append([]string{""}, args...)
+
+	// flag this call as a non-svc subcommand so if it exist with out error, we do not return an error
 	err := Exec(t.Context(), slogger, command, args, os.Environ(), true)
 	require.NoError(t, err)
 
-	// We should expect at least SOMETHING to be logged
-	require.Greater(t, len(logBytes.String()), 0)
+	// We should expect at least SOMETHING to be logged on Windows
+	if runtime.GOOS == "windows" {
+		require.Greater(t, len(logBytes.String()), 0)
+		require.Contains(t, logBytes.String(), "exit status 0")
+	}
 }
