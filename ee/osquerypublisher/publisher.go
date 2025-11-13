7479538265b328b50ea1ac12d844b6713aea2e86
zack olson <zack@kolide.co>
2025-11-13 11:34:56 -0500

    PR feedback: add request and response structs to types file and doc goals

--- ee/osquerypublisher/publisher.go
+++ ee/osquerypublisher/publisher.go
@@ -20,25 +20,6 @@ type (
 	PublisherHTTPClient interface {
 		Do(req *http.Request) (*http.Response, error)
 	}
-
-	// PublishLogsRequest represents the expected JSON structure for log data. We will likely add some
-	// identifying metadata here once we have a better idea of the authentication mechanism (may just be the node key
-	// as it current is in our existing request structure, but there is currently no reason to send it).
-	// This is exported for use by agent-ingester
-	PublishLogsRequest struct {
-		LogType osqlog.LogType `json:"log_type"`
-		Logs    []string       `json:"logs"`
-	}
-
-	// PublishLogsResponse represents the response structure from agent-ingester for a given batch of log publications.
-	// Note that this can also be used to unmarshal and errorResponse from agent-ingester, in that case
-	// only the status and message fields will be populated. This is exported here for use by agent-ingester.
-	PublishLogsResponse struct {
-		Status        string `json:"status"`
-		IngestedBytes int64  `json:"ingested_bytes"`
-		LogCount      int    `json:"log_count"`
-		Message       string `json:"message"`
-	}
 )
 
 // levelForError returns slog.LevelWarn if err != nil, else slog.LevelDebug
