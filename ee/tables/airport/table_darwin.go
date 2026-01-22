2d2f00fcaa848982545896cf6e8837219fe5753e
RebeccaMahany <rebeccamahany@gmail.com>
2026-01-22 12:12:39 -0500

    executor

--- ee/tables/airport/table_darwin.go
+++ ee/tables/airport/table_darwin.go
@@ -54,6 +54,11 @@ func (a *airportExecutor) Exec(option string) ([]byte, error) {
 	return tablehelpers.RunSimple(a.ctx, a.slogger, 30, allowedcmd.Airport, []string{"--" + option})
 }
 
+//mockery:generate: true
+//mockery:dir: ee/tables/airport/mocks
+//mockery:filename: executor.go
+//mockery:pkgname: mocks
+//mockery:structname: Executor
 type executor interface {
 	Exec(string) ([]byte, error)
 }
