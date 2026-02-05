68b97790dd1f9da5b3afcf35bcf7c0617d1ee297
RebeccaMahany <rebeccamahany@gmail.com>
2026-02-05 17:01:57 -0500

    Init filewalker actor

--- ee/filewalker/filewalker.go
+++ ee/filewalker/filewalker.go
@@ -4,38 +4,94 @@ import (
 	"context"
 	"fmt"
 	"io/fs"
+	"log/slog"
 	"path/filepath"
+	"regexp"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/charlievieth/fastwalk"
+	"github.com/kolide/launcher/ee/agent/types"
 )
 
-func Filewalk(ctx context.Context, startDir string) ([]string, error) {
-	fileNames := make([]string, 0)
-	if err := filepath.WalkDir(startDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			// Ignore permissions errors
-			if d != nil && d.IsDir() {
-				return fs.SkipDir
-			}
-			return err
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		fileNames = append(fileNames, path)
-
-		return nil
-	}); err != nil {
-		return nil, fmt.Errorf("walking %s: %w", startDir, err)
-	}
-
-	return fileNames, nil
+type filewalkConfig struct {
+	rootDir       string
+	fileNameRegex *regexp.Regexp
+	// fileType      fs.FileMode
 }
 
-func FastwalkWithChannel(ctx context.Context, startDir string) ([]string, error) {
+type Filewalker struct {
+	walkInterval  time.Duration
+	walkTicker    *time.Ticker
+	configs       map[string]filewalkConfig // map table name to config
+	filewalksLock *sync.Mutex
+	filewalks     map[string][]string // map table name to filepaths
+
+	// Internals
+	k       types.Knapsack
+	slogger *slog.Logger
+
+	// Handle actor shutdown
+	interrupt   chan struct{}
+	interrupted *atomic.Bool
+}
+
+func NewFilewalker(k types.Knapsack, slogger *slog.Logger, walkInterval time.Duration) *Filewalker {
+	return &Filewalker{
+		walkInterval:  walkInterval,
+		filewalksLock: &sync.Mutex{},
+		k:             k,
+		slogger:       slogger.With("component", "filewalker"),
+		interrupt:     make(chan struct{}, 10), // We have a buffer so we don't block on sending to this channel
+		interrupted:   &atomic.Bool{},
+	}
+}
+
+func (f *Filewalker) Execute() error {
+	f.walkTicker = time.NewTicker(f.walkInterval)
+	defer f.walkTicker.Stop()
+
+	for {
+		for tableName, config := range f.configs {
+			// fastwalk already has an optimized number of workers, so we want to perform filewalks
+			// for each table in sequence, rather than in parallel.
+			results, err := f.filewalk(context.TODO(), config.rootDir, config.fileNameRegex)
+			if err != nil {
+				f.slogger.Log(context.TODO(), slog.LevelError,
+					"could not filewalk",
+					"table_name", tableName,
+					"err", err,
+				)
+				continue
+			}
+			f.filewalksLock.Lock()
+			f.filewalks[tableName] = results
+			f.filewalksLock.Unlock()
+		}
+
+		select {
+		case <-f.interrupt:
+			f.slogger.Log(context.TODO(), slog.LevelDebug,
+				"received external interrupt, stopping",
+			)
+			return nil
+		case <-f.walkTicker.C:
+			continue
+		}
+	}
+}
+
+func (f *Filewalker) Interrupt(_ error) {
+	// Only perform shutdown tasks on first call to interrupt -- no need to repeat on potential extra calls.
+	if f.interrupted.Swap(true) {
+		return
+	}
+
+	f.interrupt <- struct{}{}
+}
+
+func (f *Filewalker) filewalk(ctx context.Context, startDir string, fileNameRegex *regexp.Regexp) ([]string, error) {
 	var wg sync.WaitGroup
 	fileNames := make([]string, 0)
 	filenamesChan := make(chan string, 1000)
@@ -49,17 +105,23 @@ func FastwalkWithChannel(ctx context.Context, startDir string) ([]string, error)
 		}
 	})
 
-	if err := fastwalk.Walk(&fastwalk.Config{
-		Follow: false, // don't follow symlinks
-	}, startDir, func(path string, d fs.DirEntry, err error) error {
+	if err := fastwalk.Walk(&fastwalk.DefaultConfig, startDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
+			f.slogger.Log(ctx, slog.LevelWarn,
+				"error while filewalking",
+				"start_dir", startDir,
+				"path", path,
+				"err", err,
+			)
 			return nil
 		}
 		if d.IsDir() {
 			return nil
 		}
 
-		filenamesChan <- path
+		if fileNameRegex == nil || fileNameRegex.MatchString(filepath.Base(path)) {
+			filenamesChan <- path
+		}
 
 		return nil
 	}); err != nil {
@@ -68,32 +130,6 @@ func FastwalkWithChannel(ctx context.Context, startDir string) ([]string, error)
 
 	close(filenamesChan)
 	wg.Wait()
-
-	return fileNames, nil
-}
-
-func FastwalkWithLock(ctx context.Context, startDir string) ([]string, error) {
-	fileNames := make([]string, 0)
-	fileNamesLock := &sync.Mutex{}
-
-	if err := fastwalk.Walk(&fastwalk.Config{
-		Follow: false, // don't follow symlinks
-	}, startDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return nil
-		}
-		if d.IsDir() {
-			return nil
-		}
-
-		fileNamesLock.Lock()
-		fileNames = append(fileNames, path)
-		fileNamesLock.Unlock()
-
-		return nil
-	}); err != nil {
-		return nil, fmt.Errorf("walking %s: %w", startDir, err)
-	}
 
 	return fileNames, nil
 }
--- ee/filewalker/filewalker_test.go
+++ ee/filewalker/filewalker_test.go
@@ -3,43 +3,29 @@ package filewalker
 import (
 	"runtime"
 	"testing"
+	"time"
 
+	typesmocks "github.com/kolide/launcher/ee/agent/types/mocks"
+	"github.com/kolide/launcher/pkg/log/multislogger"
 	"github.com/stretchr/testify/require"
 )
 
-func testDir() string {
+func BenchmarkFilewalk(b *testing.B) {
+	testFilewalker := NewFilewalker(typesmocks.NewKnapsack(b), multislogger.NewNopLogger(), 1*time.Minute)
+
+	var testDir string
 	switch runtime.GOOS {
 	case "windows":
-		return `D:\a\`
+		testDir = `D:\a\`
 	case "darwin":
-		return "/Users/"
+		testDir = "/Users/"
 	default:
-		return "/home/"
+		testDir = "/home/"
 	}
-}
 
-func BenchmarkFilewalk(b *testing.B) {
 	b.ReportAllocs()
 	for b.Loop() {
-		results, err := Filewalk(b.Context(), testDir())
-		require.NoError(b, err)
-		require.LessOrEqual(b, 100, len(results))
-	}
-}
-
-func BenchmarkFastwalkWithChannel(b *testing.B) {
-	b.ReportAllocs()
-	for b.Loop() {
-		results, err := FastwalkWithChannel(b.Context(), testDir())
-		require.NoError(b, err)
-		require.LessOrEqual(b, 100, len(results))
-	}
-}
-
-func BenchmarkFastwalkWithLock(b *testing.B) {
-	b.ReportAllocs()
-	for b.Loop() {
-		results, err := FastwalkWithLock(b.Context(), testDir())
+		results, err := testFilewalker.filewalk(b.Context(), testDir, nil)
 		require.NoError(b, err)
 		require.LessOrEqual(b, 100, len(results))
 	}
