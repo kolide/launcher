bc5a589793bf4b2dfa840b83d68d2fc49f16a4ac
RebeccaMahany <rebeccamahany@gmail.com>
2026-03-10 14:21:04 -0400

    Add goleak to all consumers subpackages

--- ee/control/consumers/acceleratecontrolconsumer/acceleratecontrolconsumer_test.go
+++ ee/control/consumers/acceleratecontrolconsumer/acceleratecontrolconsumer_test.go
@@ -7,7 +7,12 @@ import (
 
 	"github.com/kolide/launcher/v2/ee/agent/types/mocks"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
 )
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
 
 func TestAccelerateControlConsumer(t *testing.T) {
 	t.Parallel()
