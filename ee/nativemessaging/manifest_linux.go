2a3f720bc10248b6db2d4e95380ec226888e68e9
RebeccaMahany <rebeccamahany@gmail.com>
2026-06-10 11:37:16 -0400

    Use identifier to disambiguate hostname

--- ee/nativemessaging/manifest_linux.go
+++ ee/nativemessaging/manifest_linux.go
@@ -7,10 +7,10 @@ import "fmt"
 // manifestFileRegistrationLocations returns the filepaths where the native messaging manifest file should exist
 // for this OS.
 // See: https://developer.chrome.com/docs/extensions/develop/concepts/native-messaging#native-messaging-host-location
-func manifestFileRegistrationLocations() []string {
+func manifestFileRegistrationLocations(hostName string) []string {
 	return []string{
-		fmt.Sprintf("/etc/opt/chrome/native-messaging-hosts/%s.json", nativeMessagingHostName),
-		fmt.Sprintf("/etc/opt/chrome_for_testing/native-messaging-hosts/%s.json", nativeMessagingHostName),
-		fmt.Sprintf("/etc/chromium/native-messaging-hosts/%s.json", nativeMessagingHostName),
+		fmt.Sprintf("/etc/opt/chrome/native-messaging-hosts/%s.json", hostName),
+		fmt.Sprintf("/etc/opt/chrome_for_testing/native-messaging-hosts/%s.json", hostName),
+		fmt.Sprintf("/etc/chromium/native-messaging-hosts/%s.json", hostName),
 	}
 }
