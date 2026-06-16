d467cb9e21d2b6c8d16a2b215d23f30879255575
RebeccaMahany <rebeccamahany@gmail.com>
2026-06-16 15:57:25 -0400

    Fix command injection in Get-AuthenticodeSignature

--- ee/nativemessaging/host_windows.go
+++ ee/nativemessaging/host_windows.go
@@ -5,10 +5,21 @@ package nativemessaging
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/kolide/launcher/v2/ee/allowedcmd"
 	"github.com/shirou/gopsutil/v4/process"
+)
+
+const (
+	authenticodePathEnvVar = `LAUNCHER_NATIVEMESSAGING_VERIFY_PATH`
+	// authenticodeVerifyCmd uses -LiteralPath over -FilePath, plus the authenticodePathEnvVar,
+	// to avoid command injection in the case that the path to verify contains a single-quote escape
+	// or wildcard glob characters.
+	authenticodeVerifyCmd = `Get-AuthenticodeSignature -LiteralPath $env:` + authenticodePathEnvVar + ` | ` +
+		`Where-Object {$_.Status -eq "Valid"} | ` +
+		`ForEach-Object { $_.SignerCertificate.Subject }`
 )
 
 // allowlistedBrowsers maps allowlisted browsers to their expected publishers.
@@ -56,11 +67,11 @@ func validateBrowser(ctx context.Context, proc *process.Process) error {
 	// Run Get-AuthenticodeSignature to confirm the codesigning is valid,
 	// and extract the subject to check against `publisher.`
 	// See: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.security/get-authenticodesignature
-	cmdStr := fmt.Sprintf(`Get-AuthenticodeSignature -FilePath '%s' | Where-Object {$_.Status -eq "Valid"} | ForEach-Object { $_.SignerCertificate.Subject }`, pathToVerify)
-	authenticodeCmd, err := allowedcmd.Powershell.Cmd(ctx, cmdStr)
+	authenticodeCmd, err := allowedcmd.Powershell.Cmd(ctx, "-Command", authenticodeVerifyCmd)
 	if err != nil {
 		return fmt.Errorf("creating powershell Get-AuthenticodeSignature cmd: %w", err)
 	}
+	authenticodeCmd.Env = append(os.Environ(), authenticodePathEnvVar+"="+pathToVerify)
 	authenticodeOutput, err := authenticodeCmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("running powershell Get-AuthenticodeSignature against %s: output: `%s`: %w", pathToVerify, string(authenticodeOutput), err)
